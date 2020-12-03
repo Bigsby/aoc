b812bd1e7aaf5856d91456a43355e9d14fc1d5b4
Bruno Bernardo <bigsby@live.com>
2020-12-03 23:14:53 +0000

    2020 day 3 cleanup

---
+++ 2020/solutions/day_03/py/common.py
@@ -0,0 +1,47 @@
+import sys, os
+from typing import List, Tuple
+
+def getInput(filePath: str) -> List[List[int]]:
+    if not os.path.isfile(filePath):
+        print("File not found")
+        sys.exit(1)
+    
+    grid = []
+
+    try:
+        with open(filePath) as inputFile:
+            for line in inputFile.readlines():
+                row = []
+                for c in line:
+                    if c == '#':
+                        row.append(1)
+                    elif c == '.':
+                        row.append(0)
+                    elif c == '\n':
+                        break 
+                    else:
+                        raise Exception("Unrecognized character in input", str(ord(c)))
+                grid.append(row)
+
+ 
+    except Exception as ex:
+        print("Error reading file")
+        print(ex.args[0])
+        sys.exit(1)
+
+    return grid  
+
+def getNextPosition(currentPosition: Tuple[int,int], step: Tuple[int,int]) -> Tuple[int,int]:
+    return (currentPosition[0] + step[0], currentPosition[1] + step[1])
+
+def calculateTrees(grid: List[List[int]], step: Tuple[int,int]) -> int:
+    lastRow = len(grid)
+    lastColumn = len(grid[0])
+    currentPosition = (0, 0)
+    treeCount = 0
+    while currentPosition[0] < lastRow:
+        treeCount = treeCount + grid[currentPosition[0]][currentPosition[1] % lastColumn]
+        currentPosition = getNextPosition(currentPosition, step)
+    return treeCount
+
+
--- 2020/solutions/day_03/py/solve1.py
+++ 2020/solutions/day_03/py/solve1.py
@@ -1,52 +1,15 @@
 #! /usr/bin/python3
 
-import sys, os
-from typing import Tuple
-
-def getNextPosition(currentPosition: Tuple[int,int], step: Tuple[int,int]) -> Tuple[int,int]:
-    return (currentPosition[0] + step[0], currentPosition[1] + step[1])
+import sys
+from common import getInput, getNextPosition, calculateTrees
 
 def main():
     if len(sys.argv) != 2:
         print("Please, add input file path as parameter")
         sys.exit(1)
 
-    inputFilePath = sys.argv[1]
-    if not os.path.isfile(inputFilePath):
-        print("File not found")
-        sys.exit(1)
-    
-    grid = []
-
-    try:
-        with open(inputFilePath) as inputFile:
-            for line in inputFile.readlines():
-                row = []
-                for c in line:
-                    if c == '#':
-                        row.append(1)
-                    elif c == '.':
-                        row.append(0)
-                    elif c == '\n':
-                        break 
-                    else:
-                        raise Exception("Unrecognized character in input", str(ord(c)))
-                grid.append(row)
-
-
-    except Exception as ex:
-        print("Error reading file")
-        print(ex.args[0])
-        sys.exit(1)
-
-    lastRow = len(grid)
-    lastColumn = len(grid[0])
-    currentPosition = (0, 0)
-    step = (1, 3)
-    treeCount = 0
-    while currentPosition[0] < lastRow:
-        treeCount = treeCount + grid[currentPosition[0]][currentPosition[1] % lastColumn]
-        currentPosition = getNextPosition(currentPosition, step)
+    grid = getInput(sys.argv[1])
+    treeCount = calculateTrees(grid, (1, 3))
 
     print("Trees found:", treeCount)
 
