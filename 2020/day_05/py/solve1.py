f4234751d6f66c7ecc6d0db0f11effe8b8c38c18
Bruno Bernardo <bigsby@live.com>
2020-12-05 17:54:25 +0000

    2020/5/1 cleanup

--- 2020/day_05/py/solve1.py
+++ 2020/day_05/py/solve1.py
@@ -1,10 +1,12 @@
 #! /usr/bin/python3
 
+from functools import reduce
+
 from common import getInput
 
 
 def main():
-    highestId = max(map(lambda boardingPass: boardingPass.id, getInput()))
+    highestId = reduce(lambda currentMax, bp: max(currentMax, bp.id), getInput(), 0)
     print("Highest Id is", highestId)
     
 
