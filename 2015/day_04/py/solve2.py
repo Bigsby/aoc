781ce2950b106a0fdb8a6a56751b5ca0cc565bc0
Bruno Bernardo <bigsby@live.com>
2020-12-06 15:29:59 +0000

    2015/4/2

---
+++ 2015/day_04/py/solve2.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python3
+
+from hashlib import md5
+
+from common import getInput
+
+
+def main():
+    key = getInput()
+    guess = 1
+    while True:
+        result = md5((key + str(guess)).encode("utf-8")).hexdigest()
+        if result.startswith("000000"):
+            break
+        guess = guess + 1
+
+    print("Hash is:", guess)
+
+
+if __name__ == "__main__":
+    main()
