e20187de2679a4d79e521a15ddb78846b0d38eb0
Bruno Bernardo <bigsby@live.com>
2020-12-07 22:52:45 +0000

    2015/09/1 and 2

---
+++ 2015/day_09/py/solve2.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3
+
+from itertools import permutations
+
+from common import getInput, getPathDistance, getSingleNodes, getPossiblePaths
+
+
+def main():
+    edges = list(getInput())
+    singleNodes = getSingleNodes(edges)
+    possiblePaths = getPossiblePaths(singleNodes)
+    minimalDistance = max(map(lambda permutation: getPathDistance(permutation, edges), possiblePaths))
+    print("Minimal distance:", minimalDistance)
+
+
+if __name__ == "__main__":
+    main()
