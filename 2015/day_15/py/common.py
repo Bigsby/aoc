a693446768c7644f0df378677c25c65a1f468cb0
Bruno Bernardo <bigsby@live.com>
2020-12-08 18:38:07 +0000

    2015/15 clean up for 2

--- 2015/day_15/py/common.py
+++ 2015/day_15/py/common.py
@@ -1,7 +1,10 @@
 import sys, os, re
+from functools import reduce
+from itertools import combinations_with_replacement
 
 
 lineRegex = re.compile(r"^(\w+):\scapacity\s(-?\d+),\sdurability\s(-?\d+),\sflavor\s(-?\d+),\stexture\s(-?\d+),\scalories\s(-?\d+)$")
+valueProperties = [ "capacity", "durability", "flavor", "texture" ]
 
 
 class Entry():
@@ -18,6 +21,27 @@ class Entry():
     def __repr__(self):
         return self.__str__()
 
+
+def getValueForProperty(solution, entries, property):
+    return reduce(lambda soFar, entry: soFar + getattr(entry, property) * solution[entry.name], entries, 0)
+
+
+def findValueForSolution(solution, entries):
+    values = {}
+    for property in valueProperties:
+        values[property] = getValueForProperty(solution, entries, property)
+    totalScore = reduce(lambda soFar, key: soFar * (values[key] if values[key] > 0 else 1), values, 1)
+    calories = getValueForProperty(solution, entries, "calories")
+    return totalScore, calories
+
+
+def getPossibleCombinations(ingredients, totalSpoons):
+    return combinations_with_replacement(ingredients, totalSpoons)
+
+
+def createSolutionFromCombination(combination, ingredients):
+    return { ingredient: combination.count(ingredient) for ingredient in ingredients }
+ 
 
 def getInput():
     if len(sys.argv) != 2:
