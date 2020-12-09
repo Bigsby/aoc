8e5802a8e0b57c5bed6e83132fa03fc9a510fac7
Bruno Bernardo <bigsby@live.com>
2020-12-09 14:33:24 +0000

    2020/09/1

---
+++ 2020/day_09/py/solve1.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python3
+
+from itertools import combinations
+
+from common import getInput
+
+
+def hasValidPair(numberIndex, numbers):
+    number = numbers[numberIndex]
+    for testindex in range(numberIndex - 25, numberIndex):
+        testNumber = numbers[testindex]
+        for pairIndex in range(numberIndex - 25, numberIndex):
+            if pairIndex == testindex:
+                continue
+            if testNumber + numbers[pairIndex] == number:
+                return True
+    return False
+
+
+
+def main():
+    numbers = list(getInput())
+
+    for index in range(25, len(numbers)):
+        if not hasValidPair(index, numbers):
+            print("First to fail is", numbers[index])
+            break
+
+
+    
+
+
+if __name__ == "__main__":
+    main()
