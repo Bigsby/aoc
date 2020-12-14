9ad97ef01d67520fe93b6ffa87cb0b5097a07b1c
Bruno Bernardo <bigsby@live.com>
2020-12-14 11:00:15 +0000

    2020/14/2

---
+++ 2020/day_14/py/solve2.py
@@ -0,0 +1,48 @@
+#! /usr/bin/python3
+
+import re
+from functools import reduce
+
+from common import getInput, InstructionType, Computer
+
+xRegex = re.compile("X")
+
+class MemoryMaskComputer(Computer):
+
+    def getMemoryLocations(self, location):
+        orMask = int(self.mask.replace("X", "0"), 2)
+        location = location | orMask
+        flipBits = []
+        locationMask = f"{location:036b}"
+        
+        for match in xRegex.finditer(self.mask):
+            flipBits.append(match.start())
+
+        for case in range(2**len(flipBits)):
+            currentLocation = list(locationMask)
+            for index, flipBit in enumerate(flipBits):
+                newBit = ((2**index) & case) >> index
+                currentLocation[flipBit] = str(newBit)
+            yield int("".join(currentLocation), 2)
+
+
+    def runInstruction(self, instruction):
+        if instruction.type == InstructionType.Mask:
+            self.setMask(instruction.mask)
+        else:
+            for location in self.getMemoryLocations(instruction.location):
+                self.setMemory(location, instruction.value)
+
+
+def main():
+    computer = MemoryMaskComputer()
+    for instruction in getInput():
+        computer.runInstruction(instruction)
+
+
+    result = computer.getMemorySum()
+    print("Result:", result)
+
+
+if __name__ == "__main__":
+    main()
