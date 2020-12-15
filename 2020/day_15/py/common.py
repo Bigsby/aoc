44312f58b1887a9a1340c7dbe8f8f739910cea8c
Bruno Bernardo <bigsby@live.com>
2020-12-15 11:54:42 +0000

    2020/15 cleanup

--- 2020/day_15/py/common.py
+++ 2020/day_15/py/common.py
@@ -1,4 +1,39 @@
 import sys, os
+from typing import NamedTuple
+
+
+class LastOccurrences(NamedTuple):
+    last: int
+    second: int
+
+
+def getNthTurn(turns: int) -> int:
+    numbers = list(getInput())
+    turn = 0
+    occurrences = {}
+    for number in numbers:
+        turn += 1
+        occurrences[number] = LastOccurrences(turn, 0)
+
+    lastNumber = numbers[-1]
+    
+    while turn < turns:
+        turn += 1
+        occurrence = occurrences[lastNumber]
+
+        if occurrence.second == 0:
+            lastNumber = 0
+        else:
+            lastNumber = occurrence.last - occurrence.second
+
+        if lastNumber in occurrences:
+            lastNumberOccurrences = occurrences.get(lastNumber)
+            occurrences[lastNumber] = LastOccurrences(turn, lastNumberOccurrences.last)
+        else:
+            occurrences[lastNumber] = LastOccurrences(turn, 0)
+
+    return lastNumber
+    
 
 def getInput():
     if len(sys.argv) != 2:
