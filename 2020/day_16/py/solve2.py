1f4711d080ccaefe4d911191d50b2973dcbe809e
Bruno Bernardo <bigsby@live.com>
2020-12-16 11:59:50 +0000

    2020/16/2 optimize

--- 2020/day_16/py/solve2.py
+++ 2020/day_16/py/solve2.py
@@ -25,29 +25,29 @@ def main():
             if fieldName != ownerName and index in fieldPositions:
                 fieldPositions.remove(index)
                 if len(fieldPositions) == 1:
-                    purge(fieldName, fieldPositions[0])
+                    purge(fieldName, list(fieldPositions)[0])
 
     for rule in rules:
         fields[rule[0]] = {
             rangeField: (rule[1], rule[2], rule[3], rule[4]),
-            positionsField: [ i for i in range(0, len(rules)) ]
+            positionsField: { i for i in range(0, len(rules)) }
         }
 
     for ticket in validTickets:
         for index, number in enumerate(ticket):
             for fieldName in fields:
                 field = fields[fieldName]
-                if not index in field[positionsField]:
+                if index not in field[positionsField]:
                     continue
 
                 rule = field[rangeField]
                 if number < rule[0] or (number > rule[1] and number < rule[2]) or number > rule[3]:
                     field[positionsField].remove(index)
                     if len(field[positionsField]) == 1:
-                        purge(fieldName, field[positionsField][0])
+                        purge(fieldName, list(field[positionsField])[0])
 
                                 
-    departureFieldIndexes = [ fields[fieldName][positionsField][0] for fieldName in fields if fieldName.startswith("departure") ]
+    departureFieldIndexes = [ list(fields[fieldName][positionsField])[0] for fieldName in fields if fieldName.startswith("departure") ]
     result = reduce(lambda soFar, index: soFar * myTicket[index], departureFieldIndexes, 1)
      
     print("Result:", result)
