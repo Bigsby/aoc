37a3ef41c68d5d6035e1485ca622367501ee758a
Bruno Bernardo <bigsby@live.com>
2020-12-22 16:53:43 +0000

    2017/01

---
+++ 2017/day_01/py/common.py
@@ -0,0 +1,15 @@
+import sys, os
+
+def getInput():
+    if len(sys.argv) != 2:
+        print("Please, add input file path as parameter")
+        sys.exit(1)
+
+    filePath = sys.argv[1]
+    if not os.path.isfile(filePath):
+        print("File not found")
+        sys.exit(1)
+
+    with open(filePath, "r") as file:
+        for c in file.read().strip():
+            yield int(c)
