dbd8ea2d5380bb90a3c5288fe0ffe0d07f9dc489
Bruno Bernardo <bigsby@live.com>
2020-12-28 04:30:05 +0000

    Add single file Python template

---
+++ templates/py/run.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python3
+
+import sys, os, time
+
+
+def part1(puzzleInput):
+    pass
+
+
+def part2(puzzleInput):
+    pass
+
+
+def getInput(filePath):
+    if not os.path.isfile(filePath):
+        raise FileNotFoundError(filePath)
+    
+    with open(filePath, "r") as file:
+        for line in file.readlines():
+            yield line
+
+
+def main():
+    if len(sys.argv) != 2:
+        raise Exception("Please, add input file path as parameter")
+
+    puzzleInput = getInput(sys.argv[1])
+    start = time.perf_counter()
+    part1Result = part1(puzzleInput)
+    middle = time.perf_counter()
+    part2Result = part2(puzzleInput)
+    end = time.perf_counter()
+    print("P1:", part1Result)
+    print("P2:", part2Result)
+    print()
+    print(f"P1 time: {middle - start:.8f}")
+    print(f"P2 time: {end - middle:.8f}")
+
+
+if __name__ == "__main__":
+    main()
