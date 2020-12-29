8b180508364a7b8e6fcf7018ffcdebca88f7ffcd
Bruno Bernardo <bigsby@live.com>
2020-12-29 21:01:26 +0000

    2018/01 reformat

---
+++ 2018/day_01/py/run.py
@@ -0,0 +1,50 @@
+#! /usr/bin/python3
+
+import sys, os, time
+
+
+def part1(puzzleInput):
+    return sum(puzzleInput)
+
+
+def part2(puzzleInput):
+    changes = puzzleInput
+    changesLength = len(changes)
+    frequency = 0
+    previous = set() 
+    index = 0
+    while frequency not in previous:
+        previous.add(frequency)
+        frequency += changes[index]
+        index = (index + 1) % changesLength
+
+    return frequency
+
+
+def getInput(filePath):
+    if not os.path.isfile(filePath):
+        raise FileNotFoundError(filePath)
+    
+    with open(filePath, "r") as file:
+        return [ int(line) for line in file.readlines() ]
+
+
+def main():
+    if len(sys.argv) != 2:
+        raise Exception("Please, add input file path as parameter")
+
+    puzzleInput = getInput(sys.argv[1])
+    start = time.perf_counter()
+    part1Result = part1(puzzleInput)
+    middle = time.perf_counter()
+    part2Result = part2(puzzleInput)
+    end = time.perf_counter()
+    print("P1:", part1Result)
+    print("P2:", part2Result)
+    print()
+    print(f"P1 time: {middle - start:.8f}")
+    print(f"P2 time: {end - middle:.8f}")
+
+
+if __name__ == "__main__":
+    main()
