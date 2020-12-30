508a6da82bbb4a4a3fbfccdb791bf9e07346c536
Bruno Bernardo <bigsby@live.com>
2020-12-30 18:00:47 +0000

    2015/08 reformat

---
+++ 2015/day_08/py/run.py
@@ -0,0 +1,63 @@
+#! /usr/bin/python3
+
+import sys, os, time
+import re
+from functools import reduce
+
+
+def countDifferences(puzzleInput, differencesFunc):
+    return reduce(lambda soFar, s: soFar + differencesFunc(s), puzzleInput, 0)
+
+
+hexaRegex = re.compile(r"\\x[0-9a-f]{2}")
+def getStringDifference1(string):
+    totalLength = len(string)
+    stripped = string.replace(r"\\", "r")
+    stripped = stripped.replace(r"\"", "r")
+    stripped = hexaRegex.sub("r", stripped)
+    stripped = stripped.strip(r"\"")
+    return totalLength - len(stripped) 
+
+
+def part1(puzzleInput):
+    return countDifferences(puzzleInput, getStringDifference1)
+
+
+def getStringDifference2(string):
+    initialLength = len(string)
+    escaped = re.escape(string)
+    escaped = escaped.replace("\"", "\\\"")
+    return 2 + len(escaped) - initialLength
+
+
+def part2(puzzleInput):
+    return countDifferences(puzzleInput, getStringDifference2)
+
+
+def getInput(filePath):
+    if not os.path.isfile(filePath):
+        raise FileNotFoundError(filePath)
+    
+    with open(filePath, "r") as file:
+        return [ line.strip() for line in file.readlines() ]
+
+
+def main():
+    if len(sys.argv) != 2:
+        raise Exception("Please, add input file path as parameter")
+
+    puzzleInput = getInput(sys.argv[1])
+    start = time.perf_counter()
+    part1Result = part1(puzzleInput)
+    middle = time.perf_counter()
+    part2Result = part2(puzzleInput)
+    end = time.perf_counter()
+    print("P1:", part1Result)
+    print("P2:", part2Result)
+    print()
+    print(f"P1 time: {middle - start:.8f}")
+    print(f"P2 time: {end - middle:.8f}")
+
+
+if __name__ == "__main__":
+    main()
