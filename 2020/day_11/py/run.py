a715bd1ef00395ff715418d3b0c27adc2a85f177
Bruno Bernardo <bigsby@live.com>
2021-01-04 13:22:01 +0000

    2020/11 use complex numbers for coordinates

--- 2020/day_11/py/run.py
+++ 2020/day_11/py/run.py
@@ -11,135 +11,83 @@ class State(Enum):
     FLOOR = '.'
 
 
-class Grid():
-    def __init__(self):
-        self.rows = []
-        self.occupiedTolerance = 0
-        self.getNeighbors = lambda _: []
-
-    def addRow(self, row):
-        newRow = []
-        for c in row:
-            newRow.append(State(c))
-        self.rows.append(newRow)
-    
-    def _setOccupiedTolerance(self, tolerance):
-        self.occupiedTolerance = tolerance
-    
-    def _setGetNeighbors(self, func):
-        self.getNeighbors = func
-
-    def clone(self, tolerance, getNeighborsFunc):
-        clone = Grid()
-        clone._setOccupiedTolerance(tolerance)
-        clone._setGetNeighbors(getNeighborsFunc)
-        for row in self.rows:
-            clone.rows.append(list(row))
-        return clone
-
-    def print(self):
-        for row in self.rows:
-            print("".join(map(lambda s: s.value, row)))
-
-    def isNeighborValid(self, coordinate):
-        rowLimit = len(self.rows)
-        columnLimit = len(self.rows[0])
-        return coordinate[0] >= 0 and coordinate[0] < rowLimit and coordinate[1] >= 0 and coordinate[1] < columnLimit
- 
-    def getOccupiedNeighborsCount(self, row, column):
-        count = 0
-        neighbors = self.getNeighbors(self, row, column)
-        for neighbor in neighbors:
-            if self.isNeighborValid(neighbor) and self.rows[neighbor[0]][neighbor[1]] == State.OCCUPIED:
-                count += 1
-        return count
-
-    def getNewState(self, row, column):
-        currentState = self.rows[row][column]
-        if currentState == State.FLOOR:
-            return False, State.FLOOR
-        occupiedCount = self.getOccupiedNeighborsCount(row, column)
-        if currentState == State.EMPTY and occupiedCount == 0:
-            return True, State.OCCUPIED
-        if currentState == State.OCCUPIED and occupiedCount > self.occupiedTolerance:
-            return True, State.EMPTY
-        return False, currentState
-
-    def goToNextState(self):
-        rowCount = len(self.rows)
-        columnCount = len(self.rows[0])
-        newGrid = [ [State.FLOOR] * columnCount for i in range(rowCount) ]
-        seatsChangedCount = 0
-        for row in range(rowCount):
-            for column in range(columnCount):
-                changed, newState = self.getNewState(row, column)
-                seatsChangedCount += changed
-                newGrid[row][column] = newState
-        self.rows = newGrid
-        return seatsChangedCount
-
-    def getOccupiedCount(self):
-        return reduce(lambda rowCount, row: rowCount + reduce(lambda columnCount, column: columnCount + (column == State.OCCUPIED), row, 0), self.rows, 0)
-
-
-def runGrid(grid):
-    while grid.goToNextState():
-        pass
-    return grid.getOccupiedCount()
-
-
-def getNeighbors1(_, row, column):
-    return [ 
-        (row - 1, column -1),
-        (row - 1, column),
-        (row - 1, column + 1),
-        (row, column - 1),
-        (row, column + 1),
-        (row + 1, column -1),
-        (row + 1, column),
-        (row + 1, column +1)
-    ]
-
-
-def part1(puzzleInput):
-    return runGrid(puzzleInput.clone(3, getNeighbors1))
-
-
-directionSteps = [ 
-    (-1, -1),
-    (-1, 0),
-    (-1, 1),
-    (0, -1),
-    (0, 1),
-    (1, -1),
-    (1, 0),
-    (1, 1)
+NEIGHBOR_DIRECTIONS = [
+     - 1 - 1j,
+         - 1j,
+     + 1 - 1j,
+     - 1,
+     + 1,
+     - 1 + 1j,
+         + 1j,
+     + 1 + 1j
 ]
-def getNeighbors2(grid, row, column):
-    neighbors = []
-    for rowStep, columnStep in directionSteps:
-        currentNeighbor = (row + rowStep, column + columnStep)
-        while grid.isNeighborValid(currentNeighbor) and grid.rows[currentNeighbor[0]][currentNeighbor[1]] == State.FLOOR:
-            currentNeighbor = (currentNeighbor[0] + rowStep, currentNeighbor[1] + columnStep)
-
-        neighbors.append(currentNeighbor)
-
-    return neighbors
+def getOccupiedCount(grid, position, getNeighborFunc):
+    total = 0
+    for direction in NEIGHBOR_DIRECTIONS:
+        neighbor = getNeighborFunc(grid, position, direction)
+        if neighbor in grid and grid[neighbor] == State.OCCUPIED:
+            total += 1
+    return total
 
 
-def part2(puzzleInput):
-    return runGrid(puzzleInput.clone(4, getNeighbors2))
+def getPositionNewState(grid, position, tolerance, getNeighborFunc):
+    currentState = grid[position]
+    if currentState == State.FLOOR:
+        return False, State.FLOOR
+    occupiedCount = getOccupiedCount(grid, position, getNeighborFunc)
+    if currentState == State.EMPTY and occupiedCount == 0:
+        return True, State.OCCUPIED
+    if currentState == State.OCCUPIED and occupiedCount > tolerance:
+        return True, State.EMPTY
+    return False, currentState
+
+
+def getNextState(grid, tolerance, getNeighborFunc):
+    newState = dict(grid)
+    changedCount = 0
+    for position in grid:
+        changed, newPositionState = getPositionNewState(grid, position, tolerance, getNeighborFunc)
+        changedCount += changed
+        newState[position] = newPositionState
+    return changedCount, newState
+        
+
+def runGrid(grid, tolerance, getNeighborFunc):
+    grid = dict(grid)
+    changed = 1
+    while changed:
+        changed, grid = getNextState(grid, tolerance, getNeighborFunc)
+    return sum(map(lambda value: 1 if value == State.OCCUPIED else 0, grid.values()))
+
+
+def part1(grid):
+    return runGrid(grid, 3, lambda _, position, direction: position + direction)
+
+
+def getDirectionalNeighbor(grid, position, direction):
+    position += direction
+    while position in grid and grid[position] == State.FLOOR:
+        position += direction
+    return position
+
+
+def part2(grid):
+    return runGrid(grid, 4, getDirectionalNeighbor)
 
 
 def getInput(filePath):
     if not os.path.isfile(filePath):
         raise FileNotFoundError(filePath)
     
-    grid = Grid()
     with open(filePath, "r") as file:
+        grid = {}
+        pos = 0j
         for line in file.readlines():
-            grid.addRow(line.strip())
-    return grid
+            for c in line.strip():
+                grid[pos] = State(c)
+                pos += 1
+            pos += 1j - pos.real
+        return grid
 
 
 def main():
