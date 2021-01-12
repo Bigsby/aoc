993f92be311897fffd6f18df0347db50cdde0e2b
Bruno Bernardo <bigsby@live.com>
2021-01-12 16:25:22 +0000

    2017/11

---
+++ 2017/day_11/py/run.py
@@ -0,0 +1,58 @@
+#! /usr/bin/python3
+
+import sys, os, time
+from typing import List
+import re
+
+
+DIRECTIONS = {
+    "s" :       1j,
+    "se":   1     ,
+    "sw": - 1 + 1j,
+    "ne":   1 - 1j,
+    "nw": - 1     ,
+    "n" :     - 1j
+}
+def part1(instructions: List[str]):
+    furthest = 0
+    currentHex = 0
+    for instrucion in instructions:
+        currentHex += DIRECTIONS[instrucion]
+        distance = int(max(abs(currentHex.real) , abs(currentHex.imag)))
+        furthest = max(furthest, distance)
+
+    return int(max(abs(currentHex.real) , abs(currentHex.imag))), furthest
+
+
+def part2(result: int) -> int:
+    return result
+
+
+instructionRegex = re.compile(r"ne|nw|n|sw|se|s")
+def getInput(filePath: str) -> List[str]:
+    if not os.path.isfile(filePath):
+        raise FileNotFoundError(filePath)
+    
+    with open(filePath, "r") as file:
+        return [ match.group() for match in instructionRegex.finditer(file.read()) ]
+
+
+def main():
+    if len(sys.argv) != 2:
+        raise Exception("Please, add input file path as parameter")
+
+    puzzleInput = getInput(sys.argv[1])
+    start = time.perf_counter()
+    part1Result, part2Result = part1(puzzleInput)
+    middle = time.perf_counter()
+    part2Result = part2(part2Result)
+    end = time.perf_counter()
+    print("P1:", part1Result)
+    print("P2:", part2Result)
+    print()
+    print(f"P1 time: {middle - start:.8f}")
+    print(f"P2 time: {end - middle:.8f}")
+
+
+if __name__ == "__main__":
+    main()
