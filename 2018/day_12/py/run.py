3f4a11de84a6937a9665f52083ef1ff9a72e910d
Bruno Bernardo <bigsby@live.com>
2021-01-13 12:05:31 +0000

    2018/12

---
+++ 2018/day_12/py/run.py
@@ -0,0 +1,77 @@
+#! /usr/bin/python3
+
+import sys, os, time
+from typing import Dict, List, Tuple
+import re
+
+State = List[int]
+Notes = Dict[int,int]
+
+
+def getStateValue(index: int, state: State):
+    return sum([ 2 ** i for i in range(5) if i + index - 2 in state ])
+
+
+def runGenerations(puzzleInput: Tuple[State,Notes], generations: int) -> int:
+    state, notes = puzzleInput
+    generation = 0
+    while generation < generations:
+        state = [ index for index in range(min(state) - 2, max(state) + 2) if notes[getStateValue(index, state)] ]
+        generation += 1
+
+    return sum(state)
+
+
+def part1(puzzleInput: Tuple[State,Notes]) -> int:
+    return runGenerations(puzzleInput, 20)
+
+
+def part2(puzzleInput: Tuple[State,Notes]) -> int:
+    thousands = runGenerations(puzzleInput,  1000)
+    twoThousands = runGenerations(puzzleInput,  2000)
+    diff = twoThousands - thousands
+    target = 5 * 10 ** 10
+    return thousands + diff * ( target // 1000 - 1)
+
+
+initialStateRegex = re.compile(r"#|\.")
+def parseInitialState(line: str) -> List[int]:
+    return [ index for index, match in enumerate(initialStateRegex.finditer(line)) if match.group() == "#" ]
+
+
+def computePattern(pattern: str) -> int:
+    return sum([ 2**index for index, c in enumerate(pattern) if c == "#" ] )
+
+notesRegex = re.compile(r"^(?P<pattern>[#\.]{5})\s=>\s(?P<result>[#\.])$", re.MULTILINE)
+def parseNotes(noteLines: str) -> Notes:
+    return { computePattern(match.group("pattern")): 1 if match.group("result") == "#" else 0 for match in notesRegex.finditer(noteLines) }
+
+
+def getInput(filePath: str) -> Tuple[State,Notes]:
+    if not os.path.isfile(filePath):
+        raise FileNotFoundError(filePath)
+    
+    with open(filePath, "r") as file:
+        initialStateLine, noteLines = file.read().split("\n\n")
+        return parseInitialState(initialStateLine), parseNotes(noteLines)
+
+
+def main():
+    if len(sys.argv) != 2:
+        raise Exception("Please, add input file path as parameter")
+
+    puzzleInput = getInput(sys.argv[1])
+    start = time.perf_counter()
+    part1Result = part1(puzzleInput)
+    middle = time.perf_counter()
+    part2Result = part2(puzzleInput)
+    end = time.perf_counter()
+    print("P1:", part1Result)
+    print("P2:", part2Result)
+    print()
+    print(f"P1 time: {middle - start:.8f}")
+    print(f"P2 time: {end - middle:.8f}")
+
+
+if __name__ == "__main__":
+    main()
