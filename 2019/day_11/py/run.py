a2faf5663c7990da497b125a9101ce5b1ebd1c0e
Bruno Bernardo <bigsby@live.com>
2021-01-13 02:52:14 +0000

    2019/11

---
+++ 2019/day_11/py/run.py
@@ -0,0 +1,333 @@
+#! /usr/bin/python3
+
+import sys, os, time
+from typing import Dict, Iterable, List, Tuple
+from collections import defaultdict
+from itertools import product
+
+
+CHARACTER_WIDTH = 5
+LETTERS: Dict[int,str] = {
+    (0b01100 << CHARACTER_WIDTH * 0) + \
+    (0b10010 << CHARACTER_WIDTH * 1) + \
+    (0b10010 << CHARACTER_WIDTH * 2) + \
+    (0b11110 << CHARACTER_WIDTH * 3) + \
+    (0b10010 << CHARACTER_WIDTH * 4) + \
+    (0b10010 << CHARACTER_WIDTH * 5): "A",
+
+    (0b11100 << CHARACTER_WIDTH * 0) + \
+    (0b10010 << CHARACTER_WIDTH * 1) + \
+    (0b11100 << CHARACTER_WIDTH * 2) + \
+    (0b10010 << CHARACTER_WIDTH * 3) + \
+    (0b10010 << CHARACTER_WIDTH * 4) + \
+    (0b11100 << CHARACTER_WIDTH * 5): "B",
+
+    (0b01100 << CHARACTER_WIDTH * 0) + \
+    (0b10010 << CHARACTER_WIDTH * 1) + \
+    (0b10000 << CHARACTER_WIDTH * 2) + \
+    (0b10000 << CHARACTER_WIDTH * 3) + \
+    (0b10010 << CHARACTER_WIDTH * 4) + \
+    (0b01100 << CHARACTER_WIDTH * 5): "C",
+
+    (0b11100 << CHARACTER_WIDTH * 0) + \
+    (0b10010 << CHARACTER_WIDTH * 1) + \
+    (0b10010 << CHARACTER_WIDTH * 2) + \
+    (0b10010 << CHARACTER_WIDTH * 3) + \
+    (0b10010 << CHARACTER_WIDTH * 4) + \
+    (0b11100 << CHARACTER_WIDTH * 5): "D",
+
+    (0b11110 << CHARACTER_WIDTH * 0) + \
+    (0b10000 << CHARACTER_WIDTH * 1) + \
+    (0b11100 << CHARACTER_WIDTH * 2) + \
+    (0b10000 << CHARACTER_WIDTH * 3) + \
+    (0b10000 << CHARACTER_WIDTH * 4) + \
+    (0b11110 << CHARACTER_WIDTH * 5): "E",
+
+    (0b11110 << CHARACTER_WIDTH * 0) + \
+    (0b10000 << CHARACTER_WIDTH * 1) + \
+    (0b11100 << CHARACTER_WIDTH * 2) + \
+    (0b10000 << CHARACTER_WIDTH * 3) + \
+    (0b10000 << CHARACTER_WIDTH * 4) + \
+    (0b10000 << CHARACTER_WIDTH * 5): "F",
+
+    (0b01100 << CHARACTER_WIDTH * 0) + \
+    (0b10010 << CHARACTER_WIDTH * 1) + \
+    (0b10000 << CHARACTER_WIDTH * 2) + \
+    (0b10110 << CHARACTER_WIDTH * 3) + \
+    (0b10010 << CHARACTER_WIDTH * 4) + \
+    (0b01110 << CHARACTER_WIDTH * 5): "G",
+
+    (0b10010 << CHARACTER_WIDTH * 0) + \
+    (0b10010 << CHARACTER_WIDTH * 1) + \
+    (0b11110 << CHARACTER_WIDTH * 2) + \
+    (0b10010 << CHARACTER_WIDTH * 3) + \
+    (0b10010 << CHARACTER_WIDTH * 4) + \
+    (0b10010 << CHARACTER_WIDTH * 5): "H",
+
+    (0b01110 << CHARACTER_WIDTH * 0) + \
+    (0b00100 << CHARACTER_WIDTH * 1) + \
+    (0b00100 << CHARACTER_WIDTH * 2) + \
+    (0b00100 << CHARACTER_WIDTH * 3) + \
+    (0b00100 << CHARACTER_WIDTH * 4) + \
+    (0b01110 << CHARACTER_WIDTH * 5): "I",
+
+    (0b00110 << CHARACTER_WIDTH * 0) + \
+    (0b00010 << CHARACTER_WIDTH * 1) + \
+    (0b00010 << CHARACTER_WIDTH * 2) + \
+    (0b00010 << CHARACTER_WIDTH * 3) + \
+    (0b10010 << CHARACTER_WIDTH * 4) + \
+    (0b01100 << CHARACTER_WIDTH * 5): "J",
+
+    (0b10010 << CHARACTER_WIDTH * 0) + \
+    (0b10100 << CHARACTER_WIDTH * 1) + \
+    (0b11000 << CHARACTER_WIDTH * 2) + \
+    (0b10100 << CHARACTER_WIDTH * 3) + \
+    (0b10100 << CHARACTER_WIDTH * 4) + \
+    (0b10010 << CHARACTER_WIDTH * 5): "K",
+
+    (0b10000 << CHARACTER_WIDTH * 0) + \
+    (0b10000 << CHARACTER_WIDTH * 1) + \
+    (0b10000 << CHARACTER_WIDTH * 2) + \
+    (0b10000 << CHARACTER_WIDTH * 3) + \
+    (0b10000 << CHARACTER_WIDTH * 4) + \
+    (0b11110 << CHARACTER_WIDTH * 5): "L",
+
+    (0b01100 << CHARACTER_WIDTH * 0) + \
+    (0b10010 << CHARACTER_WIDTH * 1) + \
+    (0b10010 << CHARACTER_WIDTH * 2) + \
+    (0b10010 << CHARACTER_WIDTH * 3) + \
+    (0b10010 << CHARACTER_WIDTH * 4) + \
+    (0b01100 << CHARACTER_WIDTH * 5): "O",
+
+    (0b11100 << CHARACTER_WIDTH * 0) + \
+    (0b10010 << CHARACTER_WIDTH * 1) + \
+    (0b10010 << CHARACTER_WIDTH * 2) + \
+    (0b11100 << CHARACTER_WIDTH * 3) + \
+    (0b10000 << CHARACTER_WIDTH * 4) + \
+    (0b10000 << CHARACTER_WIDTH * 5): "P",
+
+    (0b11100 << CHARACTER_WIDTH * 0) + \
+    (0b10010 << CHARACTER_WIDTH * 1) + \
+    (0b10010 << CHARACTER_WIDTH * 2) + \
+    (0b11100 << CHARACTER_WIDTH * 3) + \
+    (0b10100 << CHARACTER_WIDTH * 4) + \
+    (0b10010 << CHARACTER_WIDTH * 5): "R",
+
+    (0b01110 << CHARACTER_WIDTH * 0) + \
+    (0b10000 << CHARACTER_WIDTH * 1) + \
+    (0b10000 << CHARACTER_WIDTH * 2) + \
+    (0b01100 << CHARACTER_WIDTH * 3) + \
+    (0b00010 << CHARACTER_WIDTH * 4) + \
+    (0b11100 << CHARACTER_WIDTH * 5): "S",
+
+    (0b10010 << CHARACTER_WIDTH * 0) + \
+    (0b10010 << CHARACTER_WIDTH * 1) + \
+    (0b10010 << CHARACTER_WIDTH * 2) + \
+    (0b10010 << CHARACTER_WIDTH * 3) + \
+    (0b10010 << CHARACTER_WIDTH * 4) + \
+    (0b01100 << CHARACTER_WIDTH * 5): "U",
+
+    (0b10001 << CHARACTER_WIDTH * 0) + \
+    (0b10001 << CHARACTER_WIDTH * 1) + \
+    (0b01010 << CHARACTER_WIDTH * 2) + \
+    (0b00100 << CHARACTER_WIDTH * 3) + \
+    (0b00100 << CHARACTER_WIDTH * 4) + \
+    (0b00100 << CHARACTER_WIDTH * 5): "Y",
+
+    (0b11110 << CHARACTER_WIDTH * 0) + \
+    (0b00010 << CHARACTER_WIDTH * 1) + \
+    (0b00100 << CHARACTER_WIDTH * 2) + \
+    (0b01000 << CHARACTER_WIDTH * 3) + \
+    (0b10000 << CHARACTER_WIDTH * 4) + \
+    (0b11110 << CHARACTER_WIDTH * 5): "Z"
+}
+
+
+class IntCodeComputer():
+    def __init__(self, memory: List[int], inputs: List[int] = []):
+        self.memory = defaultdict(int, [ (index, value) for index, value in enumerate(memory) ])
+        self.pointer = 0
+        self.inputs = inputs
+        self.outputs = [ ]
+        self.base = 0
+        self.running = True
+        self.polling = False
+        self.outputing = False
+    
+    def setInput(self, value: int):
+        self.inputs.insert(0, value)
+    
+    def runUntilHalt(self) -> List[int]:
+        while self.running:
+            self.tick()
+        return self.outputs
+    
+    def getParameter(self, offset: int, mode: int) -> int:
+        value = self.memory[self.pointer + offset]
+        if mode == 0: # POSITION
+            return self.memory[value]
+        if mode == 1: # IMMEDIATE
+            return value
+        elif mode == 2: # RELATIVE
+            return self.memory[self.base + value]
+        raise Exception("Unrecognized parameter mode", mode)
+    
+    def getAddress(self, offset: int, mode: int) -> int:
+        value = self.memory[self.pointer + offset]
+        if mode == 0: # POSITION
+            return value
+        if mode == 2: # RELATIVE
+            return self.base + value
+        raise Exception("Unrecognized address mode", mode)
+
+
+    def getOutput(self) -> int:
+        self.outputing = False
+        return self.outputs.pop()
+    
+    def addInput(self, value: int):
+        self.inputs.append(value)
+
+    def tick(self):
+        instruction = self.memory[self.pointer]
+        opcode, p1mode, p2mode, p3mode = instruction % 100, (instruction // 100) % 10, (instruction // 1000) % 10, (instruction // 10000) % 10
+        if not self.running:
+            return
+        if opcode == 1: # ADD
+            self.memory[self.getAddress(3, p3mode)] = self.getParameter(1, p1mode) + self.getParameter(2, p2mode)
+            self.pointer += 4
+        elif opcode == 2: # MUL
+            self.memory[self.getAddress(3, p3mode)] = self.getParameter(1, p1mode) * self.getParameter(2, p2mode)
+            self.pointer += 4
+        elif opcode == 3: # INPUT
+            if self.inputs:
+                self.polling = False
+                self.memory[self.getAddress(1, p1mode)] = self.inputs.pop(0)
+                self.pointer += 2
+            else:
+                self.polling = True
+        elif opcode == 4: # OUTPUT
+            self.outputing = True
+            self.outputs.append(self.getParameter(1, p1mode))
+            self.pointer += 2
+        elif opcode == 5: # JMP_TRUE
+            if self.getParameter(1, p1mode):
+                self.pointer = self.getParameter(2, p2mode)
+            else:
+                self.pointer += 3
+        elif opcode == 6: # JMP_FALSE
+            if not self.getParameter(1, p1mode):
+                self.pointer = self.getParameter(2, p2mode)
+            else:
+                self.pointer += 3
+        elif opcode == 7: # LESS_THAN
+            self.memory[self.getAddress(3, p3mode)] = 1 if self.getParameter(1, p1mode) < self.getParameter(2, p2mode) else 0
+            self.pointer += 4
+        elif opcode == 8: # EQUALS
+            self.memory[self.getAddress(3, p3mode)] = 1 if self.getParameter(1, p1mode) == self.getParameter(2, p2mode) else 0
+            self.pointer += 4
+        elif opcode == 9: # SET_BASE
+            self.base += self.getParameter(1, p1mode)
+            self.pointer += 2
+        elif opcode == 99: # HALT
+            self.running = False
+        else:
+            raise Exception(f"Unknown instruction", self.pointer, instruction, opcode, p1mode, p2mode, p3mode)
+    
+    def __str__(self):
+        return f"s {self.running} p {self.pointer} i {self.inputs} o {self.outputs}"
+
+
+DIRECTION_CHANGES = {
+    0: 1j,
+    1: -1j
+}
+def runProgram(memory: List[int], panels: Dict[complex,int]) -> Dict[complex,int]:
+    robot = IntCodeComputer(memory)
+    position = 0j
+    heading = 1j
+    waitingForColor = True
+    while robot.running:
+        robot.tick()
+        if robot.polling:
+            robot.inputs.append(panels[position] if position in panels else 0)
+        elif robot.outputing:
+            if waitingForColor:
+                waitingForColor = False
+                panels[position] = robot.getOutput()
+            else:
+                waitingForColor = True
+                heading *= DIRECTION_CHANGES[robot.getOutput()]
+                position += heading
+    return panels
+
+
+def part1(memory: List[int]) -> int:
+    panels = runProgram(memory, { 0: 0 })
+    return len(panels)
+
+
+def getDimensions(points: Iterable[complex]) -> Tuple[Tuple[int,int],int,int,int,int]:
+    minX = int(min(map(lambda point: point.real, points)))
+    maxX = int(max(map(lambda point: point.real, points)))
+    minY = int(min(map(lambda point: point.imag, points)))
+    maxY = int(max(map(lambda point: point.imag, points)))
+    size = abs(maxX - minX + 1), abs(maxY - minY + 1)
+    return size, minX, maxX, minY, maxY
+
+
+def printPoints(panels: List[complex]):
+    _, minX, maxX, minY, maxY = getDimensions(panels)
+    for y in range(minY, maxY + 1):
+        for x in range(minX, maxX + 1):
+            point = x + y * 1j
+            if point in panels:
+                print("#", end="")
+            else:
+                print(".", end="")
+        print()
+
+
+def getCharacterInScreen(screen: List[complex], index: int, width: int, height: int, xOffset: int, yOffset: int) -> str:
+    screenValue = sum(2**(width - 1 - x) << (y * width) \
+        for y, x in product(range(height), range(width)) \
+        if (width * index + x + xOffset ) + (y + yOffset) * 1j in screen)
+    if screenValue in LETTERS:
+        return LETTERS[screenValue]
+    return ""
+
+
+def part2(memory: List[int]):
+    panels = runProgram(memory, { 0: 1 })
+    panelPoints = [ point.real - point.imag * 1j for point, value in panels.items() if value ]
+    (width, height), minX, _, minY, _ = getDimensions(panelPoints)
+    return "".join(map(lambda index: getCharacterInScreen(panelPoints, index, CHARACTER_WIDTH, height, minX, minY), range((width // CHARACTER_WIDTH) + 1)))
+
+
+def getInput(filePath: str) -> List[int]:
+    if not os.path.isfile(filePath):
+        raise FileNotFoundError(filePath)
+    
+    with open(filePath, "r") as file:
+        return [ int(i) for i in file.read().split(",") ]
+
+
+def main():
+    if len(sys.argv) != 2:
+        raise Exception("Please, add input file path as parameter")
+
+    puzzleInput = getInput(sys.argv[1])
+    start = time.perf_counter()
+    part1Result = part1(puzzleInput)
+    middle = time.perf_counter()
+    part2Result = part2(puzzleInput)
+    end = time.perf_counter()
+    print("P1:", part1Result)
+    print("P2:", part2Result)
+    print()
+    print(f"P1 time: {middle - start:.8f}")
+    print(f"P2 time: {end - middle:.8f}")
+
+
+if __name__ == "__main__":
+    main()
