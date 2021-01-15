3a48566ab5f70fb695126bb2d9ee8e7d61fe0cef
Bruno Bernardo <bigsby@live.com>
2021-01-15 13:28:56 +0000

    2016/14

---
+++ 2016/day_14/py/run.py
@@ -0,0 +1,70 @@
+#! /usr/bin/python3
+
+import sys, os, time
+from hashlib import md5
+import re
+
+
+tripletRegex = re.compile(r"(.)\1{2}")
+quintetRegex = re.compile(r"(.)\1{4}")
+def findKey(salt: str, stretch: int) -> int:
+    index = 0
+    keys = []
+    threes = { digit: [] for digit in "0123456789abcdef" }
+    while len(keys) < 64:
+        value = salt + str(index)
+        for _ in range(stretch + 1):
+            value = md5(value.encode()).hexdigest()
+
+        match = quintetRegex.search(value)
+        if match:
+            digit = match.group()[0]
+            for tripletIndex in threes[digit]:
+                if (index - tripletIndex) <= 1000:
+                    keys.append(tripletIndex)
+            threes[digit] = []
+
+        match = tripletRegex.search(value)
+        if match:
+            threes[match.group()[0]].append(index)
+        index += 1
+
+    keys.sort()
+    return keys[63]
+
+
+def part1(salt: str):
+    return findKey(salt, 0)
+
+
+def part2(salt: str):
+    return findKey(salt, 2016)
+
+
+def getInput(filePath: str):
+    if not os.path.isfile(filePath):
+        raise FileNotFoundError(filePath)
+    
+    with open(filePath, "r") as file:
+        return file.read().strip()
+
+
+def main():
+    if len(sys.argv) != 2:
+        raise Exception("Please, add input file path as parameter")
+
+    puzzleInput = getInput(sys.argv[1])
+    start = time.perf_counter()
+    part1Result = part1(puzzleInput)
+    middle = time.perf_counter()
+    part2Result = part2(puzzleInput)
+    end = time.perf_counter()
+    print("P1:", part1Result)
+    print("P2:", part2Result)
+    print()
+    print(f"P1 time: {middle - start:.8f}")
+    print(f"P2 time: {end - middle:.8f}")
+
+
+if __name__ == "__main__":
+    main()
