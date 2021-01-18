b936d4d1cdfe5fb8505de0de2df964e51b98d584
Bruno Bernardo <bigsby@live.com>
2021-01-18 23:11:11 +0000

    2016/16

---
+++ 2016/day_16/py/run.py
@@ -0,0 +1,51 @@
+#! /usr/bin/python3
+
+import sys, os, time
+
+
+def getChecksum(data: str, diskLength: int) -> str:
+    while len(data) < diskLength:
+        b = data[::-1]
+        b = [ "1" if c == "0" else "0" for c in b ]
+        data = "0".join([ data, "".join(b) ])
+    data = data[:diskLength]
+    while len(data) % 2 == 0:
+        data = "".join([ "1" if data[index] == data[index + 1] else "0" for index in range(0, len(data), 2) ])
+    return data
+
+
+def part1(data: str) -> str:
+    return getChecksum(data, 272)
+
+
+def part2(data: str) -> str:
+    return getChecksum(data, 35651584)
+
+
+def getInput(filePath: str) -> str:
+    if not os.path.isfile(filePath):
+        raise FileNotFoundError(filePath)
+    
+    with open(filePath, "r") as file:
+        return file.read().strip()
+
+
+def main():
+    if len(sys.argv) != 2:
+        raise Exception("Please, add input file path as parameter")
+
+    puzzleInput = getInput(sys.argv[1])
+    start = time.perf_counter()
+    part1Result = part1(puzzleInput)
+    middle = time.perf_counter()
+    part2Result = part2(puzzleInput)
+    end = time.perf_counter()
+    print("P1:", part1Result)
+    print("P2:", part2Result)
+    print()
+    print(f"P1 time: {middle - start:.8f}")
+    print(f"P2 time: {end - middle:.8f}")
+
+
+if __name__ == "__main__":
+    main()
