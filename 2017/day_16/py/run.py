0102dc65fe830f3008dc77235087171a050887d6
Bruno Bernardo <bigsby@live.com>
2021-01-19 16:41:01 +0000

    2018/16

---
+++ 2017/day_16/py/run.py
@@ -0,0 +1,91 @@
+#! /usr/bin/python3
+
+import sys, os, time
+from typing import List, Tuple
+
+Instruction = Tuple[int,int,int]
+
+
+def toOrd(programs: str) -> List[int]:
+    return [ ord(c) for c in programs ]
+
+
+def toStr(programs: List[int]) -> str:
+    return "".join([ chr(c) for c in programs ])
+
+
+def dance(instructions: List[Instruction], programs: List[int]) -> List[int]:
+    for instruction in instructions:
+        move, a, b = instruction
+        if move == 0:
+            programs = programs[-a:] + programs[:-a]
+        elif move == 1:
+            oldA = programs[a]
+            programs[a] = programs[b]
+            programs[b] = oldA
+        elif move == 2:
+            aIndex = programs.index(a)
+            bIndex = programs.index(b)
+            oldA = programs[aIndex]
+            programs[aIndex] = programs[bIndex]
+            programs[bIndex] = oldA
+    return programs
+
+
+def part1(instructions: List[Instruction]) -> str:
+    return "".join(toStr(dance(instructions, toOrd("abcdefghijklmnop"))))
+
+
+CYCLES = 10**9
+def part2(instructions: List[Instruction]) -> str:    
+    programs = toOrd("abcdefghijklmnop")
+    seen = [ tuple(programs) ]
+    
+    for cycle in range(CYCLES):
+        programs = dance(instructions, programs)
+        p = tuple(programs)
+        if p in seen:
+            return toStr(list(seen[CYCLES % (cycle + 1)]))
+        seen.append(p)
+    return toStr(programs)
+
+
+def parseInstruction(text: str) -> Instruction:
+    if text.startswith("s"):
+        return (0, int(text[1:]), 0)
+    elif text.startswith("x"):
+        a, b = text[1:].split("/")
+        return (1, int(a), int(b))
+    elif text.startswith("p"):
+        a, b = text[1:].split("/")
+        return (2, ord(a), ord(b))
+    raise Exception("Unknow instruction", text)
+
+
+def getInput(filePath: str) -> List[Instruction]:
+    if not os.path.isfile(filePath):
+        raise FileNotFoundError(filePath)
+
+    with open(filePath, "r") as file:
+        return [ parseInstruction(text) for text in file.read().strip().split(",") ]
+
+
+def main():
+    if len(sys.argv) != 2:
+        raise Exception("Please, add input file path as parameter")
+
+    puzzleInput = getInput(sys.argv[1])
+    start = time.perf_counter()
+    part1Result = part1(puzzleInput)
+    middle = time.perf_counter()
+    part2Result = part2(puzzleInput)
+    end = time.perf_counter()
+    print("P1:", part1Result)
+    print("P2:", part2Result)
+    print()
+    print(f"P1 time: {middle - start:.8f}")
+    print(f"P2 time: {end - middle:.8f}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2018/day_16/py/run.py
@@ -0,0 +1,134 @@
+#! /usr/bin/python3
+
+import sys, os, time
+import re
+from typing import Dict, List, Set, Tuple
+
+Registers = Tuple[int,int,int,int]
+Operation = Tuple[int,int,int,int]
+Record = Tuple[Registers,Operation,Registers]
+MNEMONICS = [ 
+    "addr", "addi", 
+    "mulr", "muli", 
+    "banr", "bani", 
+    "borr", "bori", 
+    "setr", "seti", 
+    "gtir", "gtri", "gtrr", 
+    "eqir", "eqri", "eqrr"
+]
+
+
+def runOperation(registers: Registers, operation: Operation, mnemonic: str) -> Registers:
+    _, A, B, C = operation
+    result = list(registers)
+    value = -1
+    if   mnemonic == "addr": value = registers[A] + registers[B]
+    elif mnemonic == "addi": value = registers[A] + B
+    elif mnemonic == "mulr": value = registers[A] * registers[B]
+    elif mnemonic == "muli": value = registers[A] * B
+    elif mnemonic == "banr": value = registers[A] & registers[B]
+    elif mnemonic == "bani": value = registers[A] & B
+    elif mnemonic == "borr": value = registers[A] | registers[B]
+    elif mnemonic == "bori": value = registers[A] | B
+    elif mnemonic == "setr": value = registers[A]
+    elif mnemonic == "seti": value = A
+    elif mnemonic == "gtir": value = 1 if A > registers[B] else 0
+    elif mnemonic == "gtri": value = 1 if registers[A] > B else 0
+    elif mnemonic == "gtrr": value = 1 if registers[A] > registers[B] else 0
+    elif mnemonic == "eqir": value = 1 if A == registers[B] else 0
+    elif mnemonic == "eqri": value = 1 if registers[A] == B else 0
+    elif mnemonic == "eqrr": value = 1 if registers[A] == registers[B] else 0 
+    result[C] = value
+    return tuple(result)
+
+
+def testRecord(before: Registers, operation: Operation, after: Registers, opCodes: Dict[str,Set[int]]) -> int:
+    count = 0
+    opCode, *_ = operation
+    for mnenomic in MNEMONICS:
+        if after == runOperation(before, operation, mnenomic):
+            if -opCode not in opCodes[mnenomic]:
+                opCodes[mnenomic].add(opCode)
+            count += 1
+        else:
+            if opCode in opCodes[mnenomic]:
+                opCodes[mnenomic].remove(opCode)
+                opCodes[mnenomic].add(-opCode)
+    return count
+
+
+def part1(puzzleInput: Tuple[List[Record],List[Operation]]) -> int:
+    records, _ = puzzleInput
+    threeOrMore = 0
+    opCodes = { mnemonic: set() for mnemonic in MNEMONICS }
+    for before, operation, after in records:
+        if testRecord(before, operation, after, opCodes) >= 3:
+            threeOrMore += 1
+    return threeOrMore
+
+
+def part2(puzzleInput: Tuple[List[Record],List[Operation]]) -> int:
+    records, program = puzzleInput
+    opCodes = { mnemonic: set() for mnemonic in MNEMONICS }
+    for before, operation, after in records:
+        testRecord(before, operation, after, opCodes)
+    
+    for mnemonic, valid in opCodes.items():
+        opCodes[mnemonic] = { op for op in valid if op >= 0 }
+
+    while any(len(valid) > 1 for valid in opCodes.values()):
+        singleValid = [ next(iter(valid)) for valid in opCodes.values() if len(valid) == 1 ]
+        for _, valid in opCodes.items():
+            if len(valid) > 1:
+                for single in singleValid:
+                    if single in valid:
+                        valid.remove(single)
+    ops = { next(iter(valid)): mnemonic for mnemonic, valid in opCodes.items() }
+
+    registers = (0,0,0,0)
+    for op in program:
+        registers = runOperation(registers, op, ops[op[0]])
+    return registers[0]
+
+
+recordRegex = re.compile(r"Before: \[(?P<b0>\d+), (?P<b1>\d+), (?P<b2>\d+), (?P<b3>\d+)]\n(?P<opCode>\d+) (?P<A>\d) (?P<B>\d) (?P<C>\d)\nAfter:  \[(?P<a0>\d+), (?P<a1>\d+), (?P<a2>\d+), (?P<a3>\d+)]")
+operationRegex = re.compile(r"(?P<opCode>\d+) (?P<A>\d) (?P<B>\d) (?P<C>\d)")
+def getInput(filePath: str) -> Tuple[List[Record],List[Operation]]:
+    if not os.path.isfile(filePath):
+        raise FileNotFoundError(filePath)
+    
+    with open(filePath, "r") as file:
+        recordsText, programText = file.read().split("\n\n\n\n")
+        records = []
+        for match in recordRegex.finditer(recordsText):
+            records.append(( \
+                (int(match.group("b0")), int(match.group("b1")), int(match.group("b2")), int(match.group("b3"))), \
+                (int(match.group("opCode")), int(match.group("A")), int(match.group("B")), int(match.group("C"))), \
+                (int(match.group("a0")), int(match.group("a1")), int(match.group("a2")), int(match.group("a3"))) \
+                ))
+        operations = []
+        for match in operationRegex.finditer(programText):
+            operations.append((int(match.group("opCode")), int(match.group("A")), int(match.group("B")), int(match.group("C"))))
+        return records, operations
+
+
+
+def main():
+    if len(sys.argv) != 2:
+        raise Exception("Please, add input file path as parameter")
+
+    puzzleInput = getInput(sys.argv[1])
+    start = time.perf_counter()
+    part1Result = part1(puzzleInput)
+    middle = time.perf_counter()
+    part2Result = part2(puzzleInput)
+    end = time.perf_counter()
+    print("P1:", part1Result)
+    print("P2:", part2Result)
+    print()
+    print(f"P1 time: {middle - start:.8f}")
+    print(f"P2 time: {end - middle:.8f}")
+
+
+if __name__ == "__main__":
+    main()
