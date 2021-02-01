b6f2985192ff1c11ea21efeff8460d116767d17d
Bruno Bernardo <bigsby@live.com>
2021-02-01 23:10:43 +0000

    2016/01 py clean up

--- 2016/day_01/py/run.py
+++ 2016/day_01/py/run.py
@@ -74,8 +74,8 @@ def main():
     print("P1:", part1Result)
     print("P2:", part2Result)
     print()
-    print(f"P1 time: {middle - start:.8f}")
-    print(f"P2 time: {end - middle:.8f}")
+    print(f"P1 time: {middle - start:.7f}")
+    print(f"P2 time: {end - middle:.7f}")
 
 
 if __name__ == "__main__":
