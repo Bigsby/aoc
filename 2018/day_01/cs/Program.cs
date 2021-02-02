6714c600dd89f4794013f64eb99c78f0916cdb23
Bruno Bernardo <bigsby@live.com>
2021-02-02 00:12:07 +0000

    2018/01 cs

---
+++ 2018/day_01/cs/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AoC
+{
+    class Program
+    {
+        static int Part1(int[] changes)
+        {
+            return changes.Sum();
+        }
+
+        static int Part2(int[] changes)
+        {
+            var changesLength = changes.Count();
+            var frequency = 0;
+            var previous = new HashSet<int>();
+            var index = 0;
+            while (!previous.Contains(frequency))
+            {
+                previous.Add(frequency);
+                frequency += changes[index];
+                index = (index + 1) % changesLength;
+            }
+            return frequency;
+        }
+
+        static int[] GetInput(string filePath)
+        {
+            if (!File.Exists(filePath)) throw new FileNotFoundException(filePath);
+            return File.ReadAllLines(filePath).Select(int.Parse).ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1) throw new Exception("Please, add input file path as parameter");
+
+            var puzzleInput = GetInput(args[0]);
+            var watch = Stopwatch.StartNew();
+            var part1Result = Part1(puzzleInput);
+            watch.Stop();
+            var middle = watch.ElapsedTicks;
+            watch = Stopwatch.StartNew();
+            var part2Result = Part2(puzzleInput);
+            watch.Stop();
+            WriteLine($"P1: {part1Result}");
+            WriteLine($"P2: {part2Result}");
+            WriteLine();
+            WriteLine($"P1 time: {(double)middle / 100 / TimeSpan.TicksPerSecond:f7}");
+            WriteLine($"P2 time: {(double)watch.ElapsedTicks / 100 / TimeSpan.TicksPerSecond:f7}");
+        }
+    }
+}
