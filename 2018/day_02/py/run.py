1cbd38618e9c18f1049a2e8fc21f25b364f87479
Bruno Bernardo <bigsby@live.com>
2021-02-02 15:32:54 +0000

    2018/02 clean up

--- 2018/day_02/py/run.py
+++ 2018/day_02/py/run.py
@@ -8,24 +8,20 @@ from itertools import combinations
 def part1(ids: List[str]) -> int:
     twiceCount = 0
     thriceCount = 0
-
     for id in ids:
         idCounts = { id.count(c) for c in id }
         twiceCount += 2 in idCounts
         thriceCount += 3 in idCounts
-
     return twiceCount * thriceCount
     
 
 def part2(ids: List[str]) -> str:
-    result = ""
     for id1, id2 in combinations(ids, 2):
         differences = [ i for i in range(len(id1)) if id1[i] != id2[i] ]
         if len(differences) == 1:
             diferentIndex = differences[0]
-            result = id1[:diferentIndex] + id1[diferentIndex + 1:]
-
-    return result
+            return id1[:diferentIndex] + id1[diferentIndex + 1:]
+    raise Exception("Ids differencing 1 not found")
 
 
 def getInput(filePath: str) -> List[str]:
@@ -49,8 +45,8 @@ def main():
     print("P1:", part1Result)
     print("P2:", part2Result)
     print()
-    print(f"P1 time: {middle - start:.8f}")
-    print(f"P2 time: {end - middle:.8f}")
+    print(f"P1 time: {middle - start:.7f}")
+    print(f"P2 time: {end - middle:.7f}")
 
 
 if __name__ == "__main__":
