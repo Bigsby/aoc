4aa978dd5c10b08ed2fbfaaf77969c7036a0cbdf
Bruno Bernardo <bigsby@live.com>
2021-02-02 16:32:01 +0000

    2019/02 cs

---
+++ 2019/day_02/cs/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+
+namespace AoC
+{
+    class IntCodeComputer
+    {   
+        public bool Running { get; private set; } = true;
+
+        public IntCodeComputer(int[] memory) => _memory = memory.ToArray();
+
+        public int Run()
+        {
+            while (Running) Tick();
+            return _memory[0];
+        }
+
+        public void Tick()
+        {            
+            if (!Running) return;
+
+            var opCode = _memory[_pointer];
+            switch (opCode)
+            {
+                case 1: // ADD
+                    _memory[GetAddress(3)] = GetParameter(1) + GetParameter(2);
+                    _pointer += 4;
+                    break;
+                case 2: // MUL
+                    _memory[GetAddress(3)] = GetParameter(1) * GetParameter(2);
+                    _pointer += 4;
+                    break;
+                case 99: // HATL
+                    Running = false;
+                    break;
+                default:
+                    throw new Exception($"Unknown instruction {_pointer} {opCode}");
+            }
+        }
+
+        private int[] _memory;
+        private int _pointer;
+        private int GetAddress(int offset) => _memory[_pointer + offset];
+        private int GetParameter(int offset) => _memory[_memory[_pointer + offset]];
+    }
+
+    class Program
+    {
+        static int RunProgram(int[] memory, int noun, int verb)
+        {
+            memory[1] = noun;
+            memory[2] = verb;
+            return new IntCodeComputer(memory).Run();
+        }
+
+        static int Part1(int[] memory)
+        {
+            return RunProgram(memory, 12, 2);
+        }
+
+        static int TARGET_VALUE = 19690720;
+        static int Part2(int[] memory)
+        {
+            var range = Enumerable.Range(0, 100);
+            foreach (var noun in range)
+            {
+                foreach (var verb in range)
+                {
+                    if (RunProgram(memory, noun, verb) == TARGET_VALUE)
+                    {
+                        return 100 * noun + verb;
+                    }
+                }
+            }
+            throw new Exception("Target value not found");
+        }
+
+        static int[] GetInput(string filePath)
+        {
+            if (!File.Exists(filePath)) throw new FileNotFoundException(filePath);
+            return File.ReadAllText(filePath).Trim().Split(",").Select(int.Parse).ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1) throw new Exception("Please, add input file path as parameter");
+
+            var puzzleInput = GetInput(args[0]);
+            var watch = Stopwatch.StartNew();
+            var part1Result = Part1(puzzleInput);
+            watch.Stop();
+            var middle = watch.ElapsedTicks;
+            watch = Stopwatch.StartNew();
+            var part2Result = Part2(puzzleInput);
+            watch.Stop();
+            WriteLine($"P1: {part1Result}");
+            WriteLine($"P2: {part2Result}");
+            WriteLine();
+            WriteLine($"P1 time: {(double)middle / 100 / TimeSpan.TicksPerSecond:f7}");
+            WriteLine($"P2 time: {(double)watch.ElapsedTicks / 100 / TimeSpan.TicksPerSecond:f7}");
+        }
+    }
+}
