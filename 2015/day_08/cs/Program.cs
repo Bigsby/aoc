67ff45f39299af92ede78f0ce9f2eab85f9a2a81
Bruno Bernardo <bigsby@live.com>
2021-02-05 16:00:13 +0000

    2015/08 cs

---
+++ 2015/day_08/cs/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace AoC
+{
+    class Program
+    {
+        static int CountDifferences(IEnumerable<string> puzzleInput, Func<string, int> differencesFunc)
+            => puzzleInput.Sum(differencesFunc);
+
+        static Regex hexaRegex = new Regex(@"\\x[0-9a-f]{2}", RegexOptions.Compiled);
+        static int GetStringDifference1(string str)
+        {
+            var initialLength = str.Length;
+            var stripped = str.Replace(@"\\", "r");
+            stripped = stripped.Replace("\\\"", "r");
+            stripped = hexaRegex.Replace(stripped, "r");
+            stripped = stripped.Trim('"');
+            return initialLength - stripped.Length;
+        }
+
+        static int Part1(IEnumerable<string> puzzleInput) => CountDifferences(puzzleInput, GetStringDifference1);
+
+        static int GetStringDifference2(string str)
+        {
+            var initialLength = str.Length;
+            var escaped = Regex.Escape(str);
+            escaped = escaped.Replace("\"", "\\\"");
+            return 2 + escaped.Length - initialLength;
+        }
+        static int Part2(IEnumerable<string> puzzleInput) => CountDifferences(puzzleInput, GetStringDifference2);
+
+        static IEnumerable<string> GetInput(string filePath)
+        {
+            if (!File.Exists(filePath)) throw new FileNotFoundException(filePath);
+            return File.ReadAllLines(filePath).Select(line => line.Trim());
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1) throw new Exception("Please, add input file path as parameter");
+
+            var puzzleInput = GetInput(args[0]);
+            var watch = Stopwatch.StartNew();
+            var part1Result = Part1(puzzleInput);
+            watch.Stop();
+            var middle = watch.ElapsedTicks;
+            watch = Stopwatch.StartNew();
+            var part2Result = Part2(puzzleInput);
+            watch.Stop();
+            WriteLine($"P1: {part1Result}");
+            WriteLine($"P2: {part2Result}");
+            WriteLine();
+            WriteLine($"P1 time: {(double)middle / 100 / TimeSpan.TicksPerSecond:f7}");
+            WriteLine($"P2 time: {(double)watch.ElapsedTicks / 100 / TimeSpan.TicksPerSecond:f7}");
+        }
+    }
+}
