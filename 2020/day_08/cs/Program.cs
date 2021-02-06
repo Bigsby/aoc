02c42f1f0c91c07dc2de566ee0637227d39f5db9
Bruno Bernardo <bigsby@live.com>
2021-02-06 02:08:35 +0000

    2020/08 cs

---
+++ 2020/day_08/cs/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace AoC
+{
+    record Instruction(string mnemonic, int argument);
+
+    class Program
+    {
+        const string JMP = "jmp";
+        const string NOP = "nop";
+        const string ACC = "acc";
+
+        static (int accumulator, int instructionPointer) RunInstruction(Instruction instruction, int accumulator, int instructionPointer)
+        {
+            var (mnemonic, argument) = instruction;
+            instructionPointer += mnemonic == JMP ? argument : 1;
+            if (mnemonic == ACC)
+                accumulator += argument;
+            return (accumulator, instructionPointer);
+        }
+
+        static (bool success, int accumulator) RunBoot(IEnumerable<Instruction> boot)
+        {
+            var bootArray = boot.ToArray();
+            var accumulator = 0;
+            var instructionPointer = 0;
+            var visited = new List<int>();
+            var bootLength = boot.Count();
+            while (true)
+            {
+                visited.Add(instructionPointer);
+                (accumulator, instructionPointer) = RunInstruction(bootArray[instructionPointer], accumulator, instructionPointer);
+                if (visited.Contains(instructionPointer))
+                    return (false, accumulator);
+                if (instructionPointer == bootLength)
+                    return (true, accumulator);
+            }
+        }
+
+        static int Part1(IEnumerable<Instruction> boot) => RunBoot(boot).accumulator;
+
+        static (bool success, int accumulator) SwitchAndTest(int index, IEnumerable<Instruction> boot)
+        {
+            var bootArray = boot.ToArray();
+            var (mnemonic, argument) = bootArray[index];
+            bootArray[index] = new Instruction(mnemonic == JMP ? NOP : JMP, argument);
+            return RunBoot(bootArray);
+        }
+
+        static int Part2(IEnumerable<Instruction> boot)
+        {
+            foreach (var index in Enumerable.Range(0, boot.Count()))
+            {
+                if (boot.ElementAt(index).mnemonic == ACC)
+                    continue;
+                var (success, accumulator) = SwitchAndTest(index, boot);
+                if (success)
+                    return accumulator;
+            }
+            throw new Exception("Valid boot not found");
+        }
+
+        static Regex lineRegex = new Regex(@"^(nop|acc|jmp)\s\+?(-?\d+)$", RegexOptions.Compiled);
+        static IEnumerable<Instruction> GetInput(string filePath)
+        {
+            if (!File.Exists(filePath)) throw new FileNotFoundException(filePath);
+            return File.ReadAllLines(filePath).Select(line => {
+                var match = lineRegex.Match(line);
+                if (match.Success)
+                    return new Instruction(match.Groups[1].Value, int.Parse(match.Groups[2].Value));
+                throw new Exception($"Bad format '{line}'");
+            });
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1) throw new Exception("Please, add input file path as parameter");
+
+            var puzzleInput = GetInput(args[0]);
+            var watch = Stopwatch.StartNew();
+            var part1Result = Part1(puzzleInput);
+            watch.Stop();
+            var middle = watch.ElapsedTicks;
+            watch = Stopwatch.StartNew();
+            var part2Result = Part2(puzzleInput);
+            watch.Stop();
+            WriteLine($"P1: {part1Result}");
+            WriteLine($"P2: {part2Result}");
+            WriteLine();
+            WriteLine($"P1 time: {(double)middle / 100 / TimeSpan.TicksPerSecond:f7}");
+            WriteLine($"P2 time: {(double)watch.ElapsedTicks / 100 / TimeSpan.TicksPerSecond:f7}");
+        }
+    }
+}
