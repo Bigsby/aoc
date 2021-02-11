9e16cbb9df8c03c464291bf5b9162587dfdf65bb
Bruno Bernardo <bigsby@live.com>
2021-02-11 12:16:01 +0000

    2015/13 py optimize

--- 2015/day_13/py/run.py
+++ 2015/day_13/py/run.py
@@ -2,88 +2,63 @@
 
 import sys, os, time
 import re
-from enum import Enum
 from itertools import permutations
-from typing import Iterable, List, Set, Tuple
+from typing import Dict, Iterable, List, Tuple
+
+Entries = Dict[str,Dict[str,int]]
 
 
-class Balance(Enum):
-    GAIN = 1
-    LOSS = 2
-
-
-class Entry():
-    def __init__(self, target: str, balance: str, value: str, source: str):
-        self.target = target
-        self.source = source
-        self.balance = Balance.GAIN if balance == "gain" else Balance.LOSS
-        self.value = int(value)
-
-
-def getEntryValue(target: str, source: str, entries: List[Entry]) -> int:
-    entry = next(filter(lambda e: e.target == target and e.source == source, entries))
-    return entry.value if entry.balance == Balance.GAIN else -entry.value
-
-
-def calculateHappiness(arrangement: Tuple[str,...], entries: List[Entry]) -> int:
+def calculateHappiness(arrangement: Tuple[str,...], entries: Entries) -> int:
     total = 0
     length = len(arrangement)
     for index, person in enumerate(arrangement):
-        total += getEntryValue(person, arrangement[index - 1], entries)        
-        total += getEntryValue(person, arrangement[index + 1 if index < length - 1 else 0], entries)
+        total += entries[person][arrangement[index - 1]]
+        total += entries[person][arrangement[index + 1 if index < length - 1 else 0]]
     return total
 
 
-def calculateMaximumHappiness(possibleArragements: Iterable[Tuple[str,...]], entries: List[Entry]) -> int:
+def calculateMaximumHappiness(possibleArragements: Iterable[Tuple[str,...]], entries: Entries) -> int:
     return max(map(lambda arrangement: calculateHappiness(arrangement, entries), possibleArragements))
-
-
-def getPeople(entries: List[Entry]) -> List[str]:
-    people: Set[str] = set()
-    for entry in entries:
-        people.add(entry.target)
-        people.add(entry.source)
-    return list(people)
 
 
 def getPossibleArrangements(people: List[str]) -> Iterable[Tuple[str,...]]:
     return permutations(people, len(people))
 
 
-def part1(entries: List[Entry]) -> int:
-    people = getPeople(entries)
-    possibleArragements = getPossibleArrangements(people)
+def part1(entries: Entries) -> int:
+    possibleArragements = getPossibleArrangements(list(entries.keys()))
     result = calculateMaximumHappiness(possibleArragements, entries)
     return result
 
 
-def part2(entries: List[Entry]) -> int:
-    people = getPeople(entries)
+def part2(entries: Entries) -> int:
     me = "me"
-    for person in people:
-        entries.append(Entry(me, "GAIN", "0", person))
-        entries.append(Entry(person, "GAIN", "0", me))
-    people.append(me)
-
-    possibleArragements = getPossibleArrangements(people)
-    result = calculateMaximumHappiness(possibleArragements, entries)
-    return result
+    entries[me] = {}
+    for person in list(entries.keys()):
+        if person == me:
+            continue
+        entries[me][person] = 0
+        entries[person][me] = 0
+    return part1(entries)
 
 
 lineRegex = re.compile(r"^(\w+)\swould\s(gain|lose)\s(\d+)\shappiness\sunits\sby\ssitting\snext\sto\s(\w+)\.$")
-def parseLine(line: str) -> Entry:
-    match = lineRegex.match(line)
-    if match:
-        return Entry(*match.group(1, 2, 3, 4))
-    raise Exception("Bad format", line)
-
-
-def getInput(filePath: str) -> List[Entry]:
+def getInput(filePath: str) -> Entries:
     if not os.path.isfile(filePath):
         raise FileNotFoundError(filePath)
     
     with open(filePath, "r") as file:
-        return [ parseLine(line) for line in file.readlines() ]
+        entries = {}
+        for line in file.readlines():
+            match = lineRegex.match(line)
+            if match:
+                target = match.group(1)
+                if target not in entries:
+                    entries[target] = {}
+                entries[target][match.group(4)] = (1 if match.group(2) == "gain" else -1) * int(match.group(3))
+            else:
+                raise Exception("Bad format", line)
+        return entries
 
 
 def main():
@@ -99,8 +74,8 @@ def main():
     print("P1:", part1Result)
     print("P2:", part2Result)
     print()
-    print(f"P1 time: {middle - start:.8f}")
-    print(f"P2 time: {end - middle:.8f}")
+    print(f"P1 time: {middle - start:.7f}")
+    print(f"P2 time: {end - middle:.7f}")
 
 
 if __name__ == "__main__":
