8ad54b6d8a2f0312ba1a787c8b83dcd53a8bb30e
Bruno Bernardo <bigsby@live.com>
2021-02-11 16:09:13 +0000

    2018/13 cs

---
+++ 2018/day_13/cs/Program.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace AoC
+{
+    using Position = Complex;
+    using Direction = Complex;
+    using Map = Dictionary<Complex, MapItem>;
+
+    enum MapItemType
+    {
+        Straight,
+        Turn,
+        Intersection
+    }
+
+    enum Orientation
+    {
+        Horizontal,
+        Vertical
+    }
+
+    record MapItem(MapItemType type);
+
+    record Straight : MapItem 
+    {
+        public Orientation Orientation { get; }
+        public Straight(Orientation orientation)
+            : base(MapItemType.Straight) => Orientation = orientation;
+    }
+
+    record Turn : MapItem 
+    {
+        public Tuple<Direction, Direction> Directions { get; }
+        public Turn(Tuple<Direction, Direction> directions)
+            : base(MapItemType.Turn) => Directions = directions;
+    }
+    
+    record Intersection : MapItem 
+    {
+        public Intersection() : base(MapItemType.Intersection) { }
+    }
+
+    class Cycle<T>
+    {
+        private IEnumerator<T> _enumerator;
+        public Cycle(IEnumerable<T> source) => _enumerator = source.GetEnumerator();
+        public T Next()
+        {
+            if (!_enumerator.MoveNext())
+            {
+                _enumerator.Reset();
+                _enumerator.MoveNext();
+            }
+            return _enumerator.Current;
+        }
+    }
+
+    class Train
+    {
+        public Position Position { get; private set; }
+        public Direction Direction { get; set; }
+
+        public void Tick() =>  Position += Direction;
+
+        public void Turn() => Direction *= _directionCycle.Next();
+
+        public Train Clone() => new Train(Position, Direction);
+
+        public Train(Position position, Direction direction)
+        {
+            Position = position;
+            Direction = direction;
+        }
+        
+        static Direction[] DIRECTION_CHANGES = new [] { Direction.ImaginaryOne, 1, -Direction.ImaginaryOne };
+        Cycle<Direction> _directionCycle = new Cycle<Direction>(DIRECTION_CHANGES);
+    }
+
+    static class Program
+    {
+        static Dictionary<Direction, char> TRAIN_CHAR = new Dictionary<Position, char>
+        {
+            { Direction.One, '>' },
+            { -Direction.One, '<' },
+            { Direction.ImaginaryOne, '^' },
+            { -Direction.ImaginaryOne, 'v' }
+        };
+        static Dictionary<Tuple<Direction, Direction>, char> TURN_CHAR = new Dictionary<Tuple<Position, Position>, char>
+        {
+            { Tuple.Create( Direction.One,  Direction.ImaginaryOne), '\\' },
+            { Tuple.Create( Direction.One, -Direction.ImaginaryOne), '/'  },
+            { Tuple.Create(-Direction.One,  Direction.ImaginaryOne), '/'  },
+            { Tuple.Create(-Direction.One, -Direction.ImaginaryOne), '\\' },
+        };
+
+        static void ShowMapArea(Map mapItems, Position start, Position end, IEnumerable<Train>  trains)
+        {
+            for (var y = (int)start.Imaginary; y > (int)end.Imaginary - 1; y--)
+            {
+                for (var x = (int)start.Real; x < (int)end.Real + 1; x++)
+                {
+                    var position = new Position(x, y);
+                    var c = ' ';
+                    if (mapItems.ContainsKey(position))
+                        switch(mapItems[position])
+                        {
+                            case Straight straight:
+                                c = straight.Orientation == Orientation.Horizontal ? '-' : '|';
+                                break;
+                            case Intersection intersection:
+                                c = '+';
+                                break;
+                            case Turn turn:
+                                c = TURN_CHAR[turn.Directions];
+                                break;
+                        }
+                    var train = trains.FirstOrDefault(train => train.Position == position);
+                    if (train != null)
+                        c = TRAIN_CHAR[train.Direction];
+                    Write(c);
+                }
+                WriteLine();
+            }
+            WriteLine();
+        }
+
+        static void ShowTrain(Map mapItems, Train train, IEnumerable<Train> trains)
+        {
+            var offset = 40;
+            var maxX = (int)mapItems.Keys.Max(p => p.Real);
+            var maxY = (int)mapItems.Keys.Min(p => p.Imaginary);
+            var startX = Math.Max(0, (int)train.Position.Real - offset);
+            var endX = Math.Min(maxX, (int)train.Position.Real + offset);
+            var startY = Math.Min(0, (int)train.Position.Imaginary + offset);
+            var endY = Math.Max(maxY, (int)train.Position.Imaginary - offset);
+            ShowMapArea(mapItems, new Position(startX, startY), new Position(endX, endY), trains);
+        }
+
+        static string PositionToString(Position position)
+            => $"{(int)position.Real},{(int)Math.Abs(position.Imaginary)}";
+
+        static string Run((Map map,  IEnumerable<Train> trains) data, bool stopOnFirstCollision)
+        {
+            var (mapItems, trains) = data;
+            var trainLocations = trains.ToDictionary(train => train.Position, train => train.Clone());
+            while (true)
+            {
+                foreach (var position in trainLocations.Keys.ToArray().OrderBy(p => -p.Imaginary).ThenBy(p => p.Real))
+                {
+                    if (!trainLocations.ContainsKey(position))
+                        continue;
+                    var train = trainLocations[position];
+                    trainLocations.Remove(position);
+                    train.Tick();
+                    if (trainLocations.ContainsKey(train.Position))
+                    {
+                        if (stopOnFirstCollision)
+                            return PositionToString(train.Position);
+                        trainLocations.Remove(train.Position);
+                    }
+                    else
+                        trainLocations[train.Position] = train;
+                    var mapItem = mapItems[train.Position];
+                    switch(mapItem)
+                    {
+                        case Intersection intersection:
+                            train.Turn();
+                            break;
+                        case Turn turn:
+                            train.Direction = train.Direction.Real != 0 ? turn.Directions.Item2 : turn.Directions.Item1;
+                            break;
+                    }
+                }
+                if (trainLocations.Count == 1)
+                    return PositionToString(trainLocations.Keys.First());
+            }
+        }
+
+        static string Part1((Map map,  IEnumerable<Train> trains) data) => Run(data, true);
+
+        static string Part2((Map map,  IEnumerable<Train> trains) data) => Run(data, false);
+
+        static Dictionary<char, Direction> TRAINS = new Dictionary<char, Position>
+        {
+            { '>',  1 },
+            { '<', -1 },
+            { '^', Direction.ImaginaryOne },
+            { 'v', -Direction.ImaginaryOne }
+        };
+        static char[] TURNS = new [] { '/', '\\' };
+        static Dictionary<string, Tuple<Direction, Direction>> TURN_DIRECTIONS = new Dictionary<string, Tuple<Direction, Direction>>
+        {
+            { " /",  Tuple.Create( Direction.One, -Direction.ImaginaryOne) },
+            { "-/",  Tuple.Create(-Direction.One,  Direction.ImaginaryOne) },
+            { "+/",  Tuple.Create(-Direction.One,  Direction.ImaginaryOne) },
+            { "-\\", Tuple.Create(-Direction.One, -Direction.ImaginaryOne) },
+            { "+\\", Tuple.Create(-Direction.One, -Direction.ImaginaryOne) },
+            { " \\", Tuple.Create( Direction.One,  Direction.ImaginaryOne) }
+        };
+        static Dictionary<char, Orientation> STRAIGHTS = new Dictionary<char, Orientation>
+        {
+            { '-', Orientation.Horizontal },
+            { '|', Orientation.Vertical }
+        };
+        const char INTERSECTION = '+';
+        static char[] TURN_PREVIOUS = new [] { '-', '+' };
+        static (Map map,  IEnumerable<Train> trains) GetInput(string filePath)
+        {
+            if (!File.Exists(filePath)) throw new FileNotFoundException(filePath);        
+            var trains = new List<Train>();
+            var map = new Map();
+            var previousC = ' ';
+            var trainPositionToFillIn = new List<Position>();
+            var trainIndex = 0;
+            foreach (var (line, y) in File.ReadLines(filePath).Select((line, y) => (line, y)))
+                foreach (var (c, x) in line.Select((c, x) => (c, x)))
+                {
+                    var position = new Position(x, -y);
+                    if (c == INTERSECTION)
+                        map[position] = new Intersection();
+                    else if (STRAIGHTS.ContainsKey(c))
+                        map[position] = new Straight(STRAIGHTS[c]);
+                    else if (TURNS.Contains(c))
+                    {
+                        if (!TURN_PREVIOUS.Contains(previousC))
+                            previousC = ' ';
+                        map[position] = new Turn(TURN_DIRECTIONS[new string(new[]{ previousC, c })]);                        
+                    }
+                    else if (TRAINS.ContainsKey(c))
+                    {
+                        trains.Add(new Train(position, TRAINS[c]));
+                        trainIndex++;
+                        trainPositionToFillIn.Add(position);
+                    }
+                    previousC = c;
+                }
+            foreach (var position in trainPositionToFillIn)
+                foreach (var direction in TRAINS.Values)
+                    if (map.ContainsKey(position + direction))
+                    {
+                        var mapPosition = map[position + direction];
+                        if (mapPosition.type == MapItemType.Straight)
+                            map[position] = mapPosition;
+                        else if (mapPosition.type == MapItemType.Intersection)
+                            map[position] = new Straight(direction.Real != 0 ? Orientation.Horizontal : Orientation.Vertical);
+                    }
+            return (map, trains);
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1) throw new Exception("Please, add input file path as parameter");
+
+            var puzzleInput = GetInput(args[0]);
+            var watch = Stopwatch.StartNew();
+            var part1Result = Part1(puzzleInput);
+            watch.Stop();
+            var middle = watch.ElapsedTicks;
+            watch = Stopwatch.StartNew();
+            var part2Result = Part2(puzzleInput);
+            watch.Stop();
+            WriteLine($"P1: {part1Result}");
+            WriteLine($"P2: {part2Result}");
+            WriteLine();
+            WriteLine($"P1 time: {(double)middle / 100 / TimeSpan.TicksPerSecond:f7}");
+            WriteLine($"P2 time: {(double)watch.ElapsedTicks / 100 / TimeSpan.TicksPerSecond:f7}");
+        }
+    }
+}
