26294e4e9573578714ca96043d8eb943d0dab0f7
Bruno Bernardo <bigsby@live.com>
2021-02-18 19:10:55 +0000

    2016/19 py clean up

--- 2016/day_19/py/run.py
+++ 2016/day_19/py/run.py
@@ -2,13 +2,13 @@
 
 import sys, os, time
 
-def part1(elfCount: int):
+def part1(elfCount: int) -> int:
     # 1 + 2 * (elfCount - 2 ** (math.floor(math.log(elfCount, 2))))
     return int(f"{elfCount:b}"[1:] + "1", 2)
 
 
 class Elf:
-    def __init__(self, position: int) -> None:
+    def __init__(self, position: int):
         self.position = position + 1
         self.next = self
         self.previous = self
@@ -18,11 +18,11 @@ class Elf:
         self.next.previous = self.previous
 
 
-def part2(elfCount: int):
+def part2(elfCount: int) -> int:
     elves = list(map(Elf, range(elfCount)))
     for index in range(elfCount):
-        elves[index].next = elves[ (index + 1) % elfCount]
-        elves[index].previous = elves[ (index -1) % elfCount]
+        elves[index].next = elves[(index + 1) % elfCount]
+        elves[index].previous = elves[(index -1 ) % elfCount]
     
     currentElf = elves[0]
     elfToRemove = elves[elfCount // 2]
@@ -57,8 +57,8 @@ def main():
     print("P1:", part1Result)
     print("P2:", part2Result)
     print()
-    print(f"P1 time: {middle - start:.8f}")
-    print(f"P2 time: {end - middle:.8f}")
+    print(f"P1 time: {middle - start:.7f}")
+    print(f"P2 time: {end - middle:.7f}")
 
 
 if __name__ == "__main__":
