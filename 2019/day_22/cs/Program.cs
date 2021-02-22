f04a2269a9df63790dfd48b00940fb8b768a0c67
Bruno Bernardo <bigsby@live.com>
2021-02-22 23:34:58 +0000

    2019/22 cs

---
+++ 2019/day_22/cs/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace AoC
+{
+    static class Program
+    {
+        const int NEW_STACK = 0;
+        const int CUT = 1;
+        const int INCREMENT = 2;
+
+        static IEnumerable<int> DoShuffles(IEnumerable<int> cards, IEnumerable<(int, int)> shuffles)
+        {
+            var cardsArray = cards.ToArray();
+            var cardsCount = cards.Count();
+            var replacement = new int[cardsCount];
+            foreach (var (shuffle, count) in shuffles)
+            {
+                switch (shuffle)
+                {
+                    case NEW_STACK:
+                        cardsArray = cardsArray.Reverse().ToArray();
+                        break;
+                    case CUT:
+                        cardsArray = cardsArray[Range.StartAt(new Index(Math.Abs(count), count < 0))].Concat(cardsArray[Range.EndAt(new Index(Math.Abs(count), count < 0))]).ToArray();
+                        break;
+                    case INCREMENT:
+                        var cardsStack = new Stack<int>(cards);
+                        for (var index = 0; index < cardsCount; index++)
+                            replacement[(index * count) % cardsCount] = cardsArray[index];
+                        cardsArray = replacement.ToArray();
+                        break;
+                }
+            }
+            return cardsArray;
+        }
+
+        const int CARDS1 = 10007;
+        const int POSITION1 = 2019;
+        static int Part1(IEnumerable<(int, int)> shuffles)
+        {
+            var cards = (DoShuffles(Enumerable.Range(0, CARDS1), shuffles));
+            return cards.ToList().IndexOf(POSITION1);
+        }
+
+        static BigInteger InverModulo(BigInteger a, BigInteger n)
+            => BigInteger.ModPow(a, n - 2, n);
+
+        static BigInteger AbsoluteModulo(BigInteger a, BigInteger n) => ((a % n) + n) % n;
+
+        const long CARDS2 = 119315717514047L;
+        const long RUNS = 101741582076661L;
+        const long POSITION2 = 2020;
+
+        static BigInteger Part2(IEnumerable<(int, int)> shuffles)
+        {
+            int la = 0, lb = 0;
+            long a = 1, b = 0;
+            foreach (var (shuffle, count) in shuffles)
+            {
+                switch (shuffle)
+                {
+                    case NEW_STACK:
+                        la = -1;
+                        lb = -1;
+                        break;
+                    case CUT:
+                        la = 1;
+                        lb = -count;
+                        break;
+                    case INCREMENT:
+                        la = count;
+                        lb = 0;
+                        break;
+                }
+                a = (long)AbsoluteModulo(la * a, CARDS2);
+                b = (long)AbsoluteModulo(la * b + lb, CARDS2);
+            }
+            var Ma = System.Numerics.BigInteger.ModPow(a, RUNS, CARDS2);
+            var Mb = AbsoluteModulo((b * (Ma - 1) * InverModulo(a - 1, CARDS2)), CARDS2);
+            return AbsoluteModulo((POSITION2 - Mb) * InverModulo(Ma, CARDS2), CARDS2);
+        }
+
+        static IEnumerable<(int, int)> GetInput(string filePath)
+        {
+            if (!File.Exists(filePath)) throw new FileNotFoundException(filePath);
+            return File.ReadLines(filePath).Select(line =>
+            {
+                if (line.StartsWith("deal into"))
+                    return (NEW_STACK, 0);
+                if (line.StartsWith("cut"))
+                    return (CUT, int.Parse(line.Split(" ")[1]));
+                if (line.StartsWith("deal with"))
+                    return (INCREMENT, int.Parse(line.Split(" ")[^1]));
+                throw new Exception($"Bad format '{line}'");
+            });
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1) throw new Exception("Please, add input file path as parameter");
+
+            var puzzleInput = GetInput(args[0]);
+            var watch = Stopwatch.StartNew();
+            var part1Result = Part1(puzzleInput);
+            watch.Stop();
+            var middle = watch.ElapsedTicks;
+            watch = Stopwatch.StartNew();
+            var part2Result = Part2(puzzleInput);
+            watch.Stop();
+            WriteLine($"P1: {part1Result}");
+            WriteLine($"P2: {part2Result}");
+            WriteLine();
+            WriteLine($"P1 time: {(double)middle / 100 / TimeSpan.TicksPerSecond:f7}");
+            WriteLine($"P2 time: {(double)watch.ElapsedTicks / 100 / TimeSpan.TicksPerSecond:f7}");
+        }
+    }
+}
