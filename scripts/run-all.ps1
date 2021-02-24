c4587588a51f626256d704a0d8b4ecd69c216c57
Bruno Bernardo <bigsby@live.com>
2021-02-24 12:40:01 +0000

    scripts: run-all.ps1

---
+++ scripts/run-all.ps1
@@ -0,0 +1,23 @@
+foreach ($year in 2015..2020) {
+    foreach ($day in 1..25) {
+        $paddedDay = ([string]$day).PadLeft(2,'0')
+        Write-Output "$year/day_$paddedDay py"
+        $sw = [Diagnostics.Stopwatch]::StartNew()
+        $output = python ../$year/day_$paddedDay/py/run.py ../$year/day_$paddedDay/input.txt
+        $sw.Stop()
+        $duration = $sw.Elapsed
+        Write-Output $output
+        Write-Output ""
+        Write-Output "time: $duration"
+        Write-Output "---------------------------------------"
+        Write-Output "$year/day_$paddedDay cs"
+        $sw = [Diagnostics.Stopwatch]::StartNew()
+        $output = dotnet run -p ../$year/day_$paddedDay/cs/run.csproj ../$year/day_$paddedDay/input.txt
+        $sw.Stop()
+        $duration = $sw.Elapsed
+        Write-Output $output
+        Write-Output ""
+        Write-Output "time: $duration"
+        Write-Output "---------------------------------------"
+    }
+}
