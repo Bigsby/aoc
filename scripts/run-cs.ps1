179d312464abcc36f8a46e02b97866f207fe99b6
Bruno Bernardo <bigsby@live.com>
2021-02-24 21:15:58 +0000

    rename days folders

--- scripts/run-cs.ps1
+++ scripts/run-cs.ps1
@@ -1,9 +1,9 @@
 foreach ($year in 2015..2020) {
     foreach ($day in 1..25) {
         $paddedDay = ([string]$day).PadLeft(2,'0')
-        Write-Output "$year/day_$paddedDay cs"
+        Write-Output "$year/$paddedDay cs"
         $sw = [Diagnostics.Stopwatch]::StartNew()
-        $output = dotnet run -p ../$year/day_$paddedDay/cs/run.csproj ../$year/day_$paddedDay/input.txt
+        $output = dotnet run -p ../$year/$paddedDay/cs/run.csproj ../$year/$paddedDay/input.txt
         $sw.Stop()
         $duration = $sw.Elapsed
         Write-Output $output
--- scripts/run-cs.sh
+++ scripts/run-cs.sh
@@ -2,8 +2,8 @@ for year in {2015..2020}
 do
     for day in {1..25}
     do
-        echo "$year/day_`printf %02d $day` cs"
-        { time dotnet run -p ../$year/day_`printf %02d $day`/cs/run.csproj "../$year/day_`printf %02d $day`/input.txt" ; } 2>&1
+        echo "$year/`printf %02d $day` cs"
+        { time dotnet run -p ../$year/`printf %02d $day`/cs/run.csproj "../$year/`printf %02d $day`/input.txt" ; } 2>&1
         echo "---------------------------------------"
     done
 done
