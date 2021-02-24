800c42dea833590afd7e6c48cf3b6479c4b7995f
Bruno Bernardo <bigsby@live.com>
2021-02-24 11:36:28 +0000

    scripts: fix script

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -3,17 +3,10 @@ do
     for day in {1..25}
     do
         echo "$year/day_`printf %02d $day` py"
-        time ../$year/day_`printf %02d $day`/py/run.py "../$year/day_`printf %02d $day`/input.txt"
-        echo -e "---------------------------------------"
-    done
-done
-
-for year in {2015..2020}
-do
-    for day in {1..25}
-    do
+        { time ../$year/day_`printf %02d $day`/py/run.py "../$year/day_`printf %02d $day`/input.txt" ; } 2>&1
+        echo "---------------------------------------"
         echo "$year/day_`printf %02d $day` cs"
-        time dotnet run -p ../$year/day_`printf %02d $day`/cs/run.csproj "../$year/day_`printf %02d $day`/input.txt"
-        echo -e "---------------------------------------"
+        { time dotnet run -p ../$year/day_`printf %02d $day`/cs/run.csproj "../$year/day_`printf %02d $day`/input.txt" ; } 2>&1
+        echo "---------------------------------------"
     done
 done
