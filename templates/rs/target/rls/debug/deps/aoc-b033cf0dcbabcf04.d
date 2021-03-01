65376fedecd043057bc556deba9f9736bb9a2060
Bruno Bernardo <bigsby@live.com>
2021-03-01 12:31:04 +0000

    templates: use cargo for Rust

---
+++ templates/rs/target/rls/debug/deps/aoc-b033cf0dcbabcf04.d
@@ -0,0 +1,5 @@
+/home/bigsby/code/aoc/templates/rs/target/rls/debug/deps/aoc-b033cf0dcbabcf04.rmeta: src/main.rs
+
+/home/bigsby/code/aoc/templates/rs/target/rls/debug/deps/aoc-b033cf0dcbabcf04.d: src/main.rs
+
+src/main.rs:
