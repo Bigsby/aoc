39975007c75c0241c32dc11a1f48c52006139e64
Bruno Bernardo <bigsby@live.com>
2021-03-01 12:32:01 +0000

    templates: remove Rust target

--- templates/rs/target/rls/debug/deps/aoc-1eeb83971e8ddc59.d
+++
@@ -1,5 +0,0 @@
-/home/bigsby/code/aoc/templates/rs/target/rls/debug/deps/aoc-1eeb83971e8ddc59.rmeta: src/main.rs
-
-/home/bigsby/code/aoc/templates/rs/target/rls/debug/deps/aoc-1eeb83971e8ddc59.d: src/main.rs
-
-src/main.rs:
