848fb6b71d32290d873dfb598d795a24fcf62283
Bruno Bernardo <bigsby@live.com>
2021-03-02 17:04:49 +0000

    2015/02 py clean up

--- 2015/02/py/run.py
+++ 2015/02/py/run.py
@@ -6,43 +6,43 @@ import re
 
 
 def part1(dimensions: List[Tuple[int,int,int]]) -> int:
-    totalPaper = 0
+    total_paper = 0
     for w, l, h in dimensions:
         wl = w * l
         wh = w * h
         hl = h * l
         smallest = min(wl, wh, hl)
-        totalPaper += 2 * (wl + wh + hl) + smallest
-    return totalPaper
+        total_paper += 2 * (wl + wh + hl) + smallest
+    return total_paper
 
 
 def part2(dimensions: List[Tuple[int,int,int]]) -> int:
-    totalRibbon = 0
+    total_ribbon = 0
     for w, l, h in dimensions:
-        sidesList = [w, l, h]
-        sidesList.remove(max(sidesList))
-        totalRibbon += 2 * (sidesList[0] + sidesList[1]) + w * l * h
-    return totalRibbon
+        sides_list = [w, l, h]
+        sides_list.sort()
+        total_ribbon += 2 * (sides_list[0] + sides_list[1]) + w * l * h
+    return total_ribbon
 
 
 def solve(dimensions: List[Tuple[int,int,int]]) -> Tuple[int,int]:
     return (part1(dimensions), part2(dimensions))
 
 
-lineRegex = re.compile(r"^(\d+)x(\d+)x(\d+)$")
-def parseLine(line:str) -> Tuple[int,int,int]:
-    match = lineRegex.match(line)
+line_regex = re.compile(r"^(\d+)x(\d+)x(\d+)$")
+def parse_line(line:str) -> Tuple[int,int,int]:
+    match = line_regex.match(line)
     if match:
         return (int(match.group(1)), int(match.group(2)), int(match.group(3)))
     raise Exception("Bad format", line)
 
 
-def getInput(filePath: str) -> List[Tuple[int,int,int]]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
+def get_input(file_path: str) -> List[Tuple[int,int,int]]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
     
-    with open(filePath, "r") as file:
-        return [ parseLine(line) for line in file.readlines() ]
+    with open(file_path, "r") as file:
+        return [ parse_line(line) for line in file.readlines() ]
 
 
 def main():
@@ -50,10 +50,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
