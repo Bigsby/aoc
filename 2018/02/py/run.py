b282dfa830bb3fd2683b3e27e8755494276f0cec
Bruno Bernardo <bigsby@live.com>
2021-03-02 23:03:59 +0000

    2018/02 py clean up

--- 2018/02/py/run.py
+++ 2018/02/py/run.py
@@ -6,21 +6,21 @@ from itertools import combinations
 
 
 def part1(ids: List[str]) -> int:
-    twiceCount = 0
-    thriceCount = 0
+    twice_count = 0
+    thrice_count = 0
     for id in ids:
-        idCounts = { id.count(c) for c in id }
-        twiceCount += 2 in idCounts
-        thriceCount += 3 in idCounts
-    return twiceCount * thriceCount
+        id_counts = { id.count(c) for c in id }
+        twice_count += 2 in id_counts
+        thrice_count += 3 in id_counts
+    return twice_count * thrice_count
     
 
 def part2(ids: List[str]) -> str:
     for id1, id2 in combinations(ids, 2):
         differences = [ i for i in range(len(id1)) if id1[i] != id2[i] ]
         if len(differences) == 1:
-            diferentIndex = differences[0]
-            return id1[:diferentIndex] + id1[diferentIndex + 1:]
+            diferent_index = differences[0]
+            return id1[:diferent_index] + id1[diferent_index + 1:]
     raise Exception("Ids differencing 1 not found")
 
 
@@ -31,11 +31,11 @@ def solve(ids: List[str]) -> Tuple[int,str]:
     )
 
 
-def getInput(filePath: str) -> List[str]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
+def get_input(file_path: str) -> List[str]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
     
-    with open(filePath, "r") as file:
+    with open(file_path, "r") as file:
         return [ line.strip() for line in file.readlines() ]
 
 
@@ -44,10 +44,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
