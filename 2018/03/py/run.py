c47b0891f5045c31ff30c3f6865439ec113127b5
Bruno Bernardo <bigsby@live.com>
2021-03-03 16:52:23 +0000

    2018/03 py clean up

--- 2018/03/py/run.py
+++ 2018/03/py/run.py
@@ -8,35 +8,35 @@ from itertools import product
 Claim = Tuple[int,int,int,int,int]
 
 
-def getCoveredPoints(claims: List[Claim]) -> Dict[Tuple[int,int],int]:
-    coveredPoints: Dict[Tuple[int,int],int] = {}
+def get_covered_points(claims: List[Claim]) -> Dict[Tuple[int,int],int]:
+    covered_points: Dict[Tuple[int,int],int] = {}
     for _, left, top, width, height in claims:
         for point in product(range(left, left + width), range(top, top + height)):
-            if point in coveredPoints:
-                coveredPoints[point] += 1
+            if point in covered_points:
+                covered_points[point] += 1
             else:
-                coveredPoints[point] = 1
-    return coveredPoints
+                covered_points[point] = 1
+    return covered_points
 
 
-def part2(claims: List[Claim], coveredPoints: Dict[Tuple[int,int],int]) -> int:
+def part2(claims: List[Claim], covered_points: Dict[Tuple[int,int],int]) -> int:
     for id, left, top, width, height in claims:
-        if all(coveredPoints[point] == 1 for point in product(range(left, left + width), range(top, top + height))):
+        if all(covered_points[point] == 1 for point in product(range(left, left + width), range(top, top + height))):
             return id
     raise Exception("Claim not found")
 
 
 def solve(claims: List[Claim]) -> Tuple[int,int]:
-    coveredPoints = getCoveredPoints(claims)
+    covered_points = get_covered_points(claims)
     return (
-        sum([ 1 for value in coveredPoints.values() if value > 1 ]),
-        part2(claims, coveredPoints)
+        sum([ 1 for value in covered_points.values() if value > 1 ]),
+        part2(claims, covered_points)
     )
 
 
-lineRegex = re.compile(r"^#(?P<id>\d+)\s@\s(?P<left>\d+),(?P<top>\d+):\s(?P<width>\d+)x(?P<height>\d+)$")
-def parseLine(line: str) -> Claim:
-    match = lineRegex.match(line)
+line_regex = re.compile(r"^#(?P<id>\d+)\s@\s(?P<left>\d+),(?P<top>\d+):\s(?P<width>\d+)x(?P<height>\d+)$")
+def parse_line(line: str) -> Claim:
+    match = line_regex.match(line)
     if match:
         return (
             int(match.group("id")),
@@ -48,12 +48,12 @@ def parseLine(line: str) -> Claim:
     raise Exception("Bad format", line)
     
 
-def getInput(filePath: str) -> List[Claim]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
+def get_input(file_path: str) -> List[Claim]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
     
-    with open(filePath, "r") as file:
-        return [ parseLine(line) for line in file.readlines() ]
+    with open(file_path, "r") as file:
+        return [ parse_line(line) for line in file.readlines() ]
 
 
 def main():
@@ -61,10 +61,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
