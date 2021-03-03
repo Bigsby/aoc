5e3db0f9b57f000bc4ae5da27aa6fd7cf8ae02c4
Bruno Bernardo <bigsby@live.com>
2021-03-03 14:36:16 +0000

    2015/03 py clean up

--- 2015/03/py/run.py
+++ 2015/03/py/run.py
@@ -1,37 +1,34 @@
 #! /usr/bin/python3
 
 import sys, os, time
-from typing import Dict, List, Tuple
+from typing import List, Set, Tuple
 
 
-def processDirection(visitedHouses: Dict[complex,int], currentPosition: complex, direction: complex):
-    currentPosition += direction
-    if currentPosition in visitedHouses:
-        visitedHouses[currentPosition] += 1
-    else:
-        visitedHouses[currentPosition] = 1
-    return currentPosition
+def process_direction(visited_houses: Set[complex], position: complex, direction: complex):
+    position += direction
+    visited_houses.add(position)
+    return position
 
 
 def part1(directions: List[complex]):
-    visitedHouses = {} 
-    visitedHouses[0] = 1
-    currentPosition = 0
+    visited_houses = set()
+    visited_houses.add(0)
+    position = 0
     for direction in directions:
-        currentPosition = processDirection(visitedHouses, currentPosition, direction)
-    return len(visitedHouses.keys())
+        position = process_direction(visited_houses, position, direction)
+    return len(visited_houses)
 
 
 def part2(directions: List[complex]):
-    visitedHouses = {}
-    visitedHouses[0] = 1
-    santaCurrentPosition = robotCurrentPosition = 0
+    visited_houses = set()
+    visited_houses.add(0)
+    santa_position = robot_position = 0
     for index, direction in enumerate(directions):
         if index % 2:
-            santaCurrentPosition = processDirection(visitedHouses, santaCurrentPosition, direction)
+            santa_position = process_direction(visited_houses, santa_position, direction)
         else:
-            robotCurrentPosition = processDirection(visitedHouses, robotCurrentPosition, direction)
-    return len(visitedHouses.keys())
+            robot_position = process_direction(visited_houses, robot_position, direction)
+    return len(visited_houses)
 
 
 def solve(directions: List[complex]) -> Tuple[int,int]:
@@ -44,11 +41,11 @@ DIRECTIONS = {
     ">": 1,
     "<": -1
 }
-def getInput(filePath: str) -> List[complex]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
+def get_input(file_path: str) -> List[complex]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
     
-    with open(filePath, "r") as file:
+    with open(file_path, "r") as file:
         return [ DIRECTIONS[c] for c in file.read().strip() if c in DIRECTIONS ]
 
 
@@ -57,10 +54,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
