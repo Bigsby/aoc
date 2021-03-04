ef44583d16a3e5e4e40a4a3a4857d2de0e4df6bc
Bruno Bernardo <bigsby@live.com>
2021-03-04 00:34:18 +0000

    2017/04 py clean up

--- 2017/04/py/run.py
+++ 2017/04/py/run.py
@@ -4,36 +4,36 @@ import sys, os, time
 from typing import Callable, List, Tuple
 
 
-def runTests(passphrases: List[List[str]], validationFunc: Callable[[List[str]],bool]) -> int:
-    return sum(map(lambda passphrase: validationFunc(passphrase), passphrases))
+def run_tests(passphrases: List[List[str]], validation_func: Callable[[List[str]],bool]) -> int:
+    return sum(map(lambda passphrase: validation_func(passphrase), passphrases))
 
 
-def isAnagram(word1: str, word2: str) -> bool:
+def is_anagram(word1: str, word2: str) -> bool:
     return len(word1) == len(word2) and \
         all(map(lambda c: word1.count(c) == word2.count(c), word1))
 
 
-def hasNoAnagram(passphrase: List[str]) -> bool:
+def has_no_anagram(passphrase: List[str]) -> bool:
     for index, word in enumerate(passphrase):
-        for otherWord in passphrase[:index] + passphrase[index + 1:]:
-            if isAnagram(word, otherWord):
+        for other_word in passphrase[:index] + passphrase[index + 1:]:
+            if is_anagram(word, other_word):
                 return False
     return True
 
 
 def solve(passphrases: List[List[str]]) -> Tuple[int,int]:
     return (
-        runTests(passphrases, lambda passphrase: 
+        run_tests(passphrases, lambda passphrase: 
             not any(map(lambda word: passphrase.count(word) > 1, passphrase))),
-        runTests(passphrases, hasNoAnagram)
+        run_tests(passphrases, has_no_anagram)
     )
 
 
-def getInput(filePath: str) -> List[List[str]]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
+def get_input(file_path: str) -> List[List[str]]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
     
-    with open(filePath, "r") as file:
+    with open(file_path, "r") as file:
         return [ line.strip().split(" ") for line in file.readlines() ]
 
 
@@ -42,10 +42,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
