31730a57bfcf70b94ebcb14ec5634668163d817b
Bruno Bernardo <bigsby@live.com>
2021-03-04 21:35:45 +0000

    2018/05 py clean up

--- 2018/05/py/run.py
+++ 2018/05/py/run.py
@@ -6,27 +6,28 @@ import re
 
 
 def part1(polymer: str) -> int:
-    polymerInts = [ ord(c) for c in polymer]
-    hadChanges = True
-    while hadChanges:
-        hadChanges = False
+    polymer_ints = [ ord(c) for c in polymer]
+    had_changes = True
+    while had_changes:
+        had_changes = False
         index = 0
-        while index < len(polymerInts) - 1:
-            if abs(polymerInts[index] - polymerInts[index + 1]) == 32:
-                del polymerInts[index]
-                del polymerInts[index]
-                hadChanges = True
+        while index < len(polymer_ints) - 1:
+            if abs(polymer_ints[index] - polymer_ints[index + 1]) == 32:
+                del polymer_ints[index]
+                del polymer_ints[index]
+                had_changes = True
             else:      
                 index += 1
-    return len(polymerInts)
+    return len(polymer_ints)
 
 
 def part2(polymer: str) -> int:
-    minUnits = sys.maxsize
-    for cOrd in range(ord("A"), ord("Z") + 1):
-        strippedPolymer = re.sub("".join([ "[", chr(cOrd), chr(cOrd + 32), "]" ]), "", polymer)
-        minUnits = min(minUnits, part1(strippedPolymer))
-    return minUnits
+    min_units = sys.maxsize
+    for c_ord in range(ord("A"), ord("Z") + 1):
+        stripped_polymer = polymer.replace(chr(c_ord), "")
+        stripped_polymer = stripped_polymer.replace(chr(c_ord + 32), "")
+        min_units = min(min_units, part1(stripped_polymer))
+    return min_units
 
 
 def solve(polymer: str) -> Tuple[int,int]:
@@ -36,11 +37,11 @@ def solve(polymer: str) -> Tuple[int,int]:
     )
 
 
-def getInput(filePath: str) -> str:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
+def get_input(file_path: str) -> str:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
     
-    with open(filePath, "r") as file:
+    with open(file_path, "r") as file:
         return file.read().strip()
 
 
@@ -49,10 +50,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
