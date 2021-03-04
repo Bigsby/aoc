c43a2f9b1d71a095f34caf2ab6365633d0c38d40
Bruno Bernardo <bigsby@live.com>
2021-03-04 22:21:32 +0000

    2020/05 py clean up

--- 2020/05/py/run.py
+++ 2020/05/py/run.py
@@ -1,30 +1,34 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import List, Tuple
 
 
 def part2(seats: List[int]) -> int:
-    lastId = min(seats)
-    for currentId in sorted(seats):
-        if currentId - lastId == 2:
-            return lastId + 1
-        lastId = currentId
+    last_id = min(seats)
+    for current_id in sorted(seats):
+        if current_id - last_id == 2:
+            return last_id + 1
+        last_id = current_id
     raise Exception("Seat not found")
 
 
-def solve(seats: List[int]) -> Tuple[int,int]:
+def solve(seats: List[int]) -> Tuple[int, int]:
     return (
         max(seats),
         part2(seats)
     )
 
-def getInput(filePath: str) -> List[int]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
-        return [ int(line.replace("B", "1").replace("F", "0").replace("R", "1").replace("L", "0"), 2) for line in file.readlines() ]
+
+def get_input(file_path: str) -> List[int]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
+        return [int(line.replace("B", "1").replace("F", "0").replace("R", "1").replace("L", "0"), 2)
+                for line in file.readlines()]
 
 
 def main():
@@ -32,10 +36,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
