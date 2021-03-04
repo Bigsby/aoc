5b866950d601daaaf64de228e525c9bf0f2d5646
Bruno Bernardo <bigsby@live.com>
2021-03-04 14:42:04 +0000

    2019/04 py clean up

--- 2019/04/py/run.py
+++ 2019/04/py/run.py
@@ -5,30 +5,30 @@ from typing import Tuple
 from collections import Counter
 
 
-def isValidPassword(password: str, check2: bool) -> bool:
+def is_password_valid(password: str, check2: bool) -> bool:
     if "".join(list(sorted(list(password)))) == password:
         counts = Counter(password).values()
-        return any([ count > 1 for count in counts ]) and (2 in counts or not check2)
+        return any(count > 1 for count in counts) and (2 in counts or not check2)
     return False
 
 
-def getValidPasswordCount(limits: Tuple[int,int], check2: bool) -> int:
+def get_valid_password_count(limits: Tuple[int,int], check2: bool) -> int:
     start, end = limits
-    return sum([ isValidPassword(str(password), check2) for password in range(start, end) ])
+    return sum([ is_password_valid(str(password), check2) for password in range(start, end) ])
 
 
 def solve(limits: Tuple[int,int]) -> Tuple[int,int]:
     return (
-        getValidPasswordCount(limits, False),
-        getValidPasswordCount(limits, True)
+        get_valid_password_count(limits, False),
+        get_valid_password_count(limits, True)
     )
 
 
-def getInput(filePath: str) -> Tuple[int,int]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
+def get_input(file_path: str) -> Tuple[int,int]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
     
-    with open(filePath, "r") as file:
+    with open(file_path, "r") as file:
         split = file.read().split("-")
         return (int(split[0]), int(split[1]))
 
@@ -38,10 +38,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
