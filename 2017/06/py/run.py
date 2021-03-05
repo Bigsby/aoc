a93272cc4fbe30eabda5298cd93a038ee7a79199
Bruno Bernardo <bigsby@live.com>
2021-03-05 12:32:53 +0000

    2017/06 py clean up

--- 2017/06/py/run.py
+++ 2017/06/py/run.py
@@ -1,39 +1,40 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import List, Tuple
 
 
 def solve(numbers: List[int]) -> Tuple[int, int]:
-    numbersLength = len(numbers)
-    prevousLists: List[str] = []
+    numbers_length = len(numbers)
+    previous_lists: List[List[int]] = []
     cycles = 0
-    currentList = list(numbers)
+    current_list = list(numbers)
     while True:
-        currentListStr = ",".join(map(str, currentList))
-        if currentListStr in prevousLists:
-            return cycles, prevousLists.index(currentListStr)
+        if current_list in previous_lists:
+            return cycles, previous_lists.index(current_list)
         cycles += 1
-        prevousLists.append(currentListStr)
-        updateIndex = -1
-        maxNumber = 0
-        for index, number in enumerate(currentList):
-            if number > maxNumber:
-                maxNumber = number
-                updateIndex = index
-        currentList[updateIndex] = 0
-        while maxNumber:
-            updateIndex = updateIndex + 1 if updateIndex < numbersLength - 1 else 0
-            currentList[updateIndex] += 1
-            maxNumber -= 1
+        previous_lists.append(list(current_list))
+        update_index = -1
+        max_number = 0
+        for index, number in enumerate(current_list):
+            if number > max_number:
+                max_number = number
+                update_index = index
+        current_list[update_index] = 0
+        while max_number:
+            update_index = update_index + 1 if update_index < numbers_length - 1 else 0
+            current_list[update_index] += 1
+            max_number -= 1
 
 
-def getInput(filePath: str) -> List[int]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
-        return [ int(i) for i in file.read().split("\t") ]
+def get_input(file_path: str) -> List[int]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
+        return [int(i) for i in file.read().split("\t")]
 
 
 def main():
@@ -41,10 +42,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
