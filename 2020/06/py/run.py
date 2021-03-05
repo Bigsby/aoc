65031d39f90767962e2afd45cdbe3d7de03feea5
Bruno Bernardo <bigsby@live.com>
2021-03-05 16:59:37 +0000

    2020/06 py clean up

--- 2020/06/py/run.py
+++ 2020/06/py/run.py
@@ -1,43 +1,45 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import Dict, List, Tuple
 
-Group = Tuple[int,Dict[str,int]]
+Group = Tuple[int, List[int]]
 
 
-def getGroupCommonAnswers(group: Group) -> int:
-    peopleCount, record = group
-    return sum(map(lambda letter: record[letter] == peopleCount, record.keys()))
+def get_group_common_answers(group: Group) -> int:
+    people_count, answers = group
+    return len(list(filter(lambda count: count == people_count, answers)))
 
 
-def solve(groups: List[Group]) -> Tuple[int,int]:
+def solve(groups: List[Group]) -> Tuple[int, int]:
     return (
         sum(map(lambda group: len(group[1]), groups)),
-        sum(map(lambda group: getGroupCommonAnswers(group), groups))
+        sum(map(lambda group: get_group_common_answers(group), groups))
     )
 
 
-def processEntry(entry: str) -> Group:
-    record: Dict[str,int] = {}
-    peopleCount = 0
+def process_entry(entry: str) -> Group:
+    record: Dict[str, int] = {}
+    people_count = 0
     for line in entry.split("\n"):
         if line:
-            peopleCount = peopleCount + 1
+            people_count += 1
         for c in line:
             if c in record:
                 record[c] += 1
             else:
                 record[c] = 1
-    return (peopleCount, record)
+    return (people_count, list(record.values()))
 
 
-def getInput(filePath: str) -> List[Group]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
-        return [ processEntry(entry) for entry in file.read().split("\n\n") ]
+def get_input(file_path: str) -> List[Group]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
+        return [process_entry(entry) for entry in file.read().split("\n\n")]
 
 
 def main():
@@ -45,10 +47,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
