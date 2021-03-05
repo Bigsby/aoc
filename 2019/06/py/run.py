2a03a1421eafb0b67b48b6baedf07d4e0bd12ada
Bruno Bernardo <bigsby@live.com>
2021-03-05 16:10:45 +0000

    2019/06 py clean up

--- 2019/06/py/run.py
+++ 2019/06/py/run.py
@@ -7,67 +7,67 @@ import re
 CENTER_OF_MASS = "COM"
 
 
-def part1(planetOrbits: Dict[str, str]) -> int:
+def part1(planet_orbits: Dict[str, str]) -> int:
     planets: Set[str] = set()
-    for orbited, orbiter in planetOrbits.items():
+    for orbiter, orbited in planet_orbits.items():
         planets.add(orbited)
         planets.add(orbiter)
-    orbitCounts: Dict[str, int] = { CENTER_OF_MASS: 0 }
-    while len(orbitCounts) != len(planets):
+    orbit_counts: Dict[str, int] = { CENTER_OF_MASS: 0 }
+    while len(orbit_counts) != len(planets):
         for planet in planets:
-            if planet in orbitCounts:
+            if planet in orbit_counts:
                 continue
-            if planet in planetOrbits:
-                orbitedPlanet = planetOrbits[planet]
-                if orbitedPlanet in orbitCounts:
-                    orbitCounts[planet] = orbitCounts[orbitedPlanet] + 1
+            if planet in planet_orbits:
+                orbited_planet = planet_orbits[planet]
+                if orbited_planet in orbit_counts:
+                    orbit_counts[planet] = orbit_counts[orbited_planet] + 1
             else:
-                orbitCounts[planet] = 1
-    return sum(orbitCounts.values())
+                orbit_counts[planet] = 1
+    return sum(orbit_counts.values())
 
 
-def getPathToCenterOfMass(planet:str, planetOrbits: Dict[str, str]) -> List[str]:
+def get_path_to_center_of_mass(planet:str, planet_orbits: Dict[str, str]) -> List[str]:
     route: List[str] = []
     while planet != CENTER_OF_MASS:
-        planet = planetOrbits[planet]
+        planet = planet_orbits[planet]
         route.append(planet)
     return route
 
 
 YOU = "YOU"
 SAN = "SAN"
-def part2(planetOrbits: Dict[str, str]):
-    youPath: List[str] = getPathToCenterOfMass(YOU, planetOrbits)
-    sanPath: List[str] = getPathToCenterOfMass(SAN, planetOrbits)
-    youPath.reverse()
-    sanPath.reverse()
-    while youPath[0] == sanPath[0]:
-        del youPath[0]
-        del sanPath[0]
-    return len(youPath) + len(sanPath)
+def part2(planet_orbits: Dict[str, str]):
+    you_path: List[str] = get_path_to_center_of_mass(YOU, planet_orbits)
+    san_path: List[str] = get_path_to_center_of_mass(SAN, planet_orbits)
+    you_path.reverse()
+    san_path.reverse()
+    while you_path[0] == san_path[0]:
+        del you_path[0]
+        del san_path[0]
+    return len(you_path) + len(san_path)
 
 
-def solve(planetOrbits: Dict[str, str]) -> Tuple[int,int]:
+def solve(planet_orbits: Dict[str, str]) -> Tuple[int,int]:
     return (
-        part1(planetOrbits),
-        part2(planetOrbits)
+        part1(planet_orbits),
+        part2(planet_orbits)
     )
 
 
-lineRegex = re.compile(r"^(?P<first>[A-Z\d]{3})\)(?P<second>[A-Z\d]{3})$")
+line_regex = re.compile(r"^(?P<orbited>[A-Z\d]{3})\)(?P<orbiter>[A-Z\d]{3})$")
 def parseLine(line: str) -> Tuple[str, str]:
-    match = lineRegex.match(line)
+    match = line_regex.match(line)
     if match:
-        return match.group(1), match.group(2)
+        return match.group("orbited"), match.group("orbiter")
     else:
         raise Exception("Unmatched line", line)
 
 
-def getInput(filePath: str) -> Dict[str, str]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
+def get_input(file_path: str) -> Dict[str, str]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
     
-    with open(filePath, "r") as file:
+    with open(file_path, "r") as file:
         return {  orbiter: orbited for orbited, orbiter in map(lambda line:  parseLine(line), file.readlines()) }
 
 
@@ -76,10 +76,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
