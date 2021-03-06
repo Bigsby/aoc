7c92cbb56b6ff279d854634c45e5de1506b18d1c
Bruno Bernardo <bigsby@live.com>
2021-03-06 16:47:08 +0000

    2015/07 py clean up

--- 2015/07/py/run.py
+++ 2015/07/py/run.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import Callable, Dict, List, Tuple, Union
 import re
 
@@ -15,98 +17,98 @@ class Operand():
             self.scalar = int(value)
         except:
             self.type = WIRE
-            self.wire:str = value
+            self.wire: str = value
 
 
 class Connection():
-    def __init__(self, operation: Union[str,None], type: str, operand1: str, operand2: Union[str,None], target: str):
+    def __init__(self, operation: Union[str, None], type: str, operand1: str, operand2: Union[str, None], target: str):
         self.operation = operation
         self.type = type
         self.operand1 = Operand(operand1)
         self.operand2 = Operand(operand2) if operand2 else None
         self.target = target
 
-BINARY_OPERATIONS: Dict[str,Callable[[int,int],int]] = {
-        "AND": lambda x, y: x & y,
-        "OR": lambda x, y: x | y,
-        "LSHIFT": lambda x, y: x << y,
-        "RSHIFT": lambda x, y: x >> y
+
+BINARY_OPERATIONS: Dict[str, Callable[[int, int], int]] = {
+    "AND": lambda x, y: x & y,
+    "OR": lambda x, y: x | y,
+    "LSHIFT": lambda x, y: x << y,
+    "RSHIFT": lambda x, y: x >> y
 }
 class Circuit():
     def __init__(self, connections: List[Connection]):
         self.connections = connections
-        self.solutions: Dict[str,int] = {}
+        self.solutions: Dict[str, int] = {}
 
-    def getConnectionFromTarget(self, target: str) -> Connection:
+    def get_connection_from_target(self, target: str) -> Connection:
         return next(filter(lambda conn: conn.target == target, self.connections))
 
-    def getValueFromOperand(self, operand: Operand) -> int:
+    def get_value_from_operand(self, operand: Operand) -> int:
         if operand.type == SCALAR:
             return operand.scalar
-        return self.getValueFromConnection(self.getConnectionFromTarget(operand.wire))
-    
-    def getValueFromBinaryConnection(self, connection: Connection) -> int:
+        return self.get_value_from_connection(self.get_connection_from_target(operand.wire))
+
+    def get_value_from_binary_connection(self, connection: Connection) -> int:
         if connection.operation and connection.operand2:
             operation = BINARY_OPERATIONS[connection.operation]
-            return operation(self.getValueFromOperand(connection.operand1), self.getValueFromOperand(connection.operand2))
+            return operation(self.get_value_from_operand(connection.operand1), self.get_value_from_operand(connection.operand2))
         raise Exception("Operation no defined in connection")
 
-    def calculateValueForConnection(self, connection: Connection) -> int:
+    def calculate_value_for_connection(self, connection: Connection) -> int:
         if connection.type == INPUT:
-            return self.getValueFromOperand(connection.operand1)
+            return self.get_value_from_operand(connection.operand1)
         if connection.type == UNARY:
-            return ~self.getValueFromOperand(connection.operand1)
+            return ~self.get_value_from_operand(connection.operand1)
         if connection.type == BINARY:
-            return self.getValueFromBinaryConnection(connection)
+            return self.get_value_from_binary_connection(connection)
         raise Exception("Unknown operation:", connection)
 
-    def getValueFromConnection(self, connection: Connection) -> int:
+    def get_value_from_connection(self, connection: Connection) -> int:
         if connection.target in self.solutions:
             return self.solutions[connection.target]
-        result = self.calculateValueForConnection(connection)
+        result = self.calculate_value_for_connection(connection)
         self.solutions[connection.target] = result
         return result
 
-    def solveFor(self, target: str, initialState: Dict[str,int] = {}) -> int:
+    def solve_for(self, target: str, initialState: Dict[str, int] = {}) -> int:
         self.solutions = initialState
-        return self.getValueFromConnection(self.getConnectionFromTarget(target))
+        return self.get_value_from_connection(self.get_connection_from_target(target))
 
 
-MAX_VALUE = 1 << 17
-def solve(circuit: Circuit) -> Tuple[int,int]:
-    startingTarget = "a"
-    part1 = circuit.solveFor(startingTarget)
-    part2 = circuit.solveFor(startingTarget, { "b": part1 })
-    return part1 if part1 >= 0 else part1 + MAX_VALUE, part2 if part2 >= 0 else part2 + MAX_VALUE
+def solve(circuit: Circuit) -> Tuple[int, int]:
+    starting_target = "a"
+    part1 = circuit.solve_for(starting_target)
+    part2 = circuit.solve_for(starting_target, {"b": part1})
+    return part1, part2
 
 
-sourceTargetRegex = re.compile(r"^(.*)\s->\s(\w+)$")
-inputRegex = re.compile(r"^[^\s]+$")
-unaryRegex = re.compile(r"NOT\s(\w+)$")
-binaryRegex = re.compile(r"^(\w+|\d+)\s+(AND|OR|LSHIFT|RSHIFT)\s+(\w+|\d+)")
-def processLine(line: str) -> Connection:
-    sourceTargetMatch = sourceTargetRegex.match(line)
-    if sourceTargetMatch:
-        source, target = sourceTargetMatch.group(1, 2)
-        if inputRegex.match(source):
+source_target_regex = re.compile(r"^(.*)\s->\s(\w+)$")
+input_regex = re.compile(r"^[^\s]+$")
+unary_regex = re.compile(r"NOT\s(\w+)$")
+binary_regex = re.compile(r"^(\w+|\d+)\s+(AND|OR|LSHIFT|RSHIFT)\s+(\w+|\d+)")
+def process_line(line: str) -> Connection:
+    source_target_match = source_target_regex.match(line)
+    if source_target_match:
+        source, target = source_target_match.group(1, 2)
+        if input_regex.match(source):
             return Connection(None, INPUT, source, None, target)
-        unaryMatch = unaryRegex.match(source)
-        if unaryMatch:
-            return Connection(None, UNARY, unaryMatch.group(1), None, target)
-        binaryMatch = binaryRegex.match(source)
-        if binaryMatch:
-            return Connection(binaryMatch.group(2), BINARY, binaryMatch.group(1), binaryMatch.group(3), target)
+        unary_match = unary_regex.match(source)
+        if unary_match:
+            return Connection(None, UNARY, unary_match.group(1), None, target)
+        binary_match = binary_regex.match(source)
+        if binary_match:
+            return Connection(binary_match.group(2), BINARY, binary_match.group(1), binary_match.group(3), target)
         raise Exception("Unrecognized operation:", source)
     else:
         raise Exception("Unrecognized operation line:", line)
 
 
-def getInput(filePath: str) -> Circuit:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
-        return Circuit([ processLine(line) for line in file.readlines() ])
+def get_input(file_path: str) -> Circuit:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
+        return Circuit([process_line(line) for line in file.readlines()])
 
 
 def main():
@@ -114,10 +116,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
