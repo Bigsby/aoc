b472a692c530cdf88e11d594e70670e879c17edd
Bruno Bernardo <bigsby@live.com>
2021-03-08 18:32:10 +0000

    2017/08 py clean up

--- 2017/08/py/run.py
+++ 2017/08/py/run.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 import re
 from enum import Enum
 from typing import Dict, List, Tuple
@@ -20,10 +22,10 @@ class Operator(Enum):
     GreatherOrEqual = ">="
 
 
-Instruction = Tuple[str,str,Direction,int,Operator,int]
+Instruction = Tuple[str, str, Direction, int, Operator, int]
 
 
-def isConditionValid(source: int, operator: Operator, value: int) -> bool:
+def is_condition_valid(source: int, operator: Operator, value: int) -> bool:
     if operator == Operator.Equal:
         return source == value
     if operator == Operator.NotEqual:
@@ -39,37 +41,47 @@ def isConditionValid(source: int, operator: Operator, value: int) -> bool:
     raise Exception("Unknown operator", operator)
 
 
-def solve(instructions: List[Instruction]) -> Tuple[int,int]:
-    memory: Dict[str,int] = {}
-    maxValue = 0
+def solve(instructions: List[Instruction]) -> Tuple[int, int]:
+    memory: Dict[str, int] = {}
+    max_value = 0
     for target, source, direction, amount, operator, value in instructions:
-        sourceValue = memory[source] if source in memory else 0
-        if not isConditionValid(sourceValue, operator, value):
+        source_value = memory[source] if source in memory else 0
+        if not is_condition_valid(source_value, operator, value):
             continue
         if target not in memory:
             memory[target] = 0
-        memory[target] += amount * (1 if direction == Direction.Increment else -1)
-        maxValue = max(maxValue, memory[target])
+        memory[target] += amount * \
+            (1 if direction == Direction.Increment else -1)
+        max_value = max(max_value, memory[target])
     return (
         max(memory.values()),
-        maxValue
+        max_value
     )
 
 
-lineRegex = re.compile(r"^(?P<target>[a-z]+)\s(?P<direction>inc|dec)\s(?P<amount>-?\d+)\sif\s(?P<source>[a-z]+)\s(?P<operator>==|!=|>=|<=|>|<)\s(?P<value>-?\d+)$")
-def parseLine(line: str)  -> Instruction:
-    match = lineRegex.match(line)
+line_regex = re.compile(
+    r"^(?P<target>[a-z]+)\s(?P<direction>inc|dec)\s(?P<amount>-?\d+)\sif\s(?P<source>[a-z]+)\s(?P<operator>==|!=|>=|<=|>|<)\s(?P<value>-?\d+)$")
+
+
+def parse_line(line: str) -> Instruction:
+    match = line_regex.match(line)
     if match:
-        return match.group("target"), match.group("source"), Direction(match.group("direction")), int(match.group("amount")), Operator(match.group("operator")), int(match.group("value"))
+        return \
+            match.group("target"), \
+            match.group("source"), \
+            Direction(match.group("direction")), \
+            int(match.group("amount")), \
+            Operator(match.group("operator")), \
+            int(match.group("value"))
     raise Exception("Bad format", line)
 
 
-def getInput(filePath: str) -> List[Instruction]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
-        return [ parseLine(line) for line in file.readlines() ]
+def get_input(file_path: str) -> List[Instruction]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
+        return [parse_line(line) for line in file.readlines()]
 
 
 def main():
@@ -77,10 +89,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
