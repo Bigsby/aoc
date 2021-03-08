5f602096431c47614ac25c4cc75dbe0c586d7eef
Bruno Bernardo <bigsby@live.com>
2021-03-08 19:17:01 +0000

    2018/08 py clean up

--- 2018/08/py/run.py
+++ 2018/08/py/run.py
@@ -1,53 +1,52 @@
 #! /usr/bin/python3
 
-import sys, os, time
-from typing import Any, List, Tuple
+import sys
+import os
+import time
+from typing import List, Tuple
 
-Node = Tuple[List['Node'],List[int]]
-
-
-def readNode(data: List[int]) -> Node:
-    childrenCount = data.pop()
-    metadataCount = data.pop()
-    node = ([],[])
-    for _ in range(childrenCount):
-        node[0].append(readNode(data))
-    for _ in range(metadataCount):
-        node[1].append(data.pop())
-    return node
+Node = Tuple[List['Node'], List[int]]
 
 
-def getMetadataSum(node: Node) -> int:
-    return sum(node[1]) + sum(map(lambda child: getMetadataSum(child), node[0]))
+def read_node(data: List[int]) -> Node:
+    children_count = data.pop()
+    metadata_count = data.pop()
+    children: List[Node] = []
+    metatdata: List[int] = []
+    for _ in range(children_count):
+        children.append(read_node(data))
+    for _ in range(metadata_count):
+        metatdata.append(data.pop())
+    return children, metatdata
 
 
-def getRoot(data: List[int]) -> Node:
+def get_metadata_sum(node: Node) -> int:
+    return sum(node[1]) + sum(map(lambda child: get_metadata_sum(child), node[0]))
+
+
+def get_value(node: Node) -> int:
+    children_count = len(node[0])
+    if children_count == 0:
+        return sum(node[1])
+    return sum(map(lambda index: get_value(node[0][index - 1]) if index > 0 and index <= children_count else 0, node[1]))
+
+
+def solve(data: List[int]) -> Tuple[int, int]:
     data = list(data)
     data.reverse()
-    return readNode(data)
-
-
-def getValue(node: Tuple[List[Any],List[int]]) -> int:
-    if not node[0]:
-        return sum(node[1])
-    childrenCount = len(node[0])
-    return sum(map(lambda index: getValue(node[0][index - 1]) if index > 0 and index <= childrenCount else 0, node[1]))
-
-
-def solve(data: List[int]) -> Tuple[int,int]:
-    root = getRoot(data)
+    root = read_node(data)
     return (
-        getMetadataSum(root),
-        getValue(root)
+        get_metadata_sum(root),
+        get_value(root)
     )
 
 
-def getInput(filePath: str) -> List[int]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
-        return [ int(i) for i in file.read().strip().split(" ") ]
+def get_input(file_path: str) -> List[int]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
+        return [int(i) for i in file.read().strip().split(" ")]
 
 
 def main():
@@ -55,10 +54,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
