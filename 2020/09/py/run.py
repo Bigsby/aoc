113717596c80b691eef08ae2afab5394c69ebb67
Bruno Bernardo <bigsby@live.com>
2021-03-09 18:47:54 +0000

    2020/09 py clean up

--- 2020/09/py/run.py
+++ 2020/09/py/run.py
@@ -1,46 +1,49 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import List, Tuple
 
 
-def hasNoValidPair(numberIndex: int, numbers: List[int]) -> bool:
-    number = numbers[numberIndex]
-    for testIndex in range(numberIndex - 25, numberIndex):
-        testNumber = numbers[testIndex]
-        for pairIndex in range(numberIndex - 25, numberIndex):
-            if pairIndex != testIndex and testNumber + numbers[pairIndex] == number:
+def has_no_valid_pair(number_index: int, numbers: List[int]) -> bool:
+    number = numbers[number_index]
+    for test_index in range(number_index - 25, number_index):
+        test_number = numbers[test_index]
+        for pair_index in range(number_index - 25, number_index):
+            if pair_index != test_index and test_number + numbers[pair_index] == number:
                 return False
     return True
 
 
-def getWeakness(numbers: List[int], targetNumber: int) -> int:
-    for startIndex in range(0, len(numbers)):
-        currentSum = 0
+def get_weakness(numbers: List[int], target_number: int) -> int:
+    for start_index in range(0, len(numbers)):
+        current_sum = 0
         length = 1
-        while currentSum < targetNumber:
-            newSet = numbers[startIndex:startIndex + length]
-            currentSum = sum(newSet)
-            if currentSum == targetNumber:
-                return min(newSet) + max(newSet)
+        while current_sum < target_number:
+            new_set = numbers[start_index:start_index + length]
+            current_sum = sum(new_set)
+            if current_sum == target_number:
+                return min(new_set) + max(new_set)
             length += 1
     raise Exception("Weakness not found")
 
 
-def solve(numbers: List[int]) -> Tuple[int,int]:
-    part1Result = next(numbers[index] for index in range(25, len(numbers)) if hasNoValidPair(index, numbers))
+def solve(numbers: List[int]) -> Tuple[int, int]:
+    part1_result = next(numbers[index] for index in range(
+        25, len(numbers)) if has_no_valid_pair(index, numbers))
     return (
-        part1Result,
-        getWeakness(numbers, part1Result)
+        part1_result,
+        get_weakness(numbers, part1_result)
     )
 
 
-def getInput(filePath: str) -> List[int]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
-        return [ int(line) for line in file.readlines() ]
+def get_input(file_path: str) -> List[int]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
+        return [int(line) for line in file.readlines()]
 
 
 def main():
@@ -48,10 +51,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
