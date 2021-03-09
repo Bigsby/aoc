9fde2c707d5402bbb42f89172965227ef8dfaf3e
Bruno Bernardo <bigsby@live.com>
2021-03-09 18:23:53 +0000

    2019/09 rs

---
+++ 2019/09/rs/src/main.rs
@@ -0,0 +1,206 @@
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::collections::VecDeque;
+
+pub struct IntCodeComputer {
+    memory: RefCell<HashMap<i64, i64>>,
+    input: RefCell<VecDeque<i64>>,
+    output: RefCell<VecDeque<i64>>,
+    pointer: i64,
+    pub running: bool,
+    base: i64,
+}
+
+impl IntCodeComputer {
+    pub fn new(memory: &Vec<i64>, input: Vec<i64>) -> IntCodeComputer {
+        IntCodeComputer {
+            memory: RefCell::new(
+                memory
+                    .clone()
+                    .into_iter()
+                    .enumerate()
+                    .map(|(index, value)| (index as i64, value))
+                    .collect(),
+            ),
+            input: RefCell::new(VecDeque::from(input)),
+            output: RefCell::new(VecDeque::new()),
+            pointer: 0,
+            base: 0,
+            running: true,
+        }
+    }
+
+    pub fn run(&mut self) -> i64 {
+        while self.tick() {}
+        self.output.borrow_mut().pop_front().unwrap()
+    }
+
+    fn _add_input(&mut self, value: i64) {
+        self.input.borrow_mut().push_back(value);
+    }
+
+    fn get_memory_value(&self, location: i64) -> i64 {
+        if let Some(value) = self.memory.borrow().get(&location) {
+            *value
+        } else {
+            0
+        }
+    }
+
+    fn get_parameter(&self, offset: i64, mode: i64) -> i64 {
+        let value = self.get_memory_value(self.pointer + offset);
+        match mode {
+            0 => self.get_memory_value(value),             // POSITION
+            1 => value,                                    // IMMEDIATE
+            2 => self.get_memory_value(self.base + value), // RELATIVE
+            _ => {
+                panic!("Unrecognized parameter mode '{}'", mode);
+            }
+        }
+    }
+
+    fn get_address(&self, offset: i64, mode: i64) -> i64 {
+        let value = self.get_memory_value(self.pointer + offset);
+        match mode {
+            0 => value,             // POSITION
+            2 => self.base + value, // RELATIVE
+            _ => {
+                panic!("Unrecognized address mode '{}'", mode);
+            }
+        }
+    }
+
+    fn set_memory(&mut self, offset: i64, mode: i64, value: i64) {
+        let address = self.get_address(offset, mode);
+        *self.memory.borrow_mut().entry(address).or_insert(0) = value;
+    }
+
+    fn tick(&mut self) -> bool {
+        if !self.running {
+            return false;
+        }
+        let instruction = *self.memory.borrow().get(&self.pointer).unwrap();
+        let (opcode, p1_mode, p2_mode, p3_mode) = (
+            instruction % 100,
+            (instruction / 100) % 10,
+            (instruction / 1000) % 10,
+            (instruction / 10000) % 10,
+        );
+        match opcode {
+            1 => {
+                // ADD
+                self.set_memory(
+                    3,
+                    p3_mode,
+                    self.get_parameter(1, p1_mode) + self.get_parameter(2, p2_mode),
+                );
+                self.pointer += 4;
+            }
+            2 => {
+                // MUL
+                self.set_memory(
+                    3,
+                    p3_mode,
+                    self.get_parameter(1, p1_mode) * self.get_parameter(2, p2_mode),
+                );
+                self.pointer += 4;
+            }
+            3 => {
+                // INPUT
+                let input = self.input.borrow_mut().pop_front();
+                if let Some(value) = input {
+                    self.set_memory(1, p1_mode, value);
+                    self.pointer += 2;
+                }
+            }
+            4 => {
+                // OUTPUT
+                self.output
+                    .borrow_mut()
+                    .push_back(self.get_parameter(1, p1_mode));
+                self.pointer += 2;
+            }
+            5 => {
+                // JMP_TRUE
+                if self.get_parameter(1, p1_mode) != 0 {
+                    self.pointer = self.get_parameter(2, p2_mode);
+                } else {
+                    self.pointer += 3;
+                }
+            }
+            6 => {
+                // JMP_FALSE
+                if self.get_parameter(1, p1_mode) == 0 {
+                    self.pointer = self.get_parameter(2, p2_mode);
+                } else {
+                    self.pointer += 3;
+                }
+            }
+            7 => {
+                // LESS_THAN
+                self.set_memory(
+                    3,
+                    p3_mode,
+                    if self.get_parameter(1, p1_mode) < self.get_parameter(2, p2_mode) {
+                        1
+                    } else {
+                        0
+                    },
+                );
+                self.pointer += 4;
+            }
+            8 => {
+                // EQUALS
+                self.set_memory(
+                    3,
+                    p3_mode,
+                    if self.get_parameter(1, p1_mode) == self.get_parameter(2, p2_mode) {
+                        1
+                    } else {
+                        0
+                    },
+                );
+                self.pointer += 4;
+            }
+            9 => {
+                // SET_BASE
+                self.base += self.get_parameter(1, p1_mode);
+                self.pointer += 2;
+            }
+            99 => {
+                // HALT
+                self.running = false;
+            }
+            _ => panic!("Unknown instruction {} {}", self.pointer, opcode),
+        }
+        self.running
+    }
+}
+
+fn solve(memory: &Vec<i64>) -> (i64, i64) {
+    (
+        IntCodeComputer::new(memory, vec![1]).run(),
+        IntCodeComputer::new(memory, vec![2]).run(),
+    )
+}
+
+fn get_input(file_path: &String) -> Vec<i64> {
+    std::fs::read_to_string(file_path)
+        .expect("Error reading input file!")
+        .split(',')
+        .map(|i| i.parse().unwrap())
+        .collect()
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        panic!("Please, add input file path as parameter");
+    }
+    let now = std::time::Instant::now();
+    let (part1_result, part2_result) = solve(&get_input(&args[1]));
+    println!("P1: {}", part1_result);
+    println!("P1: {}", part2_result);
+    println!();
+    println!("Time: {:.7}", now.elapsed().as_secs_f32());
+}
