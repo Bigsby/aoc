7402a630c1a2761ef8bb29abb09de47ceb4f1c3c
Bruno Bernardo <bigsby@live.com>
2021-03-11 15:24:47 +0000

    templates: add cpp

---
+++ templates/cpp/run.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+
+using namespace std;
+
+int part1(string puzzleInput) {
+    return puzzleInput.length();
+}
+
+int part2(string puzzleInput) {
+    return puzzleInput.length();
+}
+
+struct Results {
+    int part1;
+    int part2;
+    Results(int p1, int p2) {
+        part1 = p1;
+        part2 = p2;
+    }
+};
+
+Results solve(string puzzleInput) {
+    return Results(part1(puzzleInput), part2(puzzleInput));
+}
+
+string getInput(char *filePath) {
+    ifstream file(filePath);
+    if (!file.is_open())
+        throw runtime_error("Error reading input file!");
+    
+    string content((istreambuf_iterator<char>(file)), (istreambuf_iterator<char>()));
+    file.close();
+    return content;    
+}
+
+
+int main(int argc, char *argv[]) {
+    if (argc != 2)
+        throw runtime_error("Please, add input file path as parameter");
+
+    clock_t begin = clock();
+    auto results = solve(getInput(argv[1]));
+    clock_t end = clock();
+    auto elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+    cout << "P1: " << results.part1 << endl;
+    cout << "P2: " << results.part2 << endl;
+    cout << endl;
+    cout.precision(7);
+    cout << "Time: " << std::fixed << elapsed_secs << endl;
+    return 0;
+}
