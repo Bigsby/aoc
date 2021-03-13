f6e6bbb881f58e4e8e0c1f7e4edae29542d03f2f
Bruno Bernardo <bigsby@live.com>
2021-03-13 01:58:52 +0000

    2020/10 py clean up

--- 2020/10/py/run.py
+++ 2020/10/py/run.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import List, Tuple
 from functools import reduce
 
@@ -8,53 +10,55 @@ from functools import reduce
 def part1(adapters: List[int]) -> int:
     diff1 = 0
     diff3 = 1
-    currentJoltage = 0
+    current_joltage = 0
     for joltage in adapters:
-        diff = joltage - currentJoltage
+        diff = joltage - current_joltage
         if diff == 1:
             diff1 += 1
         elif diff == 3:
             diff3 += 1
-        currentJoltage = joltage
+        current_joltage = joltage
     return diff1 * diff3
 
 
-def calculateCombinations(sequence: int) -> int:
+def calculate_combinations(sequence: int) -> int:
     if sequence < 3:
         return 1
     if sequence == 3:
         return 2
-    return calculateCombinations(sequence - 1) + calculateCombinations(sequence - 2) + calculateCombinations(sequence - 3)
+    return calculate_combinations(sequence - 1) \
+        + calculate_combinations(sequence - 2) \
+        + calculate_combinations(sequence - 3)
 
 
 def part2(adapters: List[int]) -> int:
     adapters.append(adapters[-1])
-    sequences = []
-    currentSequenceLength = 1
-    currentJoltage = 0
+    sequences: List[int] = []
+    current_sequence_length = 1
+    current_joltage = 0
     for joltage in adapters:
-        if currentJoltage == joltage - 1:
-            currentSequenceLength += 1
+        if current_joltage == joltage - 1:
+            current_sequence_length += 1
         else:
-            sequences.append(currentSequenceLength)
-            currentSequenceLength = 1
-        currentJoltage = joltage
-    return reduce(lambda soFar, length: soFar * calculateCombinations(length), sequences, 1)
+            sequences.append(current_sequence_length)
+            current_sequence_length = 1
+        current_joltage = joltage
+    return reduce(lambda soFar, length: soFar * calculate_combinations(length), sequences, 1)
 
 
-def solve(adapters: List[int]) -> Tuple[int,int]:
+def solve(adapters: List[int]) -> Tuple[int, int]:
     return (
         part1(adapters),
         part2(adapters)
     )
 
 
-def getInput(filePath: str) -> List[int]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
-        return sorted([ int(line) for line in file.readlines() ])
+def get_input(file_path: str) -> List[int]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
+        return sorted([int(line) for line in file.readlines()])
 
 
 def main():
@@ -62,10 +66,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
