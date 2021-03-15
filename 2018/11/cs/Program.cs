8cdb1dfd9f105b140cc8667f30bcb7963db50e14
Bruno Bernardo <bigsby@live.com>
2021-03-15 12:08:11 +0000

    2018/11 cs clean up

--- 2018/11/cs/Program.cs
+++ 2018/11/cs/Program.cs
@@ -8,10 +8,11 @@ using System.Drawing;
 
 namespace AoC
 {
-    using Grid = Dictionary<Point, int>;
     class Program
     {
         const int GRID_SIZE = 300;
+
+        static int GetIndex(int x, int y) => y * GRID_SIZE + x;
 
         static int CalculatePowerLevel(int x, int y, int serialNumber)
         {
@@ -23,27 +24,28 @@ namespace AoC
             return powerLevel - 5;
         }
 
-        static Grid BuildGrid(int serialNumber)
+        static int[] BuildGrid(int serialNumber)
             => Enumerable.Range(0, GRID_SIZE).SelectMany(y => Enumerable.Range(0, GRID_SIZE).Select(x => (x, y)))
-                .ToDictionary(pair => new Point(pair.x, pair.y), pair => CalculatePowerLevel(pair.x + 1, pair.y + 1, serialNumber));
+                .Select((pair) => CalculatePowerLevel(pair.x + 1, pair.y + 1, serialNumber))
+                .ToArray();
 
-        static Grid BuildSummedAreaTable(Grid grid)
+        static int[] BuildSummedAreaTable(int[] grid)
         {
             for (var y = 0; y < GRID_SIZE; y++)
                 for (var x = 0; x < GRID_SIZE; x++)
-                    grid[new Point(x, y)] =
-                                            grid[new Point(x    , y    )] +
-                                   (x > 0 ? grid[new Point(x - 1, y    )] : 0) +
-                                   (y > 0 ? grid[new Point(x    , y - 1)] : 0) +
-                        - (x > 0 && y > 0 ? grid[new Point(x - 1, y - 1)] : 0);
+                    grid[GetIndex(x, y)] =
+                                            grid[GetIndex(x,     y)] +
+                                   (x > 0 ? grid[GetIndex(x - 1, y)] : 0) +
+                                   (y > 0 ? grid[GetIndex(x    , y - 1)] : 0) +
+                        - (x > 0 && y > 0 ? grid[GetIndex(x - 1, y - 1)] : 0);
             return grid;
         }
 
-        static int SumFromAreaTable(Grid grid, int x, int y, int size)
-            => grid[new Point(x - 1       , y - 1       )]
-             - grid[new Point(x - 1 + size, y - 1       )]
-             - grid[new Point(x - 1       , y - 1 + size)]
-             + grid[new Point(x - 1 + size, y - 1 + size)];
+        static int SumFromAreaTable(int[] grid, int x, int y, int size)
+            => grid[GetIndex(x - 1       , y - 1)]
+             - grid[GetIndex(x - 1 + size, y - 1)]
+             - grid[GetIndex(x - 1       , y - 1 + size)]
+             + grid[GetIndex(x - 1 + size, y - 1 + size)];
 
         static (string, string) Solve(int serialNumber)
         {
@@ -56,7 +58,7 @@ namespace AoC
             var max3Fuel = 0;
             foreach (var size in Enumerable.Range(1, GRID_SIZE - 1))
                 foreach (var (x, y) in Enumerable.Range(1, GRID_SIZE - size - 1)
-                                    .SelectMany(y => Enumerable.Range(1, GRID_SIZE - size - 1).Select(x => (x, y))))
+                                    .SelectMany(x => Enumerable.Range(1, GRID_SIZE - size - 1).Select(y => (x, y))))
                 {
                     var fuel = SumFromAreaTable(summedAreaTable, x, y, size);
                     if (fuel > maxFuel)
@@ -71,7 +73,7 @@ namespace AoC
                         max3Cell = (x + 1, y + 1);
                     }
                 }
-            return ($"{max3Cell.Item1},{max3Cell.Item2}", $"{maxCell.Item1},{maxCell.Item1},{maxSize}");
+            return ($"{max3Cell.Item1},{max3Cell.Item2}", $"{maxCell.Item1},{maxCell.Item2},{maxSize}");
         }
 
         static int GetInput(string filePath)
