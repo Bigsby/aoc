76d602782e570078d370c95b0db2aa9d7273144c
Bruno Bernardo <bigsby@live.com>
2021-03-15 12:08:01 +0000

    2018/11 py clean up

--- 2018/11/py/run.py
+++ 2018/11/py/run.py
@@ -1,69 +1,75 @@
 #! /usr/bin/python3
 
-import sys, os, time
-from typing import Dict, Tuple
+import sys
+import os
+import time
+from typing import List, Tuple
 from itertools import product
 
-Grid = Dict[complex,int]
+Grid = List[int]
 GRID_SIZE = 300
 
 
-def calculatePowerLevel(x: int, y: int, serialNumber: int) -> int:
-    rackId = x + 10
-    powerLevel = rackId * y
-    powerLevel += serialNumber
-    powerLevel *= rackId
-    powerLevel = powerLevel % 1000 // 100
-    return powerLevel - 5
+def get_index(x: int, y: int) -> int:
+    return y * GRID_SIZE + x
+
+def calculate_power_level(x: int, y: int, serial_number: int) -> int:
+    rack_id = x + 10
+    power_level = rack_id * y
+    power_level += serial_number
+    power_level *= rack_id
+    power_level = power_level % 1000 // 100
+    return power_level - 5
 
 
-def buildGrid(serialNumber: int) -> Dict[complex,int]:
-    return { x + y * 1j: calculatePowerLevel(x + 1, y + 1, serialNumber) for y, x in product(range(GRID_SIZE), range(GRID_SIZE)) }
+def build_grid(serial_number: int) -> Grid:
+    return [calculate_power_level(x + 1, y + 1, serial_number) 
+        for y, x in product(range(GRID_SIZE), range(GRID_SIZE))]
 
 
-def buildSummedAreaTable(grid: Grid) -> Grid:
+def build_summed_area_table(grid: Grid) -> Grid:
     for y in range(GRID_SIZE):
         for x in range(GRID_SIZE):
-              grid[x     +  y      * 1j ] = \
-              grid[x     +  y      * 1j ] + \
-            ( grid[x - 1 +  y      * 1j ] if x > 0 else 0 ) + \
-            ( grid[x     + (y - 1) * 1j ] if y > 0 else 0) + \
-            (-grid[x - 1 + (y - 1) * 1j ] if x > 0 and y > 0 else 0)    
+            grid[get_index(x, y)] = \
+                  grid[get_index(x    , y    )] + \
+                ( grid[get_index(x - 1, y    )] if x > 0 else 0) + \
+                ( grid[get_index(x    , y - 1)] if y > 0 else 0) + \
+                (-grid[get_index(x - 1, y - 1)] if x > 0 and y > 0 else 0)
     return grid
 
 
-def sumFromAreaTable(grid: Grid, x: int, y: int, size: int) -> int:
-    return  grid[x - 1        + (y - 1)        * 1j] \
-          - grid[x - 1 + size + (y - 1)        * 1j] \
-          - grid[x - 1        + (y - 1 + size) * 1j] \
-          + grid[x - 1 + size + (y - 1 + size) * 1j]
+def sum_from_area_table(grid: Grid, x: int, y: int, size: int) -> int:
+    return grid[get_index(x - 1       , y - 1       )] \
+         - grid[get_index(x - 1 + size, y - 1       )] \
+         - grid[get_index(x - 1       , y - 1 + size)] \
+         + grid[get_index(x - 1 + size, y - 1 + size)]
 
 
-def solve(serialNumber: int) -> Tuple[str,str]:
-    grid = buildGrid(serialNumber)
-    summedAreaTable = buildSummedAreaTable(grid)
-    maxFuel = maxSize = 0
-    maxCell = (-1, -1)
-    max3Cell = (-1, -1)
-    max3Fuel = 0
+def solve(serial_number: int) -> Tuple[str, str]:
+    grid = build_grid(serial_number)
+    summed_area_table = build_summed_area_table(grid)
+    max_fuel = max_size = 0
+    max_cell = (-1, -1)
+    max3_cell = (-1, -1)
+    max3_fuel = 0
     for size in range(1, GRID_SIZE):
         for y, x in product(range(1, GRID_SIZE - size), range(1, GRID_SIZE - size)):
-            fuel = sumFromAreaTable(summedAreaTable, x, y, size)
-            if fuel > maxFuel:
-                maxFuel = fuel
-                maxCell = x + 1, y + 1
-                maxSize = size
-            if size == 3 and fuel > max3Fuel:
-                max3Fuel = fuel
-                max3Cell = x + 1, y + 1
-    return f"{max3Cell[0]},{max3Cell[1]}", f"{maxCell[0]},{maxCell[1]},{maxSize}"
-    
+            fuel = sum_from_area_table(summed_area_table, x, y, size)
+            if fuel > max_fuel:
+                max_fuel = fuel
+                max_cell = x + 1, y + 1
+                max_size = size
+            if size == 3 and fuel > max3_fuel:
+                max3_fuel = fuel
+                max3_cell = x + 1, y + 1
+    return f"{max3_cell[0]},{max3_cell[1]}", f"{max_cell[0]},{max_cell[1]},{max_size}"
 
-def getInput(filePath: str) -> int:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
+
+def get_input(file_path: str) -> int:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
         return int(file.read().strip())
 
 
@@ -72,10 +78,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
