5a682cccab3f851b774ff56c71c296d895c0be43
Bruno Bernardo <bigsby@live.com>
2021-03-15 01:29:25 +0000

    2017/11 py clean up

--- 2017/11/py/run.py
+++ 2017/11/py/run.py
@@ -1,40 +1,46 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import List, Tuple
 import re
 
 
-def getHexManhatanDistance(position: complex) -> int:
+def get_hex_manhatan_distance(position: complex) -> int:
     if (position.real > 0) ^ (position.imag > 0):
-        return int(max(abs(position.real) , abs(position.imag)))
-    return int(abs(position.real) +  abs(position.imag))
+        return int(max(abs(position.real), abs(position.imag)))
+    return int(abs(position.real) + abs(position.imag))
 
 
 DIRECTIONS = {
-    "s" :       1j,
-    "se":   1     ,
+    "s":        1j,
+    "se":   1,
     "sw": - 1 + 1j,
     "ne":   1 - 1j,
-    "nw": - 1     ,
-    "n" :     - 1j
+    "nw": - 1,
+    "n":      - 1j
 }
-def solve(instructions: List[str]) -> Tuple[int,int]:
+
+
+def solve(instructions: List[str]) -> Tuple[int, int]:
     furthest = 0
-    currentHex = 0
+    current_hex = 0
     for instrucion in instructions:
-        currentHex += DIRECTIONS[instrucion]
-        furthest = max(furthest, getHexManhatanDistance(currentHex))
-    return getHexManhatanDistance(currentHex), furthest
+        current_hex += DIRECTIONS[instrucion]
+        furthest = max(furthest, get_hex_manhatan_distance(current_hex))
+    return get_hex_manhatan_distance(current_hex), furthest
 
 
-instructionRegex = re.compile(r"ne|nw|n|sw|se|s")
-def getInput(filePath: str) -> List[str]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
-        return [ match.group() for match in instructionRegex.finditer(file.read()) ]
+instruction_regex = re.compile(r"ne|nw|n|sw|se|s")
+
+
+def get_input(file_path: str) -> List[str]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
+        return [match.group() for match in instruction_regex.finditer(file.read())]
 
 
 def main():
@@ -42,10 +48,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
