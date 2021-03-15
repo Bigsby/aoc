08c1f3b357de061e9e8a7376a470e80e77ef4336
Bruno Bernardo <bigsby@live.com>
2021-03-15 18:15:00 +0000

    2016/12 py clean up

--- 2016/12/py/run.py
+++ 2016/12/py/run.py
@@ -1,19 +1,24 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import Dict, List, Tuple
 
 Instruction = List[str]
 
-def runInstructions(instructions: List[Instruction], inputs: Dict[str,int] = {}) -> int:
-    registers = { register: 0 for register in [ "a", "b", "c", "d" ]}
+
+def run_instructions(instructions: List[Instruction], inputs: Dict[str, int] = {}) -> int:
+    registers = {register: 0 for register in ["a", "b", "c", "d"]}
     registers.update(inputs)
     pointer = 0
     while pointer < len(instructions):
         mnemonic, *params = instructions[pointer]
+        # print(instructions[pointer])
         if mnemonic == "cpy":
-            sourceParam, targetParam = params
-            registers[targetParam] = int(sourceParam) if sourceParam.isnumeric() else registers[sourceParam]
+            source_param, target_param = params
+            registers[target_param] = int(
+                source_param) if source_param.isnumeric() else registers[source_param]
             pointer += 1
         elif mnemonic == "inc":
             registers[params[0]] += 1
@@ -27,28 +32,27 @@ def runInstructions(instructions: List[Instruction], inputs: Dict[str,int] = {})
                 pointer += int(jump)
             else:
                 pointer += 1
+        # print(pointer, registers); input()
     return registers["a"]
 
 
-def solve(instructions: List[Instruction]) -> Tuple[int,int]:
+def solve(instructions: List[Instruction]) -> Tuple[int, int]:
     return (
-        runInstructions(instructions), 
-        runInstructions(instructions, { "c": 1 })
+        run_instructions(instructions),
+        run_instructions(instructions, {"c": 1})
     )
 
 
-def parseLine(line: str) -> Instruction:
-    mnemonic = line[:3]
-    parameters = line[3:].strip()
-    return [ mnemonic ] + parameters.split(" ")
+def parse_line(line: str) -> Instruction:
+    return list(map(lambda part: part.strip(), line.split(" ")))
 
 
-def getInput(filePath: str) -> List[Instruction]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
-        return [ parseLine(line) for line in file.readlines() ]
+def get_input(file_path: str) -> List[Instruction]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
+        return [parse_line(line) for line in file.readlines()]
 
 
 def main():
@@ -56,10 +60,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
