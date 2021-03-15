8637c0a9279532a35a7ccdb9342dace400ea15e9
Bruno Bernardo <bigsby@live.com>
2021-03-15 22:57:59 +0000

    2017/12 py refactor

--- 2017/12/py/run.py
+++ 2017/12/py/run.py
@@ -1,43 +1,50 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import Dict, List, Set, Tuple
 import re
 
 
-def getProgramGroup(program: int, connections: Dict[int,List[int]], soFar: Set[int] = set()) -> Set[int]:
-    soFar.add(program)
-    for connection in connections[program]:
-        if connection not in soFar:
-            soFar |= getProgramGroup(connection, connections)
-    return soFar
+def get_program_group(program: int, connections: Dict[int, List[int]]) -> Set[int]:
+    queue = [program]
+    result = {program}
+    while queue:
+        for connection in connections[queue.pop()]:
+            if connection not in result:
+                queue.append(connection)
+                result.add(connection)
+    return result
 
 
-def solve(connections: Dict[int,List[int]]) -> Tuple[int,int]:
-    part1Result = len(getProgramGroup(0, connections))
-    groupsCount = 0
+def solve(connections: Dict[int, List[int]]) -> Tuple[int, int]:
+    part1_result = len(get_program_group(0, connections))
+    groups_count = 0
     while connections:
-        groupsCount += 1
-        for connection in getProgramGroup(next(iter(connections.keys())), connections):
+        groups_count += 1
+        for connection in get_program_group(next(iter(connections.keys())), connections):
             if connection in connections:
-                del connections[connection]    
-    return part1Result, groupsCount
+                del connections[connection]
+    return part1_result, groups_count
 
 
-lineRegex = re.compile(r"^(?P<one>\d+)\s<->\s(?P<two>.*)$")
-def parseLine(line: str) -> Tuple[int,List[int]]:
-    match = lineRegex.match(line)
+line_regex = re.compile(r"^(?P<one>\d+)\s<->\s(?P<two>.*)$")
+
+
+def parse_line(line: str) -> Tuple[int, List[int]]:
+    match = line_regex.match(line)
     if match:
         return int(match.group("one")), list(map(int, match.group("two").split(",")))
     raise Exception("Bad format", line)
 
 
-def getInput(filePath: str) -> Dict[int,List[int]]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
-        return { program: connections for program, connections in[ parseLine(line) for line in file.readlines() ] }
+def get_input(file_path: str) -> Dict[int, List[int]]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
+        return {program: connections for program, connections in [parse_line(line) for line in file.readlines()]}
 
 
 def main():
@@ -45,10 +52,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
