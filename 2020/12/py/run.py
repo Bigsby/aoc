d862f3403509690aa5ab6417841e4fc8b26b52a4
Bruno Bernardo <bigsby@live.com>
2021-03-16 15:39:43 +0000

    2020/12 py clena up

--- 2020/12/py/run.py
+++ 2020/12/py/run.py
@@ -1,28 +1,30 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import List, Tuple
 import re
 
 
-Instruction = Tuple[str,int]
+Instruction = Tuple[str, int]
 CARDINAL_DIRECTIONS = {
-    "N": 1j, 
-    "S": -1j, 
-    "E": 1, 
+    "N": 1j,
+    "S": -1j,
+    "E": 1,
     "W": -1
 }
 ROTATIONS = {
-    "L": 1j, 
+    "L": 1j,
     "R": -1j
 }
 
 
-def navigate(instructions: List[Instruction], heading: complex,  headingOnCardinal: bool = False) -> int:
+def navigate(instructions: List[Instruction], heading: complex,  heading_on_cardinal: bool = False) -> int:
     position = 0j
     for direction, value in instructions:
         if direction in CARDINAL_DIRECTIONS:
-            if headingOnCardinal:
+            if heading_on_cardinal:
                 heading += CARDINAL_DIRECTIONS[direction] * value
             else:
                 position += CARDINAL_DIRECTIONS[direction] * value
@@ -33,27 +35,29 @@ def navigate(instructions: List[Instruction], heading: complex,  headingOnCardin
     return int(abs(position.real) + abs(position.imag))
 
 
-def solve(instructions: List[Instruction]) -> Tuple[int,int]:
+def solve(instructions: List[Instruction]) -> Tuple[int, int]:
     return (
         navigate(instructions, 1 + 0j),
         navigate(instructions, 10 + 1j, True)
     )
 
 
-lineRegex = re.compile(r"^(?P<direction>[NSEWLRF])(?P<value>\d+)$")
-def parseLine(line: str) -> Instruction:
-    match = lineRegex.match(line)
+line_regex = re.compile(r"^(?P<direction>[NSEWLRF])(?P<value>\d+)$")
+
+
+def parse_line(line: str) -> Instruction:
+    match = line_regex.match(line)
     if match:
         return match.group("direction"), int(match.group("value"))
     raise Exception("Bad format", line)
 
 
-def getInput(filePath: str) -> List[Instruction]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
-        return [ parseLine(line) for line in file.readlines() ]
+def get_input(file_path: str) -> List[Instruction]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
+        return [parse_line(line) for line in file.readlines()]
 
 
 def main():
@@ -61,10 +65,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
