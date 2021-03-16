b685da0a8af0ca681926e7c2c42316c4c5184876
Bruno Bernardo <bigsby@live.com>
2021-03-16 18:03:19 +0000

    2016/13 py clean up

--- 2016/13/py/run.py
+++ 2016/13/py/run.py
@@ -1,10 +1,12 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import List, Tuple
 
 
-def isPositionValid(position: complex, number: int) -> bool:
+def is_position_valid(position: complex, number: int) -> bool:
     x, y = int(position.real), int(position.imag)
     if x < 0 or y < 0:
         return False
@@ -12,33 +14,36 @@ def isPositionValid(position: complex, number: int) -> bool:
     return f"{value:b}".count("1") % 2 == 0
 
 
-DIRECTIONS = [ -1, 1j, -1j, 1]
-def solve(number: int) -> Tuple[int,int]:
-    startPosition = 1 + 1j
-    queue: List[Tuple[complex,List[complex]]] = [(startPosition, [startPosition])]
-    allVisited = { startPosition }
-    part1Result = 0
+DIRECTIONS = [-1, 1j, -1j, 1]
+
+
+def solve(number: int) -> Tuple[int, int]:
+    start_position = 1 + 1j
+    queue: List[Tuple[complex, List[complex]]] = [
+        (start_position, [start_position])]
+    all_visited = {start_position}
+    part1_result = 0
     target = 31 + 39 * 1j
-    while queue and part1Result == 0:
+    while queue and part1_result == 0:
         position, visited = queue.pop(0)
         for direction in DIRECTIONS:
-            newPosition = position + direction
-            if newPosition == target:
-                part1Result = len(visited)
-            if newPosition not in visited and isPositionValid(newPosition, number):
-                if (len(visited) <= 50):
-                    allVisited.add(newPosition)
-                newVisited = list(visited)
-                newVisited.append(newPosition)
-                queue.append((newPosition, newVisited))
-    return part1Result, len(allVisited)
+            new_position = position + direction
+            if new_position == target:
+                part1_result = len(visited)
+            if new_position not in visited and is_position_valid(new_position, number):
+                if len(visited) <= 50:
+                    all_visited.add(new_position)
+                new_visited = list(visited)
+                new_visited.append(new_position)
+                queue.append((new_position, new_visited))
+    return part1_result, len(all_visited)
 
 
-def getInput(filePath: str) -> int:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
+def get_input(file_path: str) -> int:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
         return int(file.read().strip())
 
 
@@ -47,10 +52,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
