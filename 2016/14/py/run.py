f0f61eda3ea9f5ddc6b4e4e9ce5bd36b55faff71
Bruno Bernardo <bigsby@live.com>
2021-03-19 17:00:28 +0000

    2016/14 py clean up

--- 2016/14/py/run.py
+++ 2016/14/py/run.py
@@ -1,29 +1,34 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import Dict, List, Tuple
 import re
 from hashlib import md5
 
 
-tripletRegex = re.compile(r"(.)\1{2}")
-quintetRegex = re.compile(r"(.)\1{4}")
-def findKey(salt: str, stretch: int) -> int:
+triplet_regex = re.compile(r"(.)\1{2}")
+quintet_regex = re.compile(r"(.)\1{4}")
+
+
+def find_key(salt: str, stretch: int) -> int:
     index = 0
     keys: List[int] = []
-    threes: Dict[str,List[int]] = { digit: [] for digit in "0123456789abcdef" }
+    threes: Dict[str, List[int]] = {digit: [] for digit in "0123456789abcdef"}
     while len(keys) < 64:
         value = salt + str(index)
         for _ in range(stretch + 1):
             value = md5(value.encode()).hexdigest()
-        match = quintetRegex.search(value)
+        match = quintet_regex.search(value)
         if match:
             digit = match.group()[0]
-            for tripletIndex in threes[digit]:
-                if (index - tripletIndex) <= 1000:
-                    keys.append(tripletIndex)
+            for triplet_index in threes[digit]:
+                if (index - triplet_index) <= 1000:
+                    keys.append(triplet_index)
             threes[digit] = []
-        match = tripletRegex.search(value)
+
+        match = triplet_regex.search(value)
         if match:
             threes[match.group()[0]].append(index)
         index += 1
@@ -31,18 +36,18 @@ def findKey(salt: str, stretch: int) -> int:
     return keys[63]
 
 
-def solve(salt: str) -> Tuple[int,int]:
+def solve(salt: str) -> Tuple[int, int]:
     return (
-        findKey(salt, 0),
-        findKey(salt, 2016)
+        find_key(salt, 0),
+        find_key(salt, 2016)
     )
 
 
-def getInput(filePath: str) -> str:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
+def get_input(file_path: str) -> str:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
         return file.read().strip()
 
 
@@ -51,10 +56,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
