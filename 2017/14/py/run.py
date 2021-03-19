670fd4bb92f7f804d341a18c37f917d50b35d125
Bruno Bernardo <bigsby@live.com>
2021-03-19 18:00:26 +0000

    2107/14 py clean up

--- 2017/14/py/run.py
+++ 2017/14/py/run.py
@@ -1,81 +1,92 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import List, Set, Tuple
 from functools import reduce
 
 MARKS_COUNT = 256
-def runLengths(marks:List[int], lengths: List[int], currentMark: int, skip: int) -> Tuple[List[int],int,int]:
+
+
+def run_lengths(marks: List[int], lengths: List[int], current_mark: int, skip: int) -> Tuple[List[int], int, int]:
     for length in lengths:
-        toReverse: List[int] = []
-        reverseMark = currentMark
+        to_reverse: List[int] = []
+        reverse_mark = current_mark
         for _ in range(length):
-            toReverse.append(marks[reverseMark])
-            reverseMark = reverseMark + 1 if reverseMark < MARKS_COUNT - 1 else 0
-        reverseMark = currentMark
+            to_reverse.append(marks[reverse_mark])
+            reverse_mark = reverse_mark + 1 if reverse_mark < MARKS_COUNT - 1 else 0
+        reverse_mark = current_mark
         for _ in range(length):
-            marks[reverseMark] = toReverse.pop()
-            reverseMark = reverseMark + 1 if reverseMark < MARKS_COUNT - 1 else 0
+            marks[reverse_mark] = to_reverse.pop()
+            reverse_mark = reverse_mark + 1 if reverse_mark < MARKS_COUNT - 1 else 0
         for _ in range(length + skip):
-            currentMark = currentMark + 1 if currentMark < MARKS_COUNT - 1 else 0
+            current_mark = current_mark + 1 if current_mark < MARKS_COUNT - 1 else 0
         skip += 1
-    return marks, currentMark, skip
+    return marks, current_mark, skip
 
 
-SUFFIX = [ 17, 31, 73, 47, 23 ]
-def knothash(key: str) -> str:
-    lengths = [ ord(c) for c in key ]
+SUFFIX = [17, 31, 73, 47, 23]
+
+
+def knot_hash(key: str) -> str:
+    lengths = [ord(c) for c in key]
     lengths.extend(SUFFIX)
-    marks = [ i for i in range(MARKS_COUNT) ]
-    currentMark = skip = 0
+    marks = [i for i in range(MARKS_COUNT)]
+    current_mark = skip = 0
     for _ in range(64):
-        marks, currentMark, skip = runLengths(marks, lengths, currentMark, skip)
-    denseHash = map(lambda index: 
-        reduce(lambda soFar, mark: soFar ^ mark, marks[index * 16:(index + 1) * 16 ]), range(16))
-    return "".join(map(lambda knot: f"{knot:02x}", denseHash))
+        marks, current_mark, skip = run_lengths(
+            marks, lengths, current_mark, skip)
+    dense_hash = map(lambda index:
+                     reduce(lambda soFar, mark: soFar ^ mark, marks[index * 16:(index + 1) * 16]), range(16))
+    return "".join(map(lambda knot: f"{knot:02x}", dense_hash))
 
 
-def getRowHashBinaryString(key: str, index: int) -> str:
-    rowHash = int(knothash(key + "-" + str(index)), 16)
-    return f"{rowHash:0128b}"
+def get_row_hash_binary_string(key: str, index: int) -> str:
+    row_hash = int(knot_hash(key + "-" + str(index)), 16)
+    return f"{row_hash:0128b}"
 
 
-DIRECTIONS = [ 1j, 1, -1j, -1 ]
-def findAdjacent(point: complex, grid: Set[complex], visited: Set[complex]):
+DIRECTIONS = [1j, 1, -1j, -1]
+
+
+def find_adjacent(point: complex, grid: Set[complex], visited: Set[complex]):
     for direction in DIRECTIONS:
         adjacent = point + direction
         if adjacent in grid and adjacent not in visited:
             visited.add(adjacent)
-            findAdjacent(adjacent, grid, visited)
+            find_adjacent(adjacent, grid, visited)
 
 
 def part2(key: str) -> int:
-    gridPoints = set()
+    grid_points = set()
     for row in range(128):
-        for column, c in enumerate(getRowHashBinaryString(key, row)):
+        for column, c in enumerate(get_row_hash_binary_string(key, row)):
             if c == "1":
-                gridPoints.add(column + row * 1j)
+                grid_points.add(column + row * 1j)
     regions = 0
-    while gridPoints:
+    while grid_points:
         regions += 1
-        point = gridPoints.pop()
-        visited = { point }
-        findAdjacent(point, gridPoints, visited)
-        gridPoints -= visited
+        point = grid_points.pop()
+        visited = {point}
+        find_adjacent(point, grid_points, visited)
+        grid_points -= visited
     return regions
 
-def solve(key: str) -> Tuple[int,int]:
+
+def solve(key: str) -> Tuple[int, int]:
     return (
-        sum(map(lambda index: getRowHashBinaryString(key, index).count("1"), range(128))),
+        sum(map(lambda index: get_row_hash_binary_string(
+            key, index).count("1"), range(128))),
         part2(key)
     )
 
 
-def getInput(filePath: str) -> str:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
+def get_input(file_path: str) -> str:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
         return file.read().strip()
 
 
@@ -84,10 +95,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
