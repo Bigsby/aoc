5513e9409e282c9e89a1023b5f31274eb929f7ae
Bruno Bernardo <bigsby@live.com>
2021-03-22 19:11:57 +0000

    2017/15 py clean up

--- 2017/15/py/run.py
+++ 2017/15/py/run.py
@@ -1,12 +1,16 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import Generator, Tuple
 import re
 
 
 MODULUS = 2147483647
-def buildGenerator(number: int, factor: int, divisor: int) -> Generator[int,int,int]:
+
+
+def build_generator(number: int, factor: int, divisor: int) -> Generator[int, int, int]:
     while True:
         number = number * factor % MODULUS
         if number % divisor == 0:
@@ -15,28 +19,35 @@ def buildGenerator(number: int, factor: int, divisor: int) -> Generator[int,int,
 
 FACTOR_A = 16807
 FACTOR_B = 48271
-def runSequences(generators: Tuple[int,int], divisorA: int, divisorB: int, millionCycles: int) -> int:
-    generatorA, generatorB = generators
-    sequenceA, sequenceB = buildGenerator(generatorA, FACTOR_A, divisorA), buildGenerator(generatorB, FACTOR_B, divisorB)
-    return sum(next(sequenceA) == next(sequenceB) for _ in range(millionCycles * (10 ** 6)))
+
+
+def run_sequences(generators: Tuple[int, int], divisor_a: int, divisor_b: int, million_cycles: int) -> int:
+    generator_a, generator_b = generators
+    sequence_a, sequence_b = build_generator(
+        generator_a, FACTOR_A, divisor_a), build_generator(generator_b, FACTOR_B, divisor_b)
+    return sum(next(sequence_a) == next(sequence_b) for _ in range(million_cycles * (10 ** 6)))
 
 
 DIVISOR_A = 4
 DIVISOR_B = 8
-def solve(generators: Tuple[int,int]) -> Tuple[int,int]:
+
+
+def solve(generators: Tuple[int, int]) -> Tuple[int, int]:
     return (
-        runSequences(generators, 1, 1, 40),
-        runSequences(generators, DIVISOR_A, DIVISOR_B, 5)
+        run_sequences(generators, 1, 1, 40),
+        run_sequences(generators, DIVISOR_A, DIVISOR_B, 5)
     )
 
 
-numbersRegex = re.compile(r"\d+")
-def getInput(filePath: str) -> Tuple[int,int]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
-        matches = numbersRegex.findall(file.read())
+numbers_regex = re.compile(r"\d+")
+
+
+def get_input(file_path: str) -> Tuple[int, int]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
+        matches = numbers_regex.findall(file.read())
         return int(matches[0]), int(matches[1])
 
 
@@ -45,10 +56,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
