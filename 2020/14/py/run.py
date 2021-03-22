f362f908671bb916bcb0d03f789b27d127ed931b
Bruno Bernardo <bigsby@live.com>
2021-03-22 16:08:54 +0000

    2020/14 py clean up

--- 2020/14/py/run.py
+++ 2020/14/py/run.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 
 import sys, os, time
-from typing import Iterable, List, Tuple
+from typing import Dict, Iterable, List, Tuple
 import re
 from enum import Enum
 
@@ -11,90 +11,91 @@ class InstructionType(Enum):
     Memory = 1
 
 
-maskRegex = re.compile(r"^mask\s=\s(?P<mask>[X01]+)$")
-memoryRegex = re.compile(r"^mem\[(?P<location>[\d]+)]\s=\s(?P<value>[\d]+)$")
+mask_regex = re.compile(r"^mask\s=\s(?P<mask>[X01]+)$")
+memory_regex = re.compile(r"^mem\[(?P<location>[\d]+)]\s=\s(?P<value>[\d]+)$")
 class Instruction():
-    def __init__(self, inputLine: str):
-        maskMatch = maskRegex.match(inputLine)
-        if maskMatch:
+    def __init__(self, input_line: str):
+        mask_match = mask_regex.match(input_line)
+        if mask_match:
             self.type = InstructionType.Mask
-            self.mask = maskMatch.group("mask")
+            self.mask = mask_match.group("mask")
         else:
-            memoryMatch = memoryRegex.match(inputLine)
-            if memoryMatch:
+            memory_match = memory_regex.match(input_line)
+            if memory_match:
                 self.type = InstructionType.Memory
-                self.location = int(memoryMatch.group("location"))
-                self.value = int(memoryMatch.group("value"))
+                self.location = int(memory_match.group("location"))
+                self.value = int(memory_match.group("value"))
             else:
-                raise Exception(f"Unrecognized instruction: {inputLine}")
+                raise Exception(f"Unrecognized instruction: {input_line}")
 
 
 class Computer():
     def __init__(self):
         self.mask = "X" * 36
-        self.memory = {}
+        self.memory: Dict[int, int] = {}
 
-    def getMemorySum(self) -> int:
+    def get_memory_sum(self) -> int:
         return sum(self.memory.values())
 
-    def runInstruction(self, instruction: Instruction):
+    def run_instruction(self, instruction: Instruction):
         if instruction.type == InstructionType.Mask:
             self.mask = instruction.mask
         else:
-            for location in self.getMemoryLocations(instruction.location):
-                self.memory[location] = self.getValue(instruction.value)
+            value = self.get_value(instruction.value)
+            for location in self.get_memory_locations(instruction.location):
+                self.memory[location] = value
     
-    def getMemoryLocations(self, location: int) -> Iterable[int]:
+    def get_memory_locations(self, location: int) -> Iterable[int]:
         yield location
     
-    def getValue(self, value: int) -> int:
+    def get_value(self, value: int) -> int:
         return value
 
-    def getOrMask(self):
+    def get_or_mask(self):
         return int(self.mask.replace("X", "0"), 2)
     
-    def getAndMask(self):
+    def get_and_mask(self):
         return int(self.mask.replace("X", "1"), 2)
     
 
-def runComputer(computer: Computer, puzzleInput: List[Instruction]) -> int:
+def run_computer(computer: Computer, puzzleInput: List[Instruction]) -> int:
     for instruction in puzzleInput:
-        computer.runInstruction(instruction)
-    return computer.getMemorySum()
+        computer.run_instruction(instruction)
+    return computer.get_memory_sum()
 
 
 class ValueMaskComputer(Computer):
-    def getValue(self, value: int) -> int:
-        return value | self.getOrMask() & self.getAndMask()
+    def get_value(self, value: int) -> int:
+        return value | self.get_or_mask() & self.get_and_mask()
 
 
 xRegex = re.compile("X")
 class MemoryMaskComputer(Computer):
-    def getMemoryLocations(self, location: int) -> Iterable[int]:
-        location |= self.getOrMask()
-        maskBitOfffset = len(self.mask) - 1
-        flipBits = [ *map(lambda match: maskBitOfffset - match.start(), xRegex.finditer(self.mask)) ]
-        for case in range(1 << len(flipBits)):
-            currentLocation = location
-            for index, flipBit in enumerate(flipBits):
-                currentLocation &= ~(1 << flipBit)
-                newBit = ((1 << index) & case) >> index
-                currentLocation |= newBit << flipBit
-            yield currentLocation
+    def get_memory_locations(self, location: int) -> Iterable[int]:
+        location |= self.get_or_mask()
+        mask_bit_offset = len(self.mask) - 1
+        flip_bits = [ *map(lambda match: mask_bit_offset - match.start(), xRegex.finditer(self.mask)) ]
+        for case in range(1 << len(flip_bits)):
+            current_location = location
+            for index, flipBit in enumerate(flip_bits):
+                current_location &= ~(1 << flipBit)
+                new_bit = ((1 << index) & case) >> index
+                current_location |= new_bit << flipBit
+            yield current_location
 
 
 def solve(instructions: List[Instruction]) -> Tuple[int,int]:
     return (
-        runComputer(ValueMaskComputer(), instructions),
-        runComputer(MemoryMaskComputer(), instructions)
+        run_computer(ValueMaskComputer(), instructions),
+        run_computer(MemoryMaskComputer(), instructions)
     )
 
 
-def getInput(filePath: str) -> List[Instruction]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
+def get_input(file_path: str) -> List[Instruction]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
     
-    with open(filePath, "r") as file:
+    with open(file_path, "r") as file:
         return [ Instruction(line) for line in file.readlines() ]
 
 
@@ -103,10 +104,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
