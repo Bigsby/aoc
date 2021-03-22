8d586fee8e32c08154bcd336792ca7fe1d41e5a1
Bruno Bernardo <bigsby@live.com>
2021-03-22 14:33:20 +0000

    2019/14 py clean up

--- 2019/14/py/run.py
+++ 2019/14/py/run.py
@@ -1,71 +1,77 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import Dict, List, Tuple
 import re
 from collections import defaultdict
 import math
 
-ChemicalPortion = Tuple[int,str]
+ChemicalPortion = Tuple[int, str]
 
 
-def getRequiredOre(reactions: Dict[str,Tuple[int,List[ChemicalPortion]]], requireFuel: int) -> int:
-    requiredChemicals = defaultdict(int, {'FUEL': requireFuel})
-    producedChemicals: Dict[str,int] = defaultdict(int)
-    oreCount = 0
-    while requiredChemicals:
-        item, amount = requiredChemicals.popitem()
-        if amount <= producedChemicals[item]:
-            producedChemicals[item] -= amount
+def calculate_required_ore(reactions: Dict[str, Tuple[int, List[ChemicalPortion]]], required_fuel: int) -> int:
+    required_chemicals = defaultdict(int, {'FUEL': required_fuel})
+    produced_chemicals: Dict[str, int] = defaultdict(int)
+    ore_count = 0
+    while required_chemicals:
+        item, amount = required_chemicals.popitem()
+        if amount <= produced_chemicals[item]:
+            produced_chemicals[item] -= amount
             continue
-        amountNeeded = amount - producedChemicals[item]
-        del producedChemicals[item]
-        amountProduced, portions = reactions[item]
-        requiredQuantity = math.ceil(amountNeeded / amountProduced)
-        producedChemicals[item] += (requiredQuantity * amountProduced) - amountNeeded
-        for otherAmountRequired, chemical in portions:
-            chemicalValue = otherAmountRequired * requiredQuantity
+        amount_needed = amount - produced_chemicals[item]
+        del produced_chemicals[item]
+        amount_produced, portions = reactions[item]
+        required_quantity = math.ceil(amount_needed / amount_produced)
+        produced_chemicals[item] += (required_quantity *
+                                     amount_produced) - amount_needed
+        for other_amount_required, chemical in portions:
+            chemical_value = other_amount_required * required_quantity
             if chemical == "ORE":
-                oreCount += chemicalValue
+                ore_count += chemical_value
             else:
-                requiredChemicals[chemical] += chemicalValue
-    return oreCount
+                required_chemicals[chemical] += chemical_value
+    return ore_count
 
 
-def part2(reactions: Dict[str,Tuple[int,List[ChemicalPortion]]]):
-    requiredFuel = 1
-    lastNeeded = getRequiredOre(reactions, requiredFuel)
-    maxOre = 10 ** 12
+def part2(reactions: Dict[str, Tuple[int, List[ChemicalPortion]]]):
+    required_fuel = 1
+    last_needed = calculate_required_ore(reactions, required_fuel)
+    max_ore = 10 ** 12
     while True:
-        requiredFuel = requiredFuel * maxOre // lastNeeded
-        oreNeeded = getRequiredOre(reactions, requiredFuel)
-        if lastNeeded == oreNeeded:
+        required_fuel = required_fuel * max_ore // last_needed
+        ore_needed = calculate_required_ore(reactions, required_fuel)
+        if last_needed == ore_needed:
             break
         else:
-            lastNeeded = oreNeeded
-    return requiredFuel
+            last_needed = ore_needed
+    return required_fuel
 
 
-def solve(reactions: Dict[str,Tuple[int,List[ChemicalPortion]]]) -> Tuple[int,int]:
+def solve(reactions: Dict[str, Tuple[int, List[ChemicalPortion]]]) -> Tuple[int, int]:
     return (
-        getRequiredOre(reactions, 1),
+        calculate_required_ore(reactions, 1),
         part2(reactions)
     )
 
 
-lineRegex = re.compile(r"(\d+)\s([A-Z]+)")
-def parseLine(line: str) -> Tuple[str,int,List[ChemicalPortion]]:
-    matches = lineRegex.findall(line)
-    result = matches.pop()
-    return result[1], int(result[0]), [ (int(part[0]), part[1]) for part in matches ]
+line_regex = re.compile(r"(\d+)\s([A-Z]+)")
 
-    
-def getInput(filePath: str) -> Dict[str,Tuple[int,List[ChemicalPortion]]]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
-        return { chemical: (amount, reactions) for chemical, amount, reactions in  [ parseLine(line) for line in file.readlines() ] }
+
+def parse_line(line: str) -> Tuple[str, int, List[ChemicalPortion]]:
+    matches = line_regex.findall(line)
+    result = matches.pop()
+    return result[1], int(result[0]), [(int(part[0]), part[1]) for part in matches]
+
+
+def get_input(file_path: str) -> Dict[str, Tuple[int, List[ChemicalPortion]]]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
+        return {chemical: (amount, reactions)
+                for chemical, amount, reactions in [parse_line(line) for line in file.readlines()]}
 
 
 def main():
@@ -73,10 +79,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
