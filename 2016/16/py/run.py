78193ad971c82e386cf6de68ba8d686b6726c07e
Bruno Bernardo <bigsby@live.com>
2021-03-26 17:11:52 +0000

    2016/16 py clean up

--- 2016/16/py/run.py
+++ 2016/16/py/run.py
@@ -1,30 +1,35 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import Tuple
 
 
-def getChecksum(data: str, diskLength: int) -> str:
-    while len(data) < diskLength:
-        data = "0".join([ data, "".join([ "1" if c == "0" else "0" for c in data[::-1] ]) ])
-    data = data[:diskLength]
+def get_checksum(data: str, disk_length: int) -> str:
+    while len(data) < disk_length:
+        data = "0".join(
+            [data, "".join(["1" if c == "0" else "0" for c in data[::-1]])])
+        print(data); input()
+    data = data[:disk_length]
     while len(data) % 2 == 0:
-        data = "".join([ "1" if data[index] == data[index + 1] else "0" for index in range(0, len(data), 2) ])
+        data = "".join(["1" if data[index] == data[index + 1]
+                        else "0" for index in range(0, len(data), 2)])
     return data
 
 
-def solve(data: str) -> Tuple[str,str]:
+def solve(data: str) -> Tuple[str, str]:
     return (
-        getChecksum(data, 272),
-        getChecksum(data, 35651584)
+        get_checksum(data, 272),
+        get_checksum(data, 35651584)
     )
 
 
-def getInput(filePath: str) -> str:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
+def get_input(file_path: str) -> str:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
         return file.read().strip()
 
 
@@ -33,10 +38,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
