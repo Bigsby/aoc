978b60ec482dcb7015ffc624b04d9e801f6721f6
Bruno Bernardo <bigsby@live.com>
2021-03-26 16:38:34 +0000

    2015/16 py clean up

--- 2015/16/py/run.py
+++ 2015/16/py/run.py
@@ -1,29 +1,31 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import Dict, List, Tuple
 import re
 from enum import Enum
 
 
-PROPS = [ "children", "cats", "samoyeds", "pomeranians", "akitas", "vizslas", "goldfish", "trees", "cars", "perfumes" ]
-NA_PROP = -1
-
-
-def buildPropDict() -> Dict[str,int]:
-    return { prop: NA_PROP for prop in PROPS }
-    
-
 class AuntRecord():
-    def __init__(self, number: str, prop1Name: str, prop1Value: str, prop2Name: str, prop2Value: str, prop3Name: str, prop3Value: str):
+    def __init__(self, number: str, prop1_name: str, prop1_value: str, prop2_name: str, prop2_value: str, prop3_name: str, prop3_value: str):
         self.number = int(number)
-        self.props = buildPropDict()
-        self.setProp(prop1Name, prop1Value)
-        self.setProp(prop2Name, prop2Value)
-        self.setProp(prop3Name, prop3Value)
+        self.props = AuntRecord.build_prop_dict()
+        self.set_prop(prop1_name, prop1_value)
+        self.set_prop(prop2_name, prop2_value)
+        self.set_prop(prop3_name, prop3_value)
 
-    def setProp(self, name:str, value:str):
+    def set_prop(self, name: str, value: str):
         self.props[name] = int(value)
+
+    @staticmethod
+    def build_prop_dict() -> Dict[str, int]:
+        return {prop: AuntRecord.NA_PROP for prop in AuntRecord.PROPS}
+
+    PROPS = ["children", "cats", "samoyeds", "pomeranians",
+             "akitas", "vizslas", "goldfish", "trees", "cars", "perfumes"]
+    NA_PROP = -1
 
 
 class Operator(Enum):
@@ -38,7 +40,7 @@ class Reading():
         self.operator = operator
 
 
-MFCSAN_READING: Dict[str,Reading] = {
+MFCSAN_READING: Dict[str, Reading] = {
     "children": Reading(3),
     "cats": Reading(7, Operator.GREATER),
     "samoyeds": Reading(2),
@@ -46,48 +48,53 @@ MFCSAN_READING: Dict[str,Reading] = {
     "akitas": Reading(0),
     "vizslas": Reading(0),
     "goldfish": Reading(5, Operator.LESS),
-    "trees": Reading(5, Operator.GREATER),
+    "trees": Reading(3, Operator.GREATER),
     "cars": Reading(2),
     "perfumes": Reading(1)
 }
-def isValidRecord(record: AuntRecord, checkOperator: bool = False) -> bool:
+
+
+def is_valid_record(record: AuntRecord, check_operator: bool = False) -> bool:
     for prop in MFCSAN_READING:
-        recordValue = record.props[prop]
-        if recordValue == NA_PROP:
+        record_value = record.props[prop]
+        if record_value == AuntRecord.NA_PROP:
             continue
         reading = MFCSAN_READING[prop]
-        readingValue = reading.value
-        if checkOperator:
-            if reading.operator == Operator.EQUAL and recordValue != readingValue \
-                or reading.operator == Operator.GREATER and recordValue <= readingValue \
-                or reading.operator == Operator.LESS and recordValue >= readingValue:
+        reading_value = reading.value
+        if check_operator:
+            if reading.operator == Operator.EQUAL and record_value != reading_value \
+                    or reading.operator == Operator.GREATER and record_value <= reading_value \
+                    or reading.operator == Operator.LESS and record_value >= reading_value:
                 return False
-        elif recordValue != readingValue:
+        elif record_value != reading_value:
             return False
     return True
 
 
-def solve(aunts: List[AuntRecord]) -> Tuple[int,int]:
+def solve(aunts: List[AuntRecord]) -> Tuple[int, int]:
     return (
-        next(filter(lambda record: isValidRecord(record), aunts)).number, 
-        next(filter(lambda record: isValidRecord(record, True), aunts)).number
+        next(filter(lambda record: is_valid_record(record), aunts)).number,
+        next(filter(lambda record: is_valid_record(record, True), aunts)).number
     )
 
 
-lineRegex = re.compile(r"^Sue\s(\d+):\s(\w+):\s(\d+),\s(\w+):\s(\d+),\s(\w+):\s(\d+)$")
-def parseLine(line: str):
-    match = lineRegex.match(line)
+line_regex = re.compile(
+    r"^Sue\s(\d+):\s(\w+):\s(\d+),\s(\w+):\s(\d+),\s(\w+):\s(\d+)$")
+
+
+def parse_line(line: str):
+    match = line_regex.match(line)
     if match:
         return AuntRecord(*match.group(1, 2, 3, 4, 5, 6, 7))
     raise Exception("Bad format", line)
 
 
-def getInput(filePath: str) -> List[AuntRecord]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
-        return [ parseLine(line) for line in file.readlines() ]
+def get_input(file_path: str) -> List[AuntRecord]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
+        return [parse_line(line) for line in file.readlines()]
 
 
 def main():
@@ -95,10 +102,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
