0aef24a55ff2b8760fbcbee00fee2408c33a3ba0
Bruno Bernardo <bigsby@live.com>
2021-03-29 16:42:16 +0100

    2016/17 py clean up

--- 2016/17/py/run.py
+++ 2016/17/py/run.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import List, Tuple
 from hashlib import md5
 
@@ -11,30 +13,32 @@ DIRECTIONS = {
     "L": -1,
     "R": 1
 }
-def solve(passcode: str) -> Tuple[str,int]:
-    queue: List[Tuple[complex,str]] = [(0j, "")]
-    longestPathFound = 0
-    shortestPath = ""
+
+
+def solve(passcode: str) -> Tuple[str, int]:
+    queue: List[Tuple[complex, str]] = [(0j, "")]
+    longest_path_found = 0
+    shortest_path = ""
     while queue:
         room, path = queue.pop(0)
         if room == 3 - 3j:
-            if not shortestPath:
-                shortestPath = path
-            longestPathFound = max(longestPathFound, len(path))
+            if not shortest_path:
+                shortest_path = path
+            longest_path_found = max(longest_path_found, len(path))
             continue
-        pathHash = md5((passcode + path).encode("utf-8")).hexdigest()[:4]
-        for index, (pathLetter, direction) in enumerate(DIRECTIONS.items()):
-            newRoom = room + direction
-            if pathHash[index] > "a" and 0 <= newRoom.real < 4 and -4 < newRoom.imag <= 0:
-                queue.append((room + direction, path + pathLetter))
-    return shortestPath, longestPathFound
+        path_hash = md5((passcode + path).encode("utf-8")).hexdigest()[:4]
+        for index, (path_letter, direction) in enumerate(DIRECTIONS.items()):
+            new_room = room + direction
+            if path_hash[index] > "a" and 0 <= new_room.real < 4 and -4 < new_room.imag <= 0:
+                queue.append((new_room, path + path_letter))
+    return shortest_path, longest_path_found
 
 
-def getInput(filePath: str) -> str:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
+def get_input(file_path: str) -> str:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
         return file.read().strip()
 
 
@@ -43,10 +47,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
