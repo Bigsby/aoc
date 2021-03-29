018a0a297dfd2c6289c39aea49b371b95a07d4a0
Bruno Bernardo <bigsby@live.com>
2021-03-29 18:17:35 +0100

    2018/17 py clean up

--- 2018/17/py/run.py
+++ 2018/17/py/run.py
@@ -10,27 +10,26 @@ import re
 Position = complex
 ClaySquares = List[Position]
 Water = Set[Position]
-Spring = Position
 
 
-def getEdges(positions: Iterable[Position]) -> Tuple[int, int, int, int]:
+def get_edges(positions: Iterable[Position]) -> Tuple[int, int, int, int]:
     return int(min(map(lambda s: s.real, positions))), \
         int(max(map(lambda s: s.real, positions))), \
         int(min(map(lambda s: s.imag, positions))), \
         int(max(map(lambda s: s.imag, positions)))
 
 
-def printArea(clay: ClaySquares, flowing: Water, settled: Water, spring: Spring, queue: List[Position], all: bool = False):
-    minX, maxX, minY, maxY = getEdges(
+def print_area(clay: ClaySquares, flowing: Water, settled: Water, spring: Position, queue: List[Position], all: bool = False):
+    min_x, max_x, min_y, max_y = get_edges(
         clay + list(settled) + list(flowing) + [spring])
     margins = 20
     if not all:
-        minX = max(int(spring.real) - margins * 2, minX)
-        maxX = min(int(spring.real) + margins * 2, maxX)
-        minY = max(int(spring.imag) - margins, minY)
-        maxY = min(int(spring.imag) + margins, maxY)
-    for y in range(minY, maxY + margins + 1):
-        for x in range(minX - margins, maxX + margins + 1):
+        min_x = max(int(spring.real) - margins * 2, min_x)
+        max_x = min(int(spring.real) + margins * 2, max_x)
+        min_y = max(int(spring.imag) - margins, min_y)
+        max_y = min(int(spring.imag) + margins, max_y)
+    for y in range(min_y, max_y + margins + 1):
+        for x in range(min_x - margins, max_x + margins + 1):
             c = " "
             position = x + y * 1j
             if position in clay:
@@ -48,7 +47,7 @@ def printArea(clay: ClaySquares, flowing: Water, settled: Water, spring: Spring,
     print()
 
 
-def findEdge(spring: Position, direction: int, settled: Water, clay: ClaySquares) -> Tuple[int, bool]:
+def find_edge(spring: Position, direction: int, settled: Water, clay: ClaySquares) -> Tuple[int, bool]:
     x = direction
     while True:
         current = spring + x
@@ -61,31 +60,32 @@ def findEdge(spring: Position, direction: int, settled: Water, clay: ClaySquares
 
 
 def solve(clay: ClaySquares) -> Tuple[int, int]:
-    maxY = int(max(map(lambda s: s.imag, clay)))
-    minY = int(min(map(lambda s: s.imag, clay)))
-    settled = Water()
-    flowing = Water()
-    queue = [500 + minY * 1j]
+    max_y = int(max(map(lambda s: s.imag, clay)))
+    min_y = int(min(map(lambda s: s.imag, clay)))
+    settled: Water = set()
+    flowing: Water = set()
+    queue = [500 + min_y * 1j]
     while queue:
         spring = queue.pop()
         below = spring + 1j
         if below in flowing:
             continue
         flowing.add(spring)
-        while below.imag <= maxY and below not in clay and below not in settled:
+        while below.imag <= max_y and below not in clay and below not in settled:
             flowing.add(below)
             below += 1j
         if below in clay or below in settled:
             x, y = int(below.real), below.imag * 1j - 1j
-            leftOffset, leftOverflown = findEdge(below - 1j, -1, settled, clay)
-            rightOffset, rightOverflown = findEdge(
+            left_offset, left_overflown = find_edge(
+                below - 1j, -1, settled, clay)
+            right_offset, right_overflown = find_edge(
                 below - 1j, 1, settled, clay)
-            isOverflown = leftOverflown or rightOverflown
-            if not isOverflown:
+            is_overflown = left_overflown or right_overflown
+            if not is_overflown:
                 queue.append(below - 2j)
-            for levelX in range(x + leftOffset, x + rightOffset + 1):
-                position = levelX + y
-                if isOverflown:
+            for level_x in range(x + left_offset, x + right_offset + 1):
+                position = level_x + y
+                if is_overflown:
                     flowing.add(position)
                 else:
                     settled.add(position)
@@ -93,41 +93,41 @@ def solve(clay: ClaySquares) -> Tuple[int, int]:
                         flowing.remove(position)
                     if position in queue:
                         queue.remove(position)
-            if leftOverflown:
-                queue.append(x + leftOffset + y)
-            if rightOverflown:
-                queue.append(x + rightOffset + y)
+            if left_overflown:
+                queue.append(x + left_offset + y)
+            if right_overflown:
+                queue.append(x + right_offset + y)
     return len(settled) + len(flowing), len(settled)
 
 
-lineRegex = re.compile(
+line_regex = re.compile(
     r"^(?P<sC>x|y)=(?P<sV>\d+), (?:x|y)=(?P<mS>\d+)..(?P<mE>\d+)$")
 
 
-def parseLine(line: str) -> ClaySquares:
-    match = lineRegex.match(line)
+def parse_line(line: str) -> ClaySquares:
+    match = line_regex.match(line)
     if match:
-        result = ClaySquares()
-        sC, sV, mS, mE = match.group("sC"), int(match.group("sV")), int(
+        result: ClaySquares = []
+        sc, sv, ms, me = match.group("sC"), int(match.group("sV")), int(
             match.group("mS")), int(match.group("mE"))
-        if sC == "x":
-            for y in range(mS, mE + 1):
-                result.append(sV + y * 1j)
+        if sc == "x":
+            for y in range(ms, me + 1):
+                result.append(sv + y * 1j)
         else:
-            for x in range(mS, mE + 1):
-                result.append(x + sV * 1j)
+            for x in range(ms, me + 1):
+                result.append(x + sv * 1j)
         return result
     raise Exception("Bad format", line)
 
 
-def getInput(filePath: str) -> ClaySquares:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
+def get_input(file_path: str) -> ClaySquares:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
 
-    with open(filePath, "r") as file:
-        clay = ClaySquares()
+    with open(file_path, "r") as file:
+        clay: ClaySquares = []
         for line in file.readlines():
-            clay += parseLine(line)
+            clay += parse_line(line)
         return clay
 
 
@@ -136,10 +136,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
