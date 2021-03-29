acdd1ac44ffd74c7fe378143839404f763728544
Bruno Bernardo <bigsby@live.com>
2021-03-29 15:43:11 +0100

    2015/17 py clean up

--- 2015/17/py/run.py
+++ 2015/17/py/run.py
@@ -10,29 +10,29 @@ from itertools import combinations
 TARGET_TOTAL = 150
 
 
-def getValidCombinations(containers: List[int]) -> List[Tuple[int, ...]]:
-    validCombinations: List[Tuple[int, ...]] = []
-    for containerCount in range(2, len(containers)):
-        for combination in combinations(containers, containerCount):
+def get_valid_combinations(containers: List[int]) -> List[Tuple[int, ...]]:
+    valid_combinations: List[Tuple[int, ...]] = []
+    for container_count in range(2, len(containers)):
+        for combination in combinations(containers, container_count):
             if sum(combination) == TARGET_TOTAL:
-                validCombinations.append(combination)
-    return validCombinations
+                valid_combinations.append(combination)
+    return valid_combinations
 
 
 def solve(containers: List[int]) -> Tuple[int, int]:
-    validCombinations = getValidCombinations(containers)
-    minCount = min([len(combination) for combination in validCombinations])
+    valid_combinations = get_valid_combinations(containers)
+    min_count = min([len(combination) for combination in valid_combinations])
     return \
-        len(validCombinations), \
-        len([combination for combination in validCombinations if len(
-            combination) == minCount])
+        len(valid_combinations), \
+        len([combination for combination in valid_combinations if len(
+            combination) == min_count])
 
 
-def getInput(filePath: str) -> List[int]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
+def get_input(file_path: str) -> List[int]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
 
-    with open(filePath, "r") as file:
+    with open(file_path, "r") as file:
         return [int(line) for line in file.readlines()]
 
 
@@ -41,10 +41,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
