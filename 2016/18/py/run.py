28aaaf1c771ceb0aaf8771900932c337eddd6832
Bruno Bernardo <bigsby@live.com>
2021-03-31 14:32:00 +0100

    2016/18 py clean up

--- 2016/18/py/run.py
+++ 2016/18/py/run.py
@@ -1,28 +1,30 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import List, Tuple
 
 Tiles = List[bool]
 
 
-def solve(tiles: Tiles) -> Tuple[int,int]:
+def solve(tiles: Tiles) -> Tuple[int, int]:
     safe = sum(tiles)
-    part1Result = 0
+    part1_result = 0
     for step in range(1, 400_000):
         if step == 40:
-            part1Result = safe
+            part1_result = safe
         tiles = [True] + tiles + [True]
         tiles = [left == right for left, right in zip(tiles, tiles[2:])]
         safe += sum(tiles)
-    return part1Result, safe
+    return part1_result, safe
 
 
-def getInput(filePath: str) -> Tiles:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
+def get_input(file_path: str) -> Tiles:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
         return list(c == "." for c in file.read().strip())
 
 
@@ -31,10 +33,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
