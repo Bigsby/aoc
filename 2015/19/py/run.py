e3865f793334c366e41c126a437cf8f91a31b34c
Bruno Bernardo <bigsby@live.com>
2021-04-07 15:56:15 +0100

    2015/19 py clean up

--- 2015/19/py/run.py
+++ 2015/19/py/run.py
@@ -4,64 +4,64 @@ import sys
 import os
 import time
 from typing import List, Tuple
+from dataclasses import dataclass
 import re
 
 
+@dataclass
 class Replacement():
-    def __init__(self, source: str, target: str):
-        self.source = source
-        self.target = target
-
-    def __str__(self) -> str:
-        return f"{self.source} => {self.target}"
+    source: str
+    target: str
 
 
-def processReplacement(molecule: str, replacement: Replacement) -> List[str]:
-    return ["".join([molecule[:match.start()], replacement.target, molecule[match.end():]]) for match in re.finditer(replacement.source, molecule)]
+def process_replacement(molecule: str, replacement: Replacement) -> List[str]:
+    return ["".join([molecule[:match.start()], replacement.target, molecule[match.end():]])
+            for match in re.finditer(replacement.source, molecule)]
 
 
-def part1(puzzleInput: Tuple[List[Replacement], str]) -> int:
-    replacements, molecule = puzzleInput
-    newMolecules: List[str] = []
+def part1(puzzle_input: Tuple[List[Replacement], str]) -> int:
+    replacements, molecule = puzzle_input
+    new_molecules: List[str] = []
     for replacement in replacements:
-        for newMolecule in processReplacement(molecule, replacement):
-            newMolecules.append(newMolecule)
+        for new_molecule in process_replacement(molecule, replacement):
+            new_molecules.append(new_molecule)
+    return len(set(new_molecules))
 
-    return len(set(newMolecules))
 
-
-def part2(puzzleInput: Tuple[List[Replacement], str]) -> int:
-    replacements, molecule = puzzleInput
-    targetMolecule = "e"
+def part2(puzzle_input: Tuple[List[Replacement], str]) -> int:
+    replacements, molecule = puzzle_input
+    target_molecule = "e"
     molecule = molecule[::-1]
-    repDict = {rep.target[::-1]: rep.source[::-1] for rep in replacements}
+    replacement_dictionary = {
+        rep.target[::-1]: rep.source[::-1] for rep in replacements}
     count = 0
-    while molecule != targetMolecule:
-        molecule = re.sub("|".join(repDict.keys()),
-                          lambda match: repDict[match.group()], molecule, 1)
+    replacements_regex = re.compile("|".join(replacement_dictionary.keys()))
+    while molecule != target_molecule:
+        molecule = replacements_regex.sub(
+            lambda match: replacement_dictionary[match.group()], molecule, 1)
         count += 1
     return count
 
 
-def solve(puzzleInput: Tuple[List[Replacement], str]) -> Tuple[int, int]:
+def solve(puzzle_input: Tuple[List[Replacement], str]) -> Tuple[int, int]:
     return (
-        part1(puzzleInput),
-        part2(puzzleInput)
+        part1(puzzle_input),
+        part2(puzzle_input)
     )
 
 
-lineRegex = re.compile(r"^(\w+)\s=>\s(\w+)$")
+line_regex = re.compile(r"^(\w+)\s=>\s(\w+)$")
 
 
-def getInput(filePath: str) -> Tuple[List[Replacement], str]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
+def get_input(file_path: str) -> Tuple[List[Replacement], str]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
 
-    with open(filePath, "r") as file:
+    with open(file_path, "r") as file:
         replacements: List[Replacement] = []
         molecule = ""
         for line in file.readlines():
-            match = lineRegex.match(line)
+            match = line_regex.match(line)
             if match:
                 replacements.append(Replacement(*match.group(1, 2)))
             else:
@@ -74,10 +74,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
