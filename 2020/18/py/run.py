1c297985c82421cbe1065d2e890e08f0ab5121f3
Bruno Bernardo <bigsby@live.com>
2021-04-07 15:04:08 +0100

    2020/18 py clean up

--- 2020/18/py/run.py
+++ 2020/18/py/run.py
@@ -1,71 +1,78 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import List, Tuple
 import re
 
 
-def getNextToken(expression: str) -> Tuple[str,str]:
-    currentToken = ""
+def get_next_token(expression: str) -> Tuple[str, str]:
+    current_token = ""
     for index, c in enumerate(expression):
         if c == " ":
-            return currentToken, expression[index + 1:].strip()
-        if currentToken.isdigit() and not c.isdigit():
-            return currentToken, expression[index:].strip()
-        currentToken += c
-    return currentToken, ""
-        
+            return current_token, expression[index + 1:].strip()
+        if current_token.isdigit() and not c.isdigit():
+            return current_token, expression[index:].strip()
+        current_token += c
+    return current_token, ""
 
-def performOperation(first: int, operation: str, second: int) -> int:
+
+def perform_operation(first: int, operation: str, second: int) -> int:
     if operation == "*":
         return first * second
     if operation == "+":
         return first + second
     raise Exception(f"Unknow operation {operation}")
-        
 
-parenRegex = re.compile(r"\((?P<expression>[^()]+)\)")
-plusRegex = re.compile(r"(?P<first>\d+)\s\+\s(?P<second>\d+)")
-def evaluateExpression(expression: str, addFirst: bool) -> int:
-    parenMatch = parenRegex.search(expression)
-    while parenMatch:
-        expression = "".join([expression[:parenMatch.start()], str(evaluateExpression(parenMatch.group("expression"), addFirst)), expression[parenMatch.end():]])
-        parenMatch = parenRegex.search(expression)
 
-    if addFirst:
-        plusMatch = plusRegex.search(expression)
-        while plusMatch:
-            expression = "".join([expression[:plusMatch.start()], str(int(plusMatch.group("first")) + int(plusMatch.group("second"))), expression[plusMatch.end():]])
-            plusMatch = plusRegex.search(expression)
+paren_regex = re.compile(r"\((?P<expression>[^()]+)\)")
+plus_regex = re.compile(r"(?P<first>\d+)\s\+\s(?P<second>\d+)")
 
-    token, expression = getNextToken(expression)
-    currentValue = int(token)
-    operationToPeform = ""
+
+def evaluate_expression(expression: str, add_first: bool) -> int:
+    paren_match = paren_regex.search(expression)
+    while paren_match:
+        expression = "".join([expression[:paren_match.start()], str(evaluate_expression(
+            paren_match.group("expression"), add_first)), expression[paren_match.end():]])
+        paren_match = paren_regex.search(expression)
+
+    if add_first:
+        plus_match = plus_regex.search(expression)
+        while plus_match:
+            expression = "".join([expression[:plus_match.start()], str(int(plus_match.group(
+                "first")) + int(plus_match.group("second"))), expression[plus_match.end():]])
+            plus_match = plus_regex.search(expression)
+
+    token, expression = get_next_token(expression)
+    current_value = int(token)
+    operation_to_perform = ""
     while expression:
-        token, expression = getNextToken(expression)
+        token, expression = get_next_token(expression)
         if token.isdigit():
-            currentValue = performOperation(currentValue, operationToPeform, int(token))
+            current_value = perform_operation(
+                current_value, operation_to_perform, int(token))
         else:
-            operationToPeform = token
-    return currentValue
+            operation_to_perform = token
+    return current_value
 
 
-def evaluateExpressions(addFirst: bool, expressions: List[str]) -> int:
-    return sum([ evaluateExpression(line, addFirst) for line in expressions ])
-    
+def evaluate_expressions(add_first: bool, expressions: List[str]) -> int:
+    return sum([evaluate_expression(expression, add_first) for expression in expressions])
 
-def solve(expression: List[str]) -> Tuple[int,int]:
+
+def solve(expressions: List[str]) -> Tuple[int, int]:
     return (
-        evaluateExpressions(False, expression),
-        evaluateExpressions(True, expression)
+        evaluate_expressions(False, expressions),
+        evaluate_expressions(True, expressions)
     )
 
 
-def getInput(filePath: str) -> List[str]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
+def get_input(file_path: str) -> List[str]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
         return file.readlines()
 
 
@@ -74,10 +81,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
