467bbab06aff73152704e86474a528792e725d8b
Bruno Bernardo <bigsby@live.com>
2021-04-07 23:26:05 +0100

    2020/19 py clean up

--- 2020/19/py/run.py
+++ 2020/19/py/run.py
@@ -13,12 +13,12 @@ class RuleType(Enum):
     Set = 1
 
 
-letterRegex = re.compile(r"^\"(?P<letter>a|b)\"$")
+letter_regex = re.compile(r"^\"(?P<letter>a|b)\"$")
 
 
 class Rule():
     def __init__(self, number: str, definition: str):
-        match = letterRegex.match(definition)
+        match = letter_regex.match(definition)
         self.number = int(number)
         if match:
             self.type = RuleType.Letter
@@ -31,62 +31,64 @@ class Rule():
                     list(map(lambda rule: int(rule), set.strip().split(" "))))
 
 
-def generateRegex(rules: Dict[int, Rule], ruleNumber: int) -> str:
-    rule = rules[ruleNumber]
+def generate_regex(rules: Dict[int, Rule], rule_number: int) -> str:
+    rule = rules[rule_number]
     if rule.type == RuleType.Letter:
         return rule.letter
     else:
-        return "(?:" + "|".join("".join(generateRegex(rules, innerNumber) for innerNumber in ruleSet) for ruleSet in rule.sets) + ")"
+        return "(?:" + \
+            "|".join("".join(generate_regex(rules, inner_number)
+                             for inner_number in rule_set) for rule_set in rule.sets) + ")"
 
 
-def isInnerMatch(rule: str, message: str, position: int) -> Tuple[bool, int]:
+def is_inner_match(rule: str, message: str, position: int) -> Tuple[bool, int]:
     match = re.match(rule, message[position:])
     if match:
         return True, position + match.end()
     return False, position
 
 
-def isMatch(firstRule: str, secondRule: str, message: str) -> bool:
+def is_match(first_rule: str, second_rule: str, message: str) -> bool:
     count = 0
-    matched, position = isInnerMatch(firstRule, message, 0)
-    while matched and position <= len(message):
-        lastPosition = position
+    matched, position = is_inner_match(first_rule, message, 0)
+    while matched and position < len(message):
+        last_position = position
         for _ in range(count):
-            matched, position = isInnerMatch(secondRule, message, position)
+            matched, position = is_inner_match(second_rule, message, position)
             if not matched:
-                position = lastPosition
+                position = last_position
                 break
             elif position == len(message):
                 return True
         count += 1
-        matched, position = isInnerMatch(firstRule, message, position)
+        matched, position = is_inner_match(first_rule, message, position)
     return False
 
 
-def solve(puzzleInput: Tuple[Dict[int, Rule], List[str]]) -> Tuple[int, int]:
-    rules, messages = puzzleInput
-    rule0 = generateRegex(rules, 0)
-    rule42 = generateRegex(rules, 42)
-    rule31 = generateRegex(rules, 31)
+def solve(puzzle_input: Tuple[Dict[int, Rule], List[str]]) -> Tuple[int, int]:
+    rules, messages = puzzle_input
+    rule_0 = generate_regex(rules, 0)
+    rule_42 = "^" + generate_regex(rules, 42)
+    rule_31 = "^" + generate_regex(rules, 31)
     return (
-        sum(1 for message in messages if re.fullmatch(rule0, message)),
-        sum(1 for message in messages if isMatch(rule42, rule31, message))
+        sum(1 for message in messages if re.fullmatch(rule_0, message)),
+        sum(1 for message in messages if is_match(rule_42, rule_31, message))
     )
 
 
-ruleRegex = re.compile(r"(?P<number>^\d+):\s(?P<rule>.+)$")
+rule_regex = re.compile(r"(?P<number>^\d+):\s(?P<rule>.+)$")
 
 
-def getInput(filePath: str) -> Tuple[Dict[int, Rule], List[str]]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
+def get_input(file_path: str) -> Tuple[Dict[int, Rule], List[str]]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
 
-    with open(filePath, "r") as file:
+    with open(file_path, "r") as file:
         rules: Dict[int, Rule] = {}
         messages: List[str] = []
         for line in file.readlines():
             if line.strip() != "":
-                match = ruleRegex.match(line)
+                match = rule_regex.match(line)
                 if match:
                     rules[int(match.group("number"))] = Rule(
                         match.group("number"), match.group("rule"))
@@ -100,10 +102,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
