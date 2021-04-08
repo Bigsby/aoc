63a17c956ebd25facb46c109ee56d4676dd1a28f
Bruno Bernardo <bigsby@live.com>
2021-04-08 11:23:32 +0100

    2016/20 py clean up

--- 2016/20/py/run.py
+++ 2016/20/py/run.py
@@ -1,38 +1,40 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import List, Tuple
 
-Range = Tuple[int,int]
+Range = Tuple[int, int]
 
 
-def solve(ranges: List[Range]) -> Tuple[int,int]:
-    part1Result = 0
+def solve(ranges: List[Range]) -> Tuple[int, int]:
+    part1_result = 0
     ranges.sort()
-    previousUpper = 0
-    allowedCount = 0
+    previous_upper = 0
+    allowed_count = 0
     for lower, upper in ranges:
-        if upper <= previousUpper:
+        if upper <= previous_upper:
             continue
-        if lower > previousUpper + 1:
-            allowedCount += lower - previousUpper - 1
-            if part1Result == 0:
-                part1Result = previousUpper + 1
-        previousUpper = upper
-    return part1Result, allowedCount
+        if lower > previous_upper + 1:
+            allowed_count += lower - previous_upper - 1
+            if part1_result == 0:
+                part1_result = previous_upper + 1
+        previous_upper = upper
+    return part1_result, allowed_count
 
 
-def parseLine(line: str) -> Range:
+def parse_line(line: str) -> Range:
     splits = line.strip().split("-")
     return int(splits[0]), int(splits[1])
 
 
-def getInput(filePath: str) -> List[Range]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
-        return [ parseLine(line) for line in file.readlines() ]
+def get_input(file_path: str) -> List[Range]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
+        return [parse_line(line) for line in file.readlines()]
 
 
 def main():
@@ -40,7 +42,7 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1Result, part2Result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
     print("P1:", part1Result)
     print("P2:", part2Result)
