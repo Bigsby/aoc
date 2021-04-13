343178597bebd49214ded5f5ae43dc5ea013d142
Bruno Bernardo <bigsby@live.com>
2021-04-13 14:23:32 +0100

    2019/20 py clean up

--- 2019/20/py/run.py
+++ 2019/20/py/run.py
@@ -18,60 +18,60 @@ def part1(data: Tuple[Maze, Portals, Position, Position]) -> int:
     queue: List[Tuple[Position, int]] = [(start, 1)]
     while queue:
         position, distance = queue.pop(0)
-        newPositions: List[Position] = []
+        new_positions: List[Position] = []
         if position in portals:
-            newPositions.append(next(p for p, label in portals.items(
+            new_positions.append(next(p for p, label in portals.items(
             ) if label == portals[position] and p != position))
         for direction in DIRECTIONS:
-            newPositions.append(position + direction)
-        for newPosition in newPositions:
-            if newPosition == end:
+            new_positions.append(position + direction)
+        for new_position in new_positions:
+            if new_position == end:
                 return distance
-            if newPosition not in visited and newPosition in maze:
-                visited.add(newPosition)
-                queue.append((newPosition, distance + 1))
+            if new_position not in visited and new_position in maze:
+                visited.add(new_position)
+                queue.append((new_position, distance + 1))
     raise Exception("Path not found")
 
 
 def part2(data: Tuple[Maze, Portals, Position, Position]) -> int:
     maze, portals, start, end = data
-    minX = min(p.real for p in maze)
-    maxX = max(p.real for p in maze)
-    minY = min(p.imag for p in maze)
-    maxY = max(p.imag for p in maze)
-    outerPortals = {position: label for position, label in portals.items()
-                    if position.real == minX or position.real == maxX or
-                    position.imag == minY or position.imag == maxY}
-    innerPortals = {position: label for position,
-                    label in portals.items() if position not in outerPortals}
-    reverseOuterPortals = {label: position for position,
-                           label in outerPortals.items()}
-    reverseInnerPortals = {label: position for position,
-                           label in innerPortals.items()}
-    endPosition = (end, 0)
-    startPosition = (start, 0)
-    visited: Set[LevelPosition] = {startPosition}
+    min_x = min(p.real for p in maze)
+    max_x = max(p.real for p in maze)
+    min_y = min(p.imag for p in maze)
+    max_y = max(p.imag for p in maze)
+    outer_portals = {position: label for position, label in portals.items()
+                    if position.real == min_x or position.real == max_x or
+                    position.imag == min_y or position.imag == max_y}
+    inner_portals = {position: label for position,
+                    label in portals.items() if position not in outer_portals}
+    reverse_outer_portals = {label: position for position,
+                           label in outer_portals.items()}
+    reverse_inner_portals = {label: position for position,
+                           label in inner_portals.items()}
+    end_position = (end, 0)
+    start_position = (start, 0)
+    visited: Set[LevelPosition] = {start_position}
     queue: List[Tuple[int, float, float, int]] = [
-        (1, startPosition[0].real, startPosition[0].imag, startPosition[1])]
+        (1, start_position[0].real, start_position[0].imag, start_position[1])]
     while queue:
         distance, x, y, level = queue.pop(0)
         position = x + y * 1j
-        newPositions: List[LevelPosition] = []
-        if position in innerPortals:
-            newPositions.append(
-                (reverseOuterPortals[innerPortals[position]], level + 1))
-        elif level != 0 and position in outerPortals:
-            newPositions.append(
-                (reverseInnerPortals[outerPortals[position]], level - 1))
+        new_positions: List[LevelPosition] = []
+        if position in inner_portals:
+            new_positions.append(
+                (reverse_outer_portals[inner_portals[position]], level + 1))
+        elif level != 0 and position in outer_portals:
+            new_positions.append(
+                (reverse_inner_portals[outer_portals[position]], level - 1))
         for direction in DIRECTIONS:
-            newPositions.append((position + direction, level))
-        for newPosition in newPositions:
-            if newPosition == endPosition:
+            new_positions.append((position + direction, level))
+        for new_position in new_positions:
+            if new_position == end_position:
                 return distance
-            if newPosition not in visited and newPosition[0] in maze:
-                visited.add(newPosition)
+            if new_position not in visited and new_position[0] in maze:
+                visited.add(new_position)
                 queue.append(
-                    (distance + 1, newPosition[0].real, newPosition[0].imag, newPosition[1]))
+                    (distance + 1, new_position[0].real, new_position[0].imag, new_position[1]))
     raise Exception("Path not found")
 
 
@@ -82,13 +82,13 @@ def solve(data: Tuple[Maze, Portals, Position, Position]) -> Tuple[int, int]:
     )
 
 
-def getInput(filePath: str) -> Tuple[Maze, Portals, Position, Position]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
+def get_input(file_path: str) -> Tuple[Maze, Portals, Position, Position]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
 
-    with open(filePath, "r") as file:
-        maze = Maze()
-        portals = Portals()
+    with open(file_path, "r") as file:
+        maze: Maze = []
+        portals: Portals = {}
         start = 0j
         end = 0j
         lines = file.readlines()
@@ -122,10 +122,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
