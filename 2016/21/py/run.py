ea80840eff6f06d71f8813f9e85682231fe9c9ef
Bruno Bernardo <bigsby@live.com>
2021-04-13 23:04:59 +0100

    2016/21 py clean up

--- 2016/21/py/run.py
+++ 2016/21/py/run.py
@@ -1,10 +1,12 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import Deque, List, Tuple
 from collections import deque
 
-Instruction = Tuple[int,int,int]
+Instruction = Tuple[int, int, int]
 SWAP_POSITION = 0
 SWAP_LETTER = 1
 ROTATE_LEFT = 2
@@ -18,46 +20,48 @@ def process(start: str, instructions: List[Instruction], reverse: bool = False) 
     password: Deque[int] = deque(ord(value) for value in start)
     if reverse:
         instructions.reverse()
-    for opCode, a, b in instructions:
-        if opCode == SWAP_POSITION:
-            oldA = password[a]
+    for op_code, a, b in instructions:
+        if op_code == SWAP_POSITION:
+            old_a = password[a]
             password[a] = password[b]
-            password[b] = oldA
-        elif opCode == SWAP_LETTER:
-            indexOfA = password.index(a)
-            indexOfB = password.index(b)
-            oldA = password[indexOfA]
-            password[indexOfA] = password[indexOfB]
-            password[indexOfB] = oldA
-        elif opCode == ROTATE_LEFT:
+            password[b] = old_a
+        elif op_code == SWAP_LETTER:
+            index_of_a = password.index(a)
+            index_of_b = password.index(b)
+            old_a = password[index_of_a]
+            password[index_of_a] = password[index_of_b]
+            password[index_of_b] = old_a
+        elif op_code == ROTATE_LEFT:
             password.rotate(a if reverse else -a)
-        elif opCode == ROTATE_RIGHT:
+        elif op_code == ROTATE_RIGHT:
             password.rotate(-a if reverse else a)
-        elif opCode == ROTATE_LETTER:
-            indexOfA = password.index(a)
-            rotation = indexOfA + 1 + (1 if indexOfA >= 4 else 0)
+        elif op_code == ROTATE_LETTER:
+            index_of_a = password.index(a)
+            rotation = index_of_a + 1 + (1 if index_of_a >= 4 else 0)
             if reverse:
-                rotation = -(indexOfA // 2 + (1 if indexOfA % 2 == 1 or indexOfA == 0 else 5))
+                rotation = -(index_of_a // 2 + (1 if index_of_a %
+                             2 == 1 or index_of_a == 0 else 5))
             password.rotate(rotation)
-        elif opCode == REVERSE:
-            passwordList = list(password)
-            password = deque(passwordList[:a] + passwordList[a:b + 1][::-1] + passwordList[b + 1:])
-        elif opCode == MOVE:
+        elif op_code == REVERSE:
+            password_list = list(password)
+            password = deque(
+                password_list[:a] + password_list[a:b + 1][::-1] + password_list[b + 1:])
+        elif op_code == MOVE:
             origin, destination = (b, a) if reverse else (a, b)
-            letterToMove = password[origin]
-            password.remove(letterToMove)
-            password.insert(destination, letterToMove)
+            letter_to_move = password[origin]
+            password.remove(letter_to_move)
+            password.insert(destination, letter_to_move)
     return "".join(chr(c) for c in password)
 
 
-def solve(instructions: List[Instruction]) -> Tuple[str,str]:
+def solve(instructions: List[Instruction]) -> Tuple[str, str]:
     return (
         process("abcdefgh", instructions),
         process("fbgdceah", instructions, True)
     )
 
 
-def parseLine(line: str) -> Instruction:
+def parse_line(line: str) -> Instruction:
     if line.startswith("swap position"):
         return (SWAP_POSITION, int(line[14]), int(line[-1]))
     elif line.startswith("swap letter"):
@@ -75,12 +79,12 @@ def parseLine(line: str) -> Instruction:
     raise Exception("Unknow instruction", line)
 
 
-def getInput(filePath: str) -> List[Instruction]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
-        return [ parseLine(line.strip()) for line in file.readlines() ]
+def get_input(file_path: str) -> List[Instruction]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
+        return [parse_line(line.strip()) for line in file.readlines()]
 
 
 def main():
@@ -88,10 +92,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
