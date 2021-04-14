48b8703db5628b8ba864c33b45574957158b2348
Bruno Bernardo <bigsby@live.com>
2021-04-14 15:38:40 +0100

    2020/21 py clean up

--- 2020/21/py/run.py
+++ 2020/21/py/run.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import Dict, List, Set, Tuple
 import re
 from functools import reduce
@@ -15,7 +17,7 @@ class Food():
         return " ".join(self.ingredients) + " (contains " + ", ".join(self.allergens) + ")"
 
 
-def getAllergens(foods: List[Food]) -> Set[str]:
+def get_allergens(foods: List[Food]) -> Set[str]:
     allergens: Set[str] = set()
     for food in foods:
         for allergen in food.allergens:
@@ -23,60 +25,63 @@ def getAllergens(foods: List[Food]) -> Set[str]:
     return allergens
 
 
-def buildAllergenGraph(foods: List[Food]) -> Dict[str,Set[str]]:
-    allergens = getAllergens(foods)
+def build_allergen_graph(foods: List[Food]) -> Dict[str, Set[str]]:
+    allergens = get_allergens(foods)
     return {
-        allergen: reduce(lambda soFar, foodAllergens: soFar & foodAllergens, \
-            (set(food.ingredients) for food in foods if allergen in food.allergens)) \
-            for allergen in allergens
+        allergen: reduce(lambda so_far, food_allergens: so_far & food_allergens,
+                         (set(food.ingredients) for food in foods if allergen in food.allergens))
+        for allergen in allergens
     }
 
 
-def part1(foods: List[Food], allergenGraph: Dict[str,Set[str]]) -> int:
-    foundIngredients = reduce(lambda soFar, ingredientsForAllergen: \
-        soFar | ingredientsForAllergen, (graph for _, graph in allergenGraph.items()))
+def part1(foods: List[Food], allergen_graph: Dict[str, Set[str]]) -> int:
+    found_ingredients = reduce(lambda so_far, ingredients_for_allergen:
+                               so_far | ingredients_for_allergen, allergen_graph.values())
     count = 0
     for food in foods:
         for ingredient in food.ingredients:
-            count += ingredient not in foundIngredients
+            count += ingredient not in found_ingredients
     return count
 
 
-def part2(allergenGraph: Dict[str,Set[str]]) -> str:
-    while any(len(ingredients) != 1 for ingredients in allergenGraph.values()):
-        singleIngredientAllergens = [  
-            (allergen, next(iter(allergenGraph[allergen]))) \
-            for allergen in allergenGraph if len(allergenGraph[allergen]) == 1 
+def part2(allergen_graph: Dict[str, Set[str]]) -> str:
+    while any(len(ingredients) != 1 for ingredients in allergen_graph.values()):
+        single_ingredient_allergens = [
+            (allergen, next(iter(allergen_graph[allergen])))
+            for allergen in allergen_graph if len(allergen_graph[allergen]) == 1
         ]
-        for singleAllergen, ingredient in singleIngredientAllergens:
-            for allergen in allergenGraph:
-                if allergen != singleAllergen and ingredient in allergenGraph[allergen]:
-                    allergenGraph[allergen].remove(ingredient)
-    return ",".join(map(lambda k: next(iter(allergenGraph[k])), sorted(allergenGraph)))
+        for single_allergen, ingredient in single_ingredient_allergens:
+            for allergen in allergen_graph:
+                if allergen != single_allergen and ingredient in allergen_graph[allergen]:
+                    allergen_graph[allergen].remove(ingredient)
+    return ",".join(map(lambda k: next(iter(allergen_graph[k])), sorted(allergen_graph)))
 
 
-def solve(foods: List[Food]) -> Tuple[int,str]:
-    allergenGraph = buildAllergenGraph(foods)
+def solve(foods: List[Food]) -> Tuple[int, str]:
+    allergen_graph = build_allergen_graph(foods)
     return (
-        part1(foods, allergenGraph),
-        part2(allergenGraph)
+        part1(foods, allergen_graph),
+        part2(allergen_graph)
     )
 
 
-lineRegex = re.compile(r"^(?P<ingredients>[^\(]+)\s\(contains\s(?P<allergens>[^\)]+)\)$")
-def parseLine(line: str) -> Food:
-    match = lineRegex.match(line)
+line_regex = re.compile(
+    r"^(?P<ingredients>[^\(]+)\s\(contains\s(?P<allergens>[^\)]+)\)$")
+
+
+def parse_line(line: str) -> Food:
+    match = line_regex.match(line)
     if match:
         return Food(match.group("ingredients").split(" "), match.group("allergens").split(", "))
     raise Exception("Bad format", line)
 
 
-def getInput(filePath: str) -> List[Food]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
-        return [ parseLine(line) for line in file.readlines() ]
+def get_input(file_path: str) -> List[Food]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+
+    with open(file_path, "r") as file:
+        return [parse_line(line) for line in file.readlines()]
 
 
 def main():
@@ -84,10 +89,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
