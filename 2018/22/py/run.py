ac19d4eea3f70aa476d068c00ad322d2a99a2515
Bruno Bernardo <bigsby@live.com>
2021-04-14 18:57:31 +0100

    2018/22 py clean up

--- 2018/22/py/run.py
+++ 2018/22/py/run.py
@@ -14,7 +14,7 @@ GEOLOGIC_Y_CONSTANT = 48271
 EROSION_CONSTANT = 20183
 
 
-def getGeologicIndex(coordinate: Coordinate, depth: int, target: Coordinate, calculated: Dict[Coordinate, int]) -> int:
+def get_geologic_index(coordinate: Coordinate, depth: int, target: Coordinate, calculated: Dict[Coordinate, int]) -> int:
     if coordinate == 0 or coordinate == target:
         return 0
     x, y = int(coordinate.real), int(coordinate.imag)
@@ -22,72 +22,73 @@ def getGeologicIndex(coordinate: Coordinate, depth: int, target: Coordinate, cal
         return y * GEOLOGIC_Y_CONSTANT
     elif y == 0:
         return x * GEOLOGIC_X_CONSTANT
-    return getErosionLevel(x - 1 + y * 1j, depth, target, calculated) \
-        * getErosionLevel(x + (y - 1) * 1j, depth, target, calculated)
+    return get_erosion_level(x - 1 + y * 1j, depth, target, calculated) \
+        * get_erosion_level(x + (y - 1) * 1j, depth, target, calculated)
 
 
-def getErosionLevel(coordinate: Coordinate, depth: int, target: Coordinate, calculated: Dict[Coordinate, int]) -> int:
+def get_erosion_level(coordinate: Coordinate, depth: int, target: Coordinate, calculated: Dict[Coordinate, int]) -> int:
     if coordinate not in calculated:
-        calculated[coordinate] = (getGeologicIndex(
+        new_value = (get_geologic_index(
             coordinate, depth, target, calculated) + depth) % EROSION_CONSTANT
+        calculated[coordinate] = new_value
     return calculated[coordinate]
 
 
-def getRisk(coordinate: Coordinate, depth: int, target: Coordinate, calculated: Dict[Coordinate, int]) -> int:
-    return getErosionLevel(coordinate, depth, target, calculated) % 3
+def get_risk(coordinate: Coordinate, depth: int, target: Coordinate, calculated: Dict[Coordinate, int]) -> int:
+    return get_erosion_level(coordinate, depth, target, calculated) % 3
 
 
-def part1(data: Tuple[int, int, int]) -> int:
-    depth, targetX, targetY = data
-    target = targetX + targetY * 1j
+def part1(data: Tuple[int, ...]) -> int:
+    depth, target_x, target_y = data
+    target = target_x + target_y * 1j
     calculated: Dict[Coordinate, int] = {}
-    return sum(getRisk(x + y * 1j, depth, target, calculated)
-               for x, y in product(range(targetX + 1), range(targetY + 1)))
+    return sum(get_risk(x + y * 1j, depth, target, calculated)
+               for x, y in product(range(target_x + 1), range(target_y + 1)))
 
 
 DIRECTIONS = [1, 1j, -1, -1j]
 
 
-def part2(data: Tuple[int, int, int]) -> int:
+def part2(data: Tuple[int, ...]) -> int:
     depth, x, y = data
     target = x + y * 1j
     calculated: Dict[Coordinate, int] = {}
     final = (x, y, 1)
     queue = [(0, 0, 0, 1)]  # 1 = torch, 0 neither, 2 climbing
-    bestTimes: Dict[Tuple[int, int, int], int] = dict()
+    best_times: Dict[Tuple[int, int, int], int] = dict()
     while queue:
         duration, x, y, risk = heappop(queue)
         coordinate = x + y * 1j
         state = (x, y, risk)
-        if state in bestTimes and bestTimes[state] <= duration:
+        if state in best_times and best_times[state] <= duration:
             continue
         if state == final:
             return duration
-        bestTimes[state] = duration
+        best_times[state] = duration
         for tool in range(3):
-            if tool != risk and tool != getRisk(coordinate, depth, target, calculated):
+            if tool != risk and tool != get_risk(coordinate, depth, target, calculated):
                 heappush(queue, (duration + 7, x, y, tool))
         for direction in DIRECTIONS:
-            newCoordinate = coordinate + direction
-            if newCoordinate.real >= 0 and newCoordinate.imag >= 0 \
-                    and getRisk(newCoordinate, depth, target, calculated) != risk:
-                heappush(queue, (duration + 1, int(newCoordinate.real),
-                                 int(newCoordinate.imag), risk))
+            new_coordinate = coordinate + direction
+            if new_coordinate.real >= 0 and new_coordinate.imag >= 0 \
+                    and get_risk(new_coordinate, depth, target, calculated) != risk:
+                heappush(queue, (duration + 1, int(new_coordinate.real),
+                                 int(new_coordinate.imag), risk))
     raise Exception("Path not found")
 
 
-def solve(data: Tuple[int, int, int]) -> Tuple[int, int]:
+def solve(data: Tuple[int, ...]) -> Tuple[int, int]:
     return (
         part1(data),
         part2(data)
     )
 
 
-def getInput(filePath: str) -> Tuple[int, int, int]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
+def get_input(file_path: str) -> Tuple[int, ...]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
 
-    with open(filePath, "r") as file:
+    with open(file_path, "r") as file:
         return tuple(map(int, re.findall(r"\d+", file.read())))
 
 
@@ -96,10 +97,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
