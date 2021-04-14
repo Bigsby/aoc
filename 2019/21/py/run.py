ce19aaba632dd6730289dd94dcd4383572708d07
Bruno Bernardo <bigsby@live.com>
2021-04-14 12:23:22 +0100

    2019/21 py clean up

--- 2019/21/py/run.py
+++ 2019/21/py/run.py
@@ -22,12 +22,12 @@ class IntCodeComputer():
     def setInput(self, value: int):
         self.inputs.insert(0, value)
 
-    def runUntilHalt(self) -> int:
+    def run(self) -> int:
         while self.running:
             self.tick()
-        return self.getOutput()
+        return self.get_output()
 
-    def getParameter(self, offset: int, mode: int) -> int:
+    def get_parameter(self, offset: int, mode: int) -> int:
         value = self.memory[self.pointer + offset]
         if mode == 0:  # POSITION
             return self.memory[value]
@@ -37,7 +37,7 @@ class IntCodeComputer():
             return self.memory[self.base + value]
         raise Exception("Unrecognized parameter mode", mode)
 
-    def getAddress(self, offset: int, mode: int) -> int:
+    def get_address(self, offset: int, mode: int) -> int:
         value = self.memory[self.pointer + offset]
         if mode == 0:  # POSITION
             return value
@@ -45,85 +45,85 @@ class IntCodeComputer():
             return self.base + value
         raise Exception("Unrecognized address mode", mode)
 
-    def getOutput(self) -> int:
+    def get_output(self) -> int:
         self.outputing = False
         return self.outputs.pop()
 
-    def addInput(self, value: int):
+    def add_input(self, value: int):
         self.inputs.append(value)
 
     def tick(self):
         instruction = self.memory[self.pointer]
-        opcode, p1mode, p2mode, p3mode = instruction % 100, (
+        opcode, p1_mode, p2_mode, p3_mode = instruction % 100, (
             instruction // 100) % 10, (instruction // 1000) % 10, (instruction // 10000) % 10
         if not self.running:
             return
         if opcode == 1:  # ADD
-            self.memory[self.getAddress(3, p3mode)] = self.getParameter(
-                1, p1mode) + self.getParameter(2, p2mode)
+            self.memory[self.get_address(3, p3_mode)] = self.get_parameter(
+                1, p1_mode) + self.get_parameter(2, p2_mode)
             self.pointer += 4
         elif opcode == 2:  # MUL
-            self.memory[self.getAddress(3, p3mode)] = self.getParameter(
-                1, p1mode) * self.getParameter(2, p2mode)
+            self.memory[self.get_address(3, p3_mode)] = self.get_parameter(
+                1, p1_mode) * self.get_parameter(2, p2_mode)
             self.pointer += 4
         elif opcode == 3:  # INPUT
             if self.inputs:
                 self.polling = False
-                self.memory[self.getAddress(1, p1mode)] = self.inputs.pop(0)
+                self.memory[self.get_address(1, p1_mode)] = self.inputs.pop(0)
                 self.pointer += 2
             else:
                 self.polling = True
         elif opcode == 4:  # OUTPUT
             self.outputing = True
-            self.outputs.append(self.getParameter(1, p1mode))
+            self.outputs.append(self.get_parameter(1, p1_mode))
             self.pointer += 2
         elif opcode == 5:  # JMP_TRUE
-            if self.getParameter(1, p1mode):
-                self.pointer = self.getParameter(2, p2mode)
+            if self.get_parameter(1, p1_mode):
+                self.pointer = self.get_parameter(2, p2_mode)
             else:
                 self.pointer += 3
         elif opcode == 6:  # JMP_FALSE
-            if not self.getParameter(1, p1mode):
-                self.pointer = self.getParameter(2, p2mode)
+            if not self.get_parameter(1, p1_mode):
+                self.pointer = self.get_parameter(2, p2_mode)
             else:
                 self.pointer += 3
         elif opcode == 7:  # LESS_THAN
-            self.memory[self.getAddress(3, p3mode)] = 1 if self.getParameter(
-                1, p1mode) < self.getParameter(2, p2mode) else 0
+            self.memory[self.get_address(3, p3_mode)] = 1 if self.get_parameter(
+                1, p1_mode) < self.get_parameter(2, p2_mode) else 0
             self.pointer += 4
         elif opcode == 8:  # EQUALS
-            self.memory[self.getAddress(3, p3mode)] = 1 if self.getParameter(
-                1, p1mode) == self.getParameter(2, p2mode) else 0
+            self.memory[self.get_address(3, p3_mode)] = 1 if self.get_parameter(
+                1, p1_mode) == self.get_parameter(2, p2_mode) else 0
             self.pointer += 4
         elif opcode == 9:  # SET_BASE
-            self.base += self.getParameter(1, p1mode)
+            self.base += self.get_parameter(1, p1_mode)
             self.pointer += 2
         elif opcode == 99:  # HALT
             self.running = False
         else:
             raise Exception(f"Unknown instruction", self.pointer,
-                            instruction, opcode, p1mode, p2mode, p3mode)
+                            instruction, opcode, p1_mode, p2_mode, p3_mode)
 
 
-def runDroid(memory: List[int], instructions: List[str]) -> int:
+def run_droid(memory: List[int], instructions: List[str]) -> int:
     droid = IntCodeComputer(memory)
     for instruction in instructions:
         for c in instruction:
             droid.inputs.append(ord(c))
         droid.inputs.append(10)
-    return droid.runUntilHalt()
+    return droid.run()
 
 
 def solve(memory: List[int]) -> Tuple[int, int]:
     return (
-        runDroid(memory, [
+        run_droid(memory, [
             "NOT C J",
             "AND D J",
             "NOT A T",
             "OR T J",
             "WALK"
         ]),
-        runDroid(memory, [
+        run_droid(memory, [
             "OR E J",
             "OR H J",
             "AND D J",
@@ -138,11 +138,11 @@ def solve(memory: List[int]) -> Tuple[int, int]:
     )
 
 
-def getInput(filePath: str) -> List[int]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
+def get_input(file_path: str) -> List[int]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
 
-    with open(filePath, "r") as file:
+    with open(file_path, "r") as file:
         return [int(i) for i in file.read().split(",")]
 
 
@@ -151,10 +151,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
