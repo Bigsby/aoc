56b5bbedbf472af8b13818c29d91b20bf5c9cc8c
Bruno Bernardo <bigsby@live.com>
2021-04-15 17:17:51 +0100

    2015/23 py clean up

--- 2015/23/py/run.py
+++ 2015/23/py/run.py
@@ -7,7 +7,7 @@ HLF, TPL, INC, JMP, JIE, JIO = 0, 1, 2, 3, 4, 5
 Instruction = Tuple[int,str,int]
 
 
-def runProgram(instructions: List[Instruction], registers: Dict[str,int] = {}) -> int:
+def run_program(instructions: List[Instruction], registers: Dict[str,int] = {}) -> int:
     registers = { **{ "a": 0, "b": 0 }, **registers }
     pointer = 0
     while pointer < len(instructions):
@@ -33,11 +33,11 @@ def runProgram(instructions: List[Instruction], registers: Dict[str,int] = {}) -
 
 def solve(instructions: List[Instruction]) -> Tuple[int,int]:
     return (
-        runProgram(instructions),
-        runProgram(instructions, { "a": 1 })
+        run_program(instructions),
+        run_program(instructions, { "a": 1 })
     )
 
-def parseLine(line: str) -> Instruction:
+def parse_line(line: str) -> Instruction:
     if line.startswith("hlf"):
         return HLF, line.split(" ")[1], 0
     elif line.startswith("tpl"):
@@ -53,12 +53,12 @@ def parseLine(line: str) -> Instruction:
     raise Exception("Bad format", line)
 
 
-def getInput(filePath: str) -> List[Instruction]:
+def get_input(filePath: str) -> List[Instruction]:
     if not os.path.isfile(filePath):
         raise FileNotFoundError(filePath)
     
     with open(filePath, "r") as file:
-        return [ parseLine(line.strip()) for line in file.readlines() ]
+        return [ parse_line(line.strip()) for line in file.readlines() ]
 
 
 def main():
@@ -66,7 +66,7 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1Result, part2Result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
     print("P1:", part1Result)
     print("P2:", part2Result)
