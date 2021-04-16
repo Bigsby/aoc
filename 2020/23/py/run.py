fe811c69750897753f782a81efe279f266b16339
Bruno Bernardo <bigsby@live.com>
2021-04-16 13:24:36 +0100

    2020/23/ py clean up

--- 2020/23/py/run.py
+++ 2020/23/py/run.py
@@ -12,7 +12,7 @@ class Node:
         self.value = value
 
 
-def buildLinkedList(cups: List[int]) -> Tuple[Node, List[Node]]:
+def build_linked_list(cups: List[int]) -> Tuple[Node, List[Node]]:
     start = previous = last = Node(cups[0])
     values = [start] * len(cups)
     values[start.value - 1] = start
@@ -25,44 +25,45 @@ def buildLinkedList(cups: List[int]) -> Tuple[Node, List[Node]]:
     return start, values
 
 
-def playGame(cups: List[int], moves: int) -> Node:
-    start, values = buildLinkedList(cups)
-    maxValue = max(cups)
+def play_game(cups: List[int], moves: int) -> Node:
+    start, values = build_linked_list(cups)
+    max_value = max(cups)
     current = start
     while moves:
         moves -= 1
-        firstRemoved = current.next
-        lastRemoved = firstRemoved.next.next
-        removedValues = {firstRemoved.value,
-                         firstRemoved.next.value, lastRemoved.value}
-        current.next = lastRemoved.next
-        destinationValue = current.value - 1
-        while destinationValue in removedValues or destinationValue < 1:
-            destinationValue -= 1
-            if destinationValue < 0:
-                destinationValue = maxValue
+        first_removed = current.next
+        last_removed = first_removed.next.next
+        removed_values = {first_removed.value,
+                          first_removed.next.value, last_removed.value}
+        current.next = last_removed.next
+        destination_value = current.value - 1
+        while destination_value in removed_values or destination_value < 1:
+            destination_value -= 1
+            if destination_value < 0:
+                destination_value = max_value
 
-        destinationLink = values[destinationValue - 1]
-        lastRemoved.next = destinationLink.next
-        destinationLink.next = firstRemoved
+        destination_link = values[destination_value - 1]
+        last_removed.next = destination_link.next
+        destination_link.next = first_removed
         current = current.next
     return values[0]
 
 
 def part1(cups: List[int]) -> str:
-    oneNode = playGame(cups, 100)
+    one_node = play_game(cups, 100)
     result: List[int] = []
-    currentNode = oneNode.next
-    while currentNode.value != 1:
-        result.append(currentNode.value)
-        currentNode = currentNode.next
+    current_node = one_node.next
+    while current_node.value != 1:
+        result.append(current_node.value)
+        current_node = current_node.next
     return "".join(map(str, result))
 
 
 def part2(cups: List[int]) -> int:
     cups += [*range(10, 10**6 + 1)]
-    oneNode = playGame(cups, 10**7)
-    return oneNode.next.value * oneNode.next.next.value
+    one_node = play_game(cups, 10**7)
+    print(one_node.next.value, one_node.next.next.value)
+    return one_node.next.value * one_node.next.next.value
 
 
 def solve(cups: List[int]) -> Tuple[str, int]:
@@ -72,11 +73,11 @@ def solve(cups: List[int]) -> Tuple[str, int]:
     )
 
 
-def getInput(filePath: str) -> List[int]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
+def get_input(file_path: str) -> List[int]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
 
-    with open(filePath, "r") as file:
+    with open(file_path, "r") as file:
         return [int(c) for c in file.read().strip()]
 
 
@@ -85,10 +86,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
