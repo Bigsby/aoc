dedc2bf40df61045e436c78c8cc7525cdb2de4e5
Bruno Bernardo <bigsby@live.com>
2021-04-20 16:25:17 +0100

    2018/24 py clean up

--- 2018/24/py/run.py
+++ 2018/24/py/run.py
@@ -13,7 +13,7 @@ class Group:
                  inititive: int, type: str, damage: int, army: int):
         self.id = id
         self.units = units
-        self.hitPoints = hit
+        self.hit_points = hit
         self.immunities = immunities
         self.weaknesses = weaknesses
         self.initiative = inititive
@@ -22,51 +22,54 @@ class Group:
         self.army = army
         self.target: Optional['Group'] = None
 
-    def effectivePower(self):
+    def effective_power(self):
         return self.units * self.damage
 
-    def damageTo(self, target: 'Group'):
+    def damage_to(self, target: 'Group'):
         if self.type in target.immunities:
             return 0
-        return self.effectivePower() * (2 if self.type in target.weaknesses else 1)
+        return self.effective_power() * (2 if self.type in target.weaknesses else 1)
 
-    def Clone(self, boost: int) -> 'Group':
-        return Group(self.id, self.units, self.hitPoints, self.immunities, self.weaknesses, self.initiative, self.type, self.damage + boost, self.army)
+    def clone(self, boost: int) -> 'Group':
+        return Group(self.id, self.units, self.hit_points, self.immunities, self.weaknesses, self.initiative, self.type, self.damage + boost, self.army)
+
+    def __str__(self) -> str:
+        return f"{self.id} {self.units} {self.hit_points} {self.damage} {self.effective_power()} {self.type} ({self.immunities}) ({self.weaknesses})"
 
 
 def combat(groups: List[Group], boost: int) -> Tuple[int, int]:
-    groups = [group.Clone(boost if group.army == 0 else 0) for group in groups]
+    groups = [group.clone(boost if group.army == 0 else 0) for group in groups]
     while True:
         groups = sorted(
-            groups, key=lambda u: (-u.effectivePower(), -u.initiative))
-        selectedTargets: Set[str] = set()
+            groups, key=lambda u: (-u.effective_power(), -u.initiative))
+        selected_targets: Set[str] = set()
         for group in groups:
             targets = [target for target in groups
-                       if target.army != group.army and target.id not in selectedTargets and group.damageTo(target) > 0]
+                       if target.army != group.army and target.id not in selected_targets and group.damage_to(target) > 0]
             if targets:
-                targets.sort(key=lambda target: (-group.damageTo(target), -
-                                                 target.effectivePower(), -target.initiative))
+                targets.sort(key=lambda target: (-group.damage_to(target), -
+                                                 target.effective_power(), -target.initiative))
                 group.target = targets[0]
-                selectedTargets.add(group.target.id)
+                selected_targets.add(group.target.id)
         groups = sorted(groups, key=lambda group: -group.initiative)
-        unitsKilled = False
+        units_killed = False
         for group in groups:
             if group.target:
-                killed = min(group.target.units, group.damageTo(
-                    group.target) // group.target.hitPoints)
-                unitsKilled |= killed > 0
+                killed = min(group.target.units, group.damage_to(
+                    group.target) // group.target.hit_points)
+                units_killed |= killed > 0
                 group.target.units -= killed
         groups = [group for group in groups if group.units > 0]
         for group in groups:
             group.target = None
-        immuneSystemUnits = sum(
+        immune_system_units = sum(
             [group.units for group in groups if group.army == 0])
-        infectionUnits = sum(
+        infection_units = sum(
             [group.units for group in groups if group.army == 1])
-        if not unitsKilled or immuneSystemUnits == 0:
-            return 1, infectionUnits
-        if infectionUnits == 0:
-            return 0, immuneSystemUnits
+        if not units_killed or immune_system_units == 0:
+            return 1, infection_units
+        if infection_units == 0:
+            return 0, immune_system_units
 
 
 def solve(groups: List[Group]) -> Tuple[int, int]:
@@ -78,19 +81,19 @@ def solve(groups: List[Group]) -> Tuple[int, int]:
             return combat(groups, 0)[1], left
 
 
-numbersRegex = re.compile(
+numbers_regex = re.compile(
     r"^(?P<units>\d+) units.*with (?P<hit>\d+) hit.*does (?P<damage>\d+) (?P<type>\w+).*initiative (?P<initiative>\d+)$")
-immunityWeaknessRegex = re.compile(r"\(.*\)")
+immunity_weakness_regex = re.compile(r"\(.*\)")
 
 
-def parseGroup(text: str, army: int, number: int) -> Group:
-    numberMatch = numbersRegex.match(text)
-    if numberMatch:
+def parse_group(text: str, army: int, number: int) -> Group:
+    number_match = numbers_regex.match(text)
+    if number_match:
         immunities: List[str] = []
         weaknesses: List[str] = []
-        immunityWeaknessMatch = immunityWeaknessRegex.search(text)
-        if immunityWeaknessMatch:
-            for group in immunityWeaknessMatch.group()[1:-1].split(";"):
+        immunity_weakness_match = immunity_weakness_regex.search(text)
+        if immunity_weakness_match:
+            for group in immunity_weakness_match.group()[1:-1].split(";"):
                 if group.strip().startswith("weak"):
                     weaknesses = list(
                         map(str.strip, group.strip()[8:].split(",")))
@@ -99,34 +102,34 @@ def parseGroup(text: str, army: int, number: int) -> Group:
                         map(str.strip, group.strip()[10:].split(",")))
         return Group(
             f"{army}{number}",
-            int(numberMatch.group("units")),
-            int(numberMatch.group("hit")),
+            int(number_match.group("units")),
+            int(number_match.group("hit")),
             immunities, weaknesses,
-            int(numberMatch.group("initiative")),
-            numberMatch.group("type"),
-            int(numberMatch.group("damage")),
+            int(number_match.group("initiative")),
+            number_match.group("type"),
+            int(number_match.group("damage")),
             army
         )
     raise Exception("Bad format", text)
 
 
-def parseArmy(text: str, army: int) -> List[Group]:
+def parse_army(text: str, army: int) -> List[Group]:
     split = text.split("\n")
     groups: List[Group] = []
-    for index, groupText in enumerate(split[1:]):
-        groups.append(parseGroup(groupText, army, index + 1))
+    for index, group_text in enumerate(split[1:]):
+        groups.append(parse_group(group_text, army, index + 1))
     return groups
 
 
-def getInput(filePath: str) -> List[Group]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
+def get_input(file_path: str) -> List[Group]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
 
-    with open(filePath, "r") as file:
-        armyTexts = file.read().split("\n\n")
-        immuneSystem = parseArmy(armyTexts[0], 0)
-        infection = parseArmy(armyTexts[1], 1)
-        return [*immuneSystem, *infection]
+    with open(file_path, "r") as file:
+        army_texts = file.read().split("\n\n")
+        immune_system = parse_army(army_texts[0], 0)
+        infection = parse_army(army_texts[1], 1)
+        return [*immune_system, *infection]
 
 
 def main():
@@ -134,10 +137,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
