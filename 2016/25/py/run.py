4cf4e76b5165aad63b657eae0df526296033b03a
Bruno Bernardo <bigsby@live.com>
2021-04-20 19:06:39 +0100

    2016/25 py clean up

--- 2016/25/py/run.py
+++ 2016/25/py/run.py
@@ -1,12 +1,14 @@
 #! /usr/bin/python3
 
-import sys, os, time
+import sys
+import os
+import time
 from typing import List, Tuple
 
 Instruction = List[str]
 
 
-def solve(instructions: List[Instruction]) -> Tuple[int,str]:
+def solve(instructions: List[Instruction]) -> Tuple[int, str]:
     target = int(instructions[1][1]) * int(instructions[2][1])
     a = 1
     while a < target:
@@ -17,18 +19,12 @@ def solve(instructions: List[Instruction]) -> Tuple[int,str]:
     return a - target, ""
 
 
-def parseLine(line: str) -> Instruction:
-    mnemonic = line[:3]
-    parameters = line[3:].strip()
-    return [ mnemonic ] + parameters.split(" ")
+def get_input(file_path: str) -> List[Instruction]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
 
-
-def getInput(filePath: str) -> List[Instruction]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    
-    with open(filePath, "r") as file:
-        return [ parseLine(line) for line in file.readlines() ]
+    with open(file_path, "r") as file:
+        return [line.split(" ") for line in file.readlines()]
 
 
 def main():
@@ -36,10 +32,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
