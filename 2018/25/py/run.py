631f31f17a2c66ae36da5609bfff34cdc90dd05b
Bruno Bernardo <bigsby@live.com>
2021-04-20 20:49:38 +0100

    2018/25 py clean up

--- 2018/25/py/run.py
+++ 2018/25/py/run.py
@@ -5,37 +5,37 @@ import os
 import time
 from typing import List, Set, Tuple
 
-Point = Tuple[int, int, int, int]
+Point = Tuple[int, ...]
 
 
 def solve(points: List[Point]) -> Tuple[int, str]:
     edges: List[Set[int]] = [set() for _ in range(len(points))]
-    for thisPoint, (w0, x0, y0, z0) in enumerate(points):
-        for thatPoint, (w1, x1, y1, z1) in enumerate(points):
+    for this_point, (w0, x0, y0, z0) in enumerate(points):
+        for that_point, (w1, x1, y1, z1) in enumerate(points):
             if abs(w0 - w1) + abs(x0 - x1) + abs(y0 - y1) + abs(z0 - z1) < 4:
-                edges[thisPoint].add(thatPoint)
+                edges[this_point].add(that_point)
     visited: Set[int] = set()
     constellations = 0
-    for thisPoint in range(len(points)):
-        if thisPoint in visited:
+    for this_point in range(len(points)):
+        if this_point in visited:
             continue
         constellations += 1
-        queue = [thisPoint]
+        queue = [this_point]
         while queue:
-            currentPoint = queue.pop(0)
-            if currentPoint in visited:
+            current_point = queue.pop(0)
+            if current_point in visited:
                 continue
-            visited.add(currentPoint)
-            for other in edges[currentPoint]:
+            visited.add(current_point)
+            for other in edges[current_point]:
                 queue.append(other)
     return constellations, ""
 
 
-def getInput(filePath: str) -> List[Point]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
+def get_input(file_path: str) -> List[Point]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
 
-    with open(filePath, "r") as file:
+    with open(file_path, "r") as file:
         points: List[Point] = []
         for line in file.readlines():
             points.append(tuple(map(int, line.strip().split(","))))
@@ -47,10 +47,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
