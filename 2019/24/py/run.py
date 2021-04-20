46c89c8dcaac2c57e572ec1675fb64c8f2915bfb
Bruno Bernardo <bigsby@live.com>
2021-04-20 17:40:18 +0100

    2019/24 py clean up

--- 2019/24/py/run.py
+++ 2019/24/py/run.py
@@ -11,26 +11,26 @@ Bugs = List[Bug]
 DIRECTIONS = [1, -1, 1j, -1j]
 
 
-def getsBug(hasBug: bool, adjacentCount: int) -> bool:
-    return adjacentCount == 1 if hasBug else adjacentCount in (1, 2)
+def gets_bug(has_bug: bool, adjacent_count: int) -> bool:
+    return adjacent_count == 1 if has_bug else adjacent_count in (1, 2)
 
 
-def nextMinute(bugs: Bugs) -> Bugs:
-    newState: List[Bug] = []
+def next_minute(bugs: Bugs) -> Bugs:
+    new_state: List[Bug] = []
     for y in range(5):
         for x in range(5):
             position = x + y * 1j
-            adjacentCount = sum(
+            adjacent_count = sum(
                 offset + position in bugs for offset in DIRECTIONS)
-            if getsBug(position in bugs, adjacentCount):
-                newState.append(position)
-    return newState
+            if gets_bug(position in bugs, adjacent_count):
+                new_state.append(position)
+    return new_state
 
 
 def part1(bugs: Bugs) -> int:
     previous = [bugs]
     while True:
-        bugs = nextMinute(bugs)
+        bugs = next_minute(bugs)
         if bugs in previous:
             break
         previous.append(bugs)
@@ -49,48 +49,48 @@ MIDDLE_RIGHT = 3 + 2j
 MIDDLE_BOTTOM = 2 + 3j
 
 
-def nextLayeredMinute(layers: Dict[int, Bugs]) -> Dict[int, Bugs]:
-    newState: Dict[int, Bugs] = defaultdict(list)
+def next_layered_minute(layers: Dict[int, Bugs]) -> Dict[int, Bugs]:
+    new_state: Dict[int, Bugs] = defaultdict(list)
     for layer in range(min(layers.keys()) - 1, max(layers.keys()) + 2):
         for y in range(5):
             for x in range(5):
                 position = x + y * 1j
                 if position == CENTER:
                     continue
-                adjacentCount = sum(
+                adjacent_count = sum(
                     offset + position in layers[layer] for offset in DIRECTIONS)
                 if y == 0:
-                    adjacentCount += MIDDLE_TOP in layers[layer - 1]
+                    adjacent_count += MIDDLE_TOP in layers[layer - 1]
                 elif y == 4:
-                    adjacentCount += MIDDLE_BOTTOM in layers[layer - 1]
+                    adjacent_count += MIDDLE_BOTTOM in layers[layer - 1]
 
                 if x == 0:
-                    adjacentCount += MIDDLE_LEFT in layers[layer - 1]
+                    adjacent_count += MIDDLE_LEFT in layers[layer - 1]
                 elif x == 4:
-                    adjacentCount += MIDDLE_RIGHT in layers[layer - 1]
+                    adjacent_count += MIDDLE_RIGHT in layers[layer - 1]
 
                 if position == MIDDLE_TOP:
-                    adjacentCount += sum(x in layers[layer + 1]
-                                         for x in range(5))
+                    adjacent_count += sum(x in layers[layer + 1]
+                                          for x in range(5))
                 elif position == MIDDLE_LEFT:
-                    adjacentCount += sum(y *
-                                         1j in layers[layer + 1] for y in range(5))
+                    adjacent_count += sum(y *
+                                          1j in layers[layer + 1] for y in range(5))
                 elif position == MIDDLE_RIGHT:
-                    adjacentCount += sum(4 + y *
-                                         1j in layers[layer + 1] for y in range(5))
+                    adjacent_count += sum(4 + y *
+                                          1j in layers[layer + 1] for y in range(5))
                 elif position == MIDDLE_BOTTOM:
-                    adjacentCount += sum(x +
-                                         4j in layers[layer + 1] for x in range(5))
-                if getsBug(position in layers[layer], adjacentCount):
-                    newState[layer].append(position)
-    return newState
+                    adjacent_count += sum(x +
+                                          4j in layers[layer + 1] for x in range(5))
+                if gets_bug(position in layers[layer], adjacent_count):
+                    new_state[layer].append(position)
+    return new_state
 
 
 def part2(bugs: Bugs) -> int:
     layers: Dict[int, Bugs] = defaultdict(list)
     layers[0] = bugs
     for _ in range(200):
-        layers = nextLayeredMinute(layers)
+        layers = next_layered_minute(layers)
     return sum(len(bugs) for bugs in layers.values())
 
 
@@ -101,11 +101,11 @@ def solve(bugs: Bugs) -> Tuple[int, int]:
     )
 
 
-def getInput(filePath: str) -> Bugs:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
+def get_input(file_path: str) -> Bugs:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
 
-    with open(filePath, "r") as file:
+    with open(file_path, "r") as file:
         bugs: Bugs = []
         for y, line in enumerate(file.readlines()):
             for x, c in enumerate(line.strip()):
@@ -119,10 +119,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
