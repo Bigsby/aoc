cea7abf0c6d3e4324abc4373af0a92f67f91aaba
Bruno Bernardo <bigsby@live.com>
2021-04-20 18:46:14 +0100

    2020/24 py clean up

--- 2020/24/py/run.py
+++ 2020/24/py/run.py
@@ -19,9 +19,9 @@ DIRECTIONS = {
 }
 
 
-def flipInitialTiles(tilePaths: List[Directions]) -> Floor:
-    floor = Floor()
-    for path in tilePaths:
+def flip_initial_tiles(tile_paths: List[Directions]) -> Floor:
+    floor: Floor = {}
+    for path in tile_paths:
         current = 0
         for direction in path:
             current += DIRECTIONS[direction]
@@ -32,61 +32,61 @@ def flipInitialTiles(tilePaths: List[Directions]) -> Floor:
     return floor
 
 
-def getNeighbors(tile: Tile) -> List[Tile]:
+def get_neighbors(tile: Tile) -> List[Tile]:
     return [tile + direction for direction in DIRECTIONS.values()]
 
 
-def getBlackCount(neighbors: List[Tile], floor: Floor) -> int:
+def get_black_count(neighbors: List[Tile], floor: Floor) -> int:
     return sum([1 for neighbor in neighbors if neighbor in floor and floor[neighbor]])
 
 
-def getTileState(tile: Tile, floor: Floor) -> bool:
+def get_tile_state(tile: Tile, floor: Floor) -> bool:
     return tile in floor and floor[tile]
 
 
-def getNewState(tile: Tile, floor: Floor) -> bool:
-    adjacentBlackCount = getBlackCount(getNeighbors(tile), floor)
-    tileState = getTileState(tile, floor)
-    if tileState and adjacentBlackCount == 0 or adjacentBlackCount > 2:
+def get_new_state(tile: Tile, floor: Floor) -> bool:
+    adjacent_black_count = get_black_count(get_neighbors(tile), floor)
+    tile_state = get_tile_state(tile, floor)
+    if tile_state and adjacent_black_count == 0 or adjacent_black_count > 2:
         return False
-    return (not tileState and adjacentBlackCount == 2) or tileState
+    return (not tile_state and adjacent_black_count == 2) or tile_state
 
 
-def runDay(floor: Floor) -> Floor:
-    newFloor = Floor()
-    edgesToTest: Set[Tile] = set()
+def run_day(floor: Floor) -> Floor:
+    new_floor: Floor = {}
+    edges_to_test: Set[Tile] = set()
     for tile in floor:
-        edgesToTest.update(
-            {neighbor for neighbor in getNeighbors(tile) if neighbor not in floor})
-        newFloor[tile] = getNewState(tile, floor)
-    for tile in edgesToTest:
-        newFloor[tile] = getNewState(tile, floor)
-    return newFloor
+        edges_to_test.update(
+            {neighbor for neighbor in get_neighbors(tile) if neighbor not in floor})
+        new_floor[tile] = get_new_state(tile, floor)
+    for tile in edges_to_test:
+        new_floor[tile] = get_new_state(tile, floor)
+    return new_floor
 
 
 def part2(floor: Floor) -> int:
     for _ in range(100):
-        floor = runDay(floor)
+        floor = run_day(floor)
     return sum(floor.values())
 
 
-def solve(tilePaths: List[Directions]) -> Tuple[int, int]:
-    floor = flipInitialTiles(tilePaths)
+def solve(tile_paths: List[Directions]) -> Tuple[int, int]:
+    floor = flip_initial_tiles(tile_paths)
     return (
         sum(floor.values()),
         part2(floor)
     )
 
 
-lineRegex = re.compile(r"e|se|sw|w|nw|ne")
+line_regex = re.compile(r"e|se|sw|w|nw|ne")
 
 
-def getInput(filePath: str) -> List[Directions]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
+def get_input(file_path: str) -> List[Directions]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
 
-    with open(filePath, "r") as file:
-        return [lineRegex.findall(line.strip()) for line in file.readlines()]
+    with open(file_path, "r") as file:
+        return [line_regex.findall(line.strip()) for line in file.readlines()]
 
 
 def main():
@@ -94,10 +94,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
