a200c008e2a66ad96d97ced0ffc411f162a76b17
Bruno Bernardo <bigsby@live.com>
2021-04-20 19:45:54 +0100

    2017/25 py clean up

--- 2017/25/py/run.py
+++ 2017/25/py/run.py
@@ -22,31 +22,39 @@ def solve(data: Tuple[str, int, States]) -> Tuple[int, str]:
     return sum(tape.values()), ""
 
 
-setupRegex = re.compile(
+setup_regex = re.compile(
     r"^Begin in state (?P<state>\w).*\s+^[^\d]*(?P<steps>\d+)", re.MULTILINE)
-stateRegex = re.compile(
+state_regex = re.compile(
     r"^In state (?P<state>\w):\n.*If.*\n[^\d]*(?P<fValue>\d).\n.*(?P<fSlot>right|left).\n.*state (?P<fState>\w).\n.*If.*\n[^\d]*(?P<tValue>\d).\n.*(?P<tSlot>right|left).\n.*state (?P<tState>\w)", re.MULTILINE)
 
 
-def getInput(filePath: str) -> Tuple[str, int, States]:
-    if not os.path.isfile(filePath):
-        raise FileNotFoundError(filePath)
-    states = States()
-    initialState = ""
+def get_input(file_path: str) -> Tuple[str, int, States]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+    states: States = {}
+    initial_state = ""
     steps = 0
-    with open(filePath, "r") as file:
+    with open(file_path, "r") as file:
         for split in file.read().split("\n\n"):
-            setupMatch = setupRegex.match(split)
-            if setupMatch:
-                initialState = setupMatch.group("state")
-                steps = int(setupMatch.group("steps"))
+            setup_match = setup_regex.match(split)
+            if setup_match:
+                initial_state = setup_match.group("state")
+                steps = int(setup_match.group("steps"))
                 continue
-            stateMatch = stateRegex.match(split)
-            if stateMatch:
-                states[stateMatch.group("state")] = \
-                    ((int(stateMatch.group("fValue")), 1 if stateMatch.group("fSlot") == "right" else -1, stateMatch.group("fState")),
-                     (int(stateMatch.group("tValue")), 1 if stateMatch.group("tSlot") == "right" else -1, stateMatch.group("tState")))
-    return initialState, steps, states
+            state_match = state_regex.match(split)
+            if state_match:
+                states[state_match.group("state")] = \
+                    ((
+                        int(state_match.group("fValue")),
+                        1 if state_match.group("fSlot") == "right" else -1,
+                        state_match.group("fState")
+                    ),
+                    (
+                        int(state_match.group("tValue")),
+                        1 if state_match.group("tSlot") == "right" else -1,
+                        state_match.group("tState")
+                    ))
+    return initial_state, steps, states
 
 
 def main():
@@ -54,10 +62,10 @@ def main():
         raise Exception("Please, add input file path as parameter")
 
     start = time.perf_counter()
-    part1Result, part2Result = solve(getInput(sys.argv[1]))
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
     end = time.perf_counter()
-    print("P1:", part1Result)
-    print("P2:", part2Result)
+    print("P1:", part1_result)
+    print("P2:", part2_result)
     print()
     print(f"Time: {end - start:.7f}")
 
