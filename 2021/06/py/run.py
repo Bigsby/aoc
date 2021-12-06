e4fba1c2ef0cd827c60ea5418662b4cffd83631d
Bruno Bernardo <bigsby@live.com>
2021-12-06 12:36:39 +0000

    2021/06 py

---
+++ 2021/06/py/run.py
@@ -0,0 +1,60 @@
+#! /usr/bin/python3
+
+import sys, os, time
+from typing import Tuple, List
+
+
+def runGenerations(fishes: List[int], generations: int) -> int:
+    fishCounts = [ 0 ] * 9
+    for fish in fishes:
+        fishCounts[fish] += 1
+    for _ in range(generations):
+        fishesAtZero = fishCounts[0]
+        for day in range(8):
+            fishCounts[day] = fishCounts[day + 1]
+        fishCounts[8] = fishesAtZero
+        fishCounts[6] += fishesAtZero
+    return sum(fishCounts)
+#    fishes = list(fishes)
+#    print(fishes)
+#    day = 0
+#    for _ in range(generations):
+#        day += 1
+#        print(day)
+#        for index in range(len(fishes)):
+#            if fishes[index]:
+#                fishes[index] -= 1
+#            else:
+#                fishes[index] = 6
+#                fishes.append(8)
+#        print(fishes); input()
+#    return len(fishes)
+
+
+def solve(puzzle_input: str) -> Tuple[int,int]:
+    return (runGenerations(puzzle_input, 80), runGenerations(puzzle_input, 256))
+
+
+def get_input(file_path: str) -> List[int]:
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(file_path)
+    
+    with open(file_path) as file:
+        return [ int(number) for number in file.read().strip().split(',') ]
+
+
+def main():
+    if len(sys.argv) != 2:
+        raise Exception("Please, add input file path as parameter")
+
+    start = time.perf_counter()
+    part1_result, part2_result = solve(get_input(sys.argv[1]))
+    end = time.perf_counter()
+    print("P1:", part1_result)
+    print("P2:", part2_result)
+    print()
+    print(f"Time: {end - start:.7f}")
+
+
+if __name__ == "__main__":
+    main()
