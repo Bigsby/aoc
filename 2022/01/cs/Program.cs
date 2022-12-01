89013acc3572510da751bd44ba94da66a97f0ee6
Bruno Bernardo <bigsby@live.com>
2022-12-01 12:58:49 +0000

    2022/01 cs

---
+++ 2022/01/cs/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AoC
+{
+    using Elf = List<int>;
+    static class Program
+    {
+        static int Part1(List<Elf> elves) =>
+            elves.Aggregate(0, (currentMax, elf) => Math.Max(currentMax, elf.Sum()));
+
+        static int Part2(List<Elf> elves)
+        {
+            int[] topElves = new [] { 0, 0, 0 };
+            foreach (var elf in elves)
+            {
+                var elfSum = elf.Sum();
+                foreach (var index in Enumerable.Range(0, 3))
+                {
+                    if (elfSum > topElves[index])
+                    {
+                        var temp = topElves[index];
+                        topElves[index] = elfSum;
+                        elfSum = temp;
+                    }
+                }
+            }
+            return topElves.Sum();
+        }
+
+        static (int, int) Solve(List<Elf> elves)
+            => (Part1(elves), Part2(elves));
+
+        static List<Elf> GetInput(string filePath)
+        {
+            if (!File.Exists(filePath)) throw new FileNotFoundException(filePath);
+
+            var elves = new List<Elf>();
+            var currentElf = new Elf();
+            foreach (var line in File.ReadAllLines(filePath))
+                if (string.IsNullOrEmpty(line))
+                {
+                    elves.Add(currentElf);
+                    currentElf = new Elf();    
+                }
+                else
+                    currentElf.Add(int.Parse(line));
+            elves.Add(currentElf);
+            return elves;
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1) throw new Exception("Please, add input file path as parameter");
+
+            var watch = Stopwatch.StartNew();
+            var (part1Result, part2Result) = Solve(GetInput(args[0]));
+            watch.Stop();
+            WriteLine($"P1: {part1Result}");
+            WriteLine($"P2: {part2Result}");
+            WriteLine();
+            WriteLine($"Time: {(double)watch.ElapsedTicks / 100 / TimeSpan.TicksPerSecond:f7}");
+        }
+    }
+}
