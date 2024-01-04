2496d86fd12ea1815f19de656f6f169216a7d779
Bruno Bernardo <bigsby@live.com>
2024-01-04 16:04:16 +0000

    2023/13 cs start part2

--- 2023/13/cs/Program.cs
+++ 2023/13/cs/Program.cs
@@ -35,89 +35,53 @@ namespace AoC
             }
         }
 
-        static int GetMirrorValue(IEnumerable<Complex> pattern)
+        static int GetMirrorValue(IEnumerable<Complex> pattern, int maximumDifference = 0)
         {
-            // PrintPattern(pattern);
+            PrintPattern(pattern);
             var (maxX, maxY) = GetMaxs(pattern);
-            var areRowsEqual = (int one, int two) => Enumerable.Range(0, maxX).All(column => !pattern.Contains(new Complex(column, one)) ^ pattern.Contains(new Complex(column, two)));
-            var areColumnsEqual = (int one, int two) => Enumerable.Range(0, maxY).All(row => !pattern.Contains(new Complex(one, row)) ^ pattern.Contains(new Complex(two, row)));
-            var hasReflection = (int max, Func<int, int, bool> equalityTester, out int reflectionIndex) =>
+            var rowDifferenceCounter = (int one, int two) 
+                => Enumerable.Range(0, maxX)
+                    .Count(column => pattern.Contains(new Complex(column, one)) ^ pattern.Contains(new Complex(column, two)));
+            var columnDifferenceCounter = (int one, int two) 
+                => Enumerable.Range(0, maxY)
+                    .Count(row => pattern.Contains(new Complex(one, row)) ^ pattern.Contains(new Complex(two, row)));
+            var hasReflection = (int max, Func<int, int, int> differenceCounter, string name, out int reflectionIndex) =>
             {
                 for (var index = 0; index < max; index++)
-                    if (equalityTester(index, index + 1))
+                {
+                    var differences = 0;
+                    if ((differences = differenceCounter(index, index + 1)) <= maximumDifference)
                     {
                         var one = index - 1;
                         var two = index + 2;
-                        var isReflection = true;
-                        while (one >= 0 && two < max)
-                            if (!(isReflection &= equalityTester(one--, two++)))
-                                break;
-                        reflectionIndex = index + 1; 
-                        if (isReflection)
-                            return isReflection;
+                        while (one >= 0 && two < max && differences <= maximumDifference)
+                            differences += differenceCounter(one--, two++);
+                        if (differences <= maximumDifference)
+                        {
+                            reflectionIndex = index + 1; 
+                            // WriteLine($"{name} {reflectionIndex} {maximumDifference} {one} {two}"); ReadLine();
+                            return true;
+                        }
                     }
+                }
                 reflectionIndex = -1;
                 return false;
             };
-            if (hasReflection(maxY, areRowsEqual, out var rowIndex))
+            if (hasReflection(maxY, rowDifferenceCounter, "row", out var rowIndex))
                 return rowIndex * 100;
-            if (hasReflection(maxX, areColumnsEqual, out var columnIndex))
+            if (hasReflection(maxX, columnDifferenceCounter, "column", out var columnIndex))
                 return columnIndex;
-
-            // for (var row = 0; row < maxY - 1; row++)
-            // {
-            //     if (areRowsEqual(row, row + 1))
-            //     {
-            //         WriteLine($"Equal rows {row} {row + 1}");
-            //         var one = row - 1;
-            //         var two = row + 2;
-            //         var isReflection = true;
-            //         while (one >= 0 && two < maxY)
-            //             if (!(isReflection &= areRowsEqual(one--, two++)))
-            //                 break;
-            //         WriteLine($"Reflection {isReflection} {one} {two}");
-            //         if (isReflection)
-            //         {
-            //             WriteLine($"Row {row}");
-            //             return (row + 1) * 100;
-            //         }
-            //     }
-            // }
-
-            // for (var column = 0; column < maxX - 1; column++)
-            // {
-            //     if (areColumnsEqual(column, column + 1))
-            //     {
-            //         WriteLine($"Equal columns {column} {column + 1}");
-            //         var one = column - 1;
-            //         var two = column + 2;
-            //         var isReflection = true;
-            //         while (one >= 0 && two < maxX)
-            //             if (!(isReflection &= areColumnsEqual(one--, two++)))
-            //                 break;
-            //         WriteLine($"Reflection {isReflection} {one} {two}");
-            //         if (isReflection)
-            //         {
-            //             WriteLine($"Column {column}");
-            //             return column + 1;
-            //         }
-            //     }
-            // }
+                
             throw new Exception("Reflection not found!");
         }
         
-        static int Part1(Input puzzleInput)
-        { 
-            return puzzleInput.Sum(pattern => GetMirrorValue(pattern));
-        }
-
         static int Part2(Input puzzleInput)
         {
             return 2;
         }
 
         static (int, int) Solve(Input puzzleInput)
-            => (Part1(puzzleInput), Part2(puzzleInput));
+            => (puzzleInput.Sum(pattern => GetMirrorValue(pattern)), puzzleInput.Sum(pattern => GetMirrorValue(pattern, 1)));
 
         static Input GetInput(string filePath)
         {
