ff929f2efe0c8bdc40001c5b4cc42d61e0284d47
Bruno Bernardo <bigsby@live.com>
2024-01-05 14:43:51 +0000

    2023/15 cs part1#

---
+++ 2023/15/cs/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AoC
+{
+    using Input = IEnumerable<string>;
+
+    static class Program
+    {
+        static int GetHashValue(string step)
+        {
+            var currentValue = 0;
+            foreach (var c in step)
+            {
+                currentValue += (byte)c;
+                currentValue *= 17;
+                currentValue %= 256;
+            }
+            return currentValue;
+        }
+        static int Part1(Input puzzleInput)
+        { 
+            return puzzleInput.Sum(step => GetHashValue(step));
+        }
+
+        static int Part2(Input puzzleInput)
+        {
+            return 2;
+        }
+
+        static (int, int) Solve(Input puzzleInput)
+            => (puzzleInput.Sum(step => GetHashValue(step)), Part2(puzzleInput));
+
+        static Input GetInput(string filePath)
+            => !File.Exists(filePath) ? throw new FileNotFoundException(filePath)
+            : File.ReadAllText(filePath).Trim().Split(",");
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1) throw new Exception("Please, add input file path as parameter");
+
+            var watch = Stopwatch.StartNew();
+            var (part1Result, part2Result) = Solve(GetInput(args[0]));
+            watch.Stop();
+            WriteLine($"P1: {part1Result}");
+            WriteLine($"P2: {part2Result}");
+            WriteLine();
+            WriteLine($"Time: {(double)watch.ElapsedTicks / 100 / TimeSpan.TicksPerSecond:f7}");
+        }
+    }
+}
