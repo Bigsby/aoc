fa373737e9c53d8b2a30deb17608f708ed43ea24
Bruno Bernardo <bigsby@live.com>
2025-12-10 19:15:37 +0000

    2025/06 py

--- 2025/06/py/run.py
+++ 2025/06/py/run.py
@@ -1,28 +1,64 @@
 #! /usr/bin/python3
 
 import sys, os, time
-from typing import Tuple
+import re
+from typing import Tuple, List
 
-Input = str
-
-def part1(puzzle_input: Input) -> int:
-    return 1
+Input = List[Tuple[Tuple[str,int,int],List[str]]]
 
 
-def part2(puzzle_input: Input) -> int:
-    return 2
+def p1(homework) -> int:
+    total = 0
+    for op, values in homework:
+        operation, start, end = op
+        sub_total = 0 if operation == '+' else 1
+        for value in values:
+            number = int(value.strip())
+            if operation == '+':
+                sub_total += number
+            else:
+                sub_total *= number
+        total += sub_total
+    return total
+
+
+def p2(homework) -> int:
+    total = 0
+    for op, values in homework:
+        operation, start, end = op
+        sub_total = 0 if operation == '+' else 1
+        for index in range(end - start):
+            digits = [ str(number[index]) for number in values]
+            if all([ d == ' ' for d in digits]):
+                continue
+            value = int("".join(digits).strip())
+            if operation == '+':
+                sub_total += value
+            else:
+                sub_total *= value
+        total += sub_total
+    return total
 
 
 def solve(puzzle_input: Input) -> Tuple[int,int]:
-    return (part1(puzzle_input), part2(puzzle_input))
+    return (p1(puzzle_input), p2(puzzle_input))
 
 
+operations_regex = re.compile(r"([\+|\*] +)")
 def get_input(file_path: str) -> Input:
     if not os.path.isfile(file_path):
         raise FileNotFoundError(file_path)
     
     with open(file_path) as file:
-        return file.read().strip()
+        lines = file.readlines()
+        operations_line = lines[-1]
+        values = lines[:-1]
+        homework = []
+        for match in operations_regex.finditer(operations_line):
+            operation, start, end = match.group(1).strip(), match.start(1), match.end(1)
+            homework.append(((operation, start, end), [ line[start:end] for line in values ]))
+
+        return homework
 
 
 def main():
